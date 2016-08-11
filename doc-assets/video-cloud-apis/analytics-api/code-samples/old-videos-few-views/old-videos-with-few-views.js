0d79c8af14130a33957212f69e75ed8e2700345c
rcrooks <rcrooks@brightcove.com>
2016-08-11 14:46:59 -0400

    WIP

--- doc-assets/video-cloud-apis/analytics-api/code-samples/old-videos-few-views/old-videos-with-few-views.js
+++ doc-assets/video-cloud-apis/analytics-api/code-samples/old-videos-few-views/old-videos-with-few-views.js
@@ -1,7 +1,8 @@
 var BCLS = (function (window, document) {
-    "use strict";
+    'use strict';
     var videoData = [],
-        itemsArray = [],
+        analyticsData = [],
+        videoIdsArray = [],
         lastPublishedDate,
         totalVideos = null,
         videoCount = 0,
@@ -10,11 +11,11 @@ var BCLS = (function (window, document) {
         pageNumber = 0,
         params = {},
         // aapi stuff
-        proxyURL = "https://solutions.brightcove.com/bcls/bcls-proxy/non-performing-videos-proxy.php",
-        useMyAccount = document.getElementById("useMyAccount"),
-        basicInfo = document.getElementById("basicInfo"),
+        proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/non-performing-videos-proxy.php',
+        useMyAccount = document.getElementById('useMyAccount'),
+        basicInfo = document.getElementById('basicInfo'),
         accountID = document.getElementById('accountID'),
-        account_id = "1752604059001",
+        account_id = '1752604059001',
         $client_id = document.getElementById('client_id'),
         $client_secret = document.getElementById('client_secret'),
         client_id = null,
@@ -34,8 +35,8 @@ var BCLS = (function (window, document) {
         from,
         oldestPubDate = now.valueOf() - ($excludeMonths.value * mMonth),
         requestTrimmed = false,
-        lastChar = "",
-        requestURL = "",
+        lastChar = '',
+        requestURL = '',
         totalAnalyticsCalls = 0,
         analyticsCallNumber = 0,
         i,
@@ -49,16 +50,16 @@ var BCLS = (function (window, document) {
      * @return {}
      */
     function bclslog(context, message) {
-        if (window["console"] && console["log"]) {
+        if (window['console'] && console['log']) {
           console.log(context, message);
         }
         return;
     };
 
 
-    // more robust test for strings "not defined"
+    // more robust test for strings 'not defined'
     function isDefined(v) {
-        if (v === "" || v === null || v === undefined || v === NaN) {
+        if (v === '' || v === null || v === undefined || v === NaN) {
             return false;
         }
         return true;
@@ -66,7 +67,7 @@ var BCLS = (function (window, document) {
 
     function removeSpaces(str) {
         if (isDefined(str)) {
-            str = str.replace(/\s+/g, "");
+            str = str.replace(/\s+/g, '');
             return str;
         }
     }
@@ -74,22 +75,48 @@ var BCLS = (function (window, document) {
     function trimRequest() {
         if (!requestTrimmed) {
             lastChar = requestURL.charAt((requestURL.length - 1));
-            if (lastChar === "?" || lastChar === "&" || lastChar === ";") {
+            if (lastChar === '?' || lastChar === '&' || lastChar === ';') {
                 requestURL = requestURL.substring(0, (requestURL.length - 1));
                 // recall this function until trim finished
                 trimRequest(requestURL);
-            } else if (requestURL.indexOf("&&") > -1) {
-                requestURL = requestURL.replace("&&", "&");
-            } else if (requestURL.indexOf("?&") > -1) {
-                requestURL = requestURL.replace("?&", "?");
+            } else if (requestURL.indexOf('&&') > -1) {
+                requestURL = requestURL.replace('&&', '&');
+            } else if (requestURL.indexOf('?&') > -1) {
+                requestURL = requestURL.replace('?&', '?');
             } else {
                 requestTrimmed = true;
             }
         }
     }
-    // construct the request
+
+    /**
+     * find index of an object in array of objects
+     * based on some property value
+     *
+     * @param {array} targetArray array to search
+     * @param {string} objProperty object property to search
+     * @param {string} value of the property to search for
+     * @return {integer} index of first instance if found, otherwise returns -1
+    */
+    function findObjectInArray(targetArray, objProperty, value) {
+        var i, totalItems = targetArray.length, objFound = false;
+        for (i = 0; i < totalItems; i++) {
+            if (targetArray[i][objProperty] === value) {
+                objFound = true;
+                return i;
+            }
+        }
+        if (objFound === false) {
+            return -1;
+        }
+    }
+
+    /**
+     * Builds the API requests and handles responses
+     * @param {String} type the request type (getCount | getVideos | getAnalytics)
+     */
     function buildRequest(type) {
-        var requestOptions = {}, i, iMax;
+        var requestOptions = {}, tmpArray;
         // add credentials if submitted
         if (isDefined(client_id) && isDefined(client_secret)) {
             requestOptions.client_id = client_id;
@@ -97,7 +124,7 @@ var BCLS = (function (window, document) {
         }
         switch (type) {
             case 'getCount':
-                requestOptions.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/counts/videos?q=created_at:' + encodeURI(lastPublishedDate);
+                requestOptions.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/counts/videos?q=published_at:..' + lastPublishedDate;
                 $request.textContent = requestOptions.url;
                 getData(requestOptions, type, function(response) {
                     totalVideos = response.count;
@@ -106,115 +133,58 @@ var BCLS = (function (window, document) {
                 break;
             case 'getVideos':
                 totalVideoCalls = Math.ceil(totalVideos / limit);
-                requestOptions.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/videos?limit=' + limit + '&offset=' + (limit * callNumber);
+                requestOptions.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/videos?q=published_at:..' + lastPublishedDate + '&limit=' + limit + '&offset=' + (limit * callNumber) + '&sort=published_at';
                 getData(requestOptions, type, function(response) {
+                    // add the current item array to overall one
                     videoData = videoData.concat(response);
                     callNumber++;
                     if (callNumber < totalVideoCalls) {
+                        // still have more videos to get
                         buildRequest('getVideos');
                     } else {
                         videoData.forEach(function(video, index, videoData) {
+                            // initialize the analytics data for each video
                             video.video_view = 0;
                             video.engagement_score = 0;
                             video.video_percent_viewed = 0;
+                            // add the video id to the video ids array
+                            videoIdsArray.push(video.id);
                         });
+                        // reset callNumber
+                        callNumber = 0;
                         buildRequest('getAnalytics');
                     }
                 });
                 break;
             case 'getAnalytics':
-                totalAnalyticsCalls = Math.ceil(totalVideos / limit);
+                // because of URL length restrictions, we can get up to 150 videos at a time
+                totalAnalyticsCalls = Math.ceil(videoIdsArray.length / 150);
+                tmpArray = videoIdsArray.slice((callNumber * 150), ((callNumber * 150) + 150));
                 account_id = (isDefined(accountID.value)) ? accountID.value : account_id;
                 minViews = $includeVideos.value;
-                requestURL = "https://analytics.api.brightcove.com/v1";
-                requestURL += "/data?accounts=" + account_id + "&dimensions=video";
-                requestURL += "&from=" + from;
+                requestOptions.url = 'https://analytics.api.brightcove.com/v1';
+                requestOptions.url += '/data?accounts=' + account_id + '&dimensions=video';
+                // add where filter
+                requestOptions.url += '&where=video==' + tmpArray.join(',');
+                // add from date
+                requestURL += '&from=' + from;
                 // add fields
-                requestURL += "&fields=video,engagement_score,video_view,video_percent_viewed";
-                $request.textContent = requestURL;
-                $request.setAttribute("value", requestURL);
-                getData();
+                requestURL += '&fields=video,engagement_score,video_view,video_percent_viewed';
+                $request.textContent = requestOptions.url;
+                $request.setAttribute('value', requestURL);
+                getData(requestOptions, type, function(response) {
+                    analyticsData = analyticsData.concat(response.items);
+                    callNumber++;
+                    if (callNumber < totalAnalyticsCalls) {
+                        buildRequest('getAnalytics');
+                    } else {
+                        bclslog('analyticsData', analyticsData);
+                    }
+                });
                 break;
         }
 
     }
-    // submit request
-    function oldgetData() {
-        var itemsMax, item, video, options = {}, data;
-        // clear the results frame
-        $responseFrame.html("Loading...");
-        options.client_id = (isDefined($client_id.value)) ? $client_id.value : client_id;
-        options.client_secret = (isDefined($client_secret.value)) ? $client_secret.value : client_secret;
-        options.url = $request.value;
-        options.requestType = "GET";
-        options.requestBody = null;
-        bclslog("options", options);
-        $.ajax({
-            url: proxyURL,
-            type: "POST",
-            data: options,
-            success: function (jsondata) {
-                bclslog("aapi call complete");
-                try {
-                  data = JSON.parse(jsondata);
-                } catch (e) {
-                  alert('invalid json');
-                }
-                minViews = $includeVideos.value;
-                itemsMax = data.items.length;
-                // add analytics data to video data
-                for (i = 0; i < itemsMax; i++) {
-                    item = data.items[i];
-                    if (isDefined(videoData[parseInt(item.video)])) {
-                        videoData[item.video].engagement_score = item.engagement_score;
-                        videoData[item.video].video_view = item.video_view;
-                        videoData[item.video].average_percent_viewed = item.video_percent_viewed / item.video_view;
-                    }
-                }
-                // convert it to a simple array to make conversion to CSV easier
-                for (video in videoData) {
-                    itemsArray.push(videoData[video]);
-                }
-                analyticsCallNumber++;
-                if (analyticsCallNumber < totalAnalyticsCalls) {
-                    buildRequest();
-                } else {
-                    itemsMax = itemsArray.length;
-                    // assume videos not returned in analytics data had 0 views
-                    for (i = 0; i < itemsMax; i++) {
-                        video = itemsArray[i];
-                        if (!video.hasOwnProperty("video_view")) {
-                            video.engagement_score = 0;
-                            video.video_view = 0;
-                            video.average_percent_viewed = 0;
-                        }
-                    }
-                    // remove videos above the views minimum
-                    i = itemsArray.length;
-                    while (i--) {
-                        video = itemsArray[i];
-                        if (video.video_view > minViews) {
-                            itemsArray.splice(i, 1);
-                        }
-                    }
-                    // remove items if the published date is too new
-                    i = itemsArray.length;
-                    while (i--) {
-                        video = itemsArray[i];
-                        if (video.publishedDate > oldestPubDate) {
-                            itemsArray.splice(i, 1);
-                        }
-                    }
-                    $responseFrame.html(JSON.stringify(itemsArray, null, '  '));
-                }
-
-            },
-            error : function (XMLHttpRequest, textStatus, errorThrown) {
-                $responseFrame.html("Sorry, your request was not successful. Here's what the server sent back: " + errorThrown);
-            }
-        });
-    }
-
     /**
      * send API request to the proxy
      * @param  {Object} requestData options for the request
@@ -232,17 +202,8 @@ var BCLS = (function (window, document) {
                   if (httpRequest.readyState === 4) {
                     if (httpRequest.status === 200) {
                       bclslog('httpRequest.responseText', httpRequest.responseText);
-                      switch (type) {
-                          case 'getCount':
-                              parsedData = JSON.parse(httpRequest.responseText);
-                              callback(parsedData);
-                              break;
-                          case 'getVideos':
-
-                              break;
-                          case 'getAnalytics':
-
-                      }
+                      parsedData = JSON.parse(httpRequest.responseText);
+                      callback(parsedData);
                     } else {
                       alert('There was a problem with the request. Request returned ' + httpRequest.status);
                     }
@@ -252,9 +213,9 @@ var BCLS = (function (window, document) {
                 }
             };
             // set up request data
-        requestParams = "url=" + encodeURIComponent(options.url) + "&requestType=GET";
+        requestParams = 'url=' + encodeURIComponent(options.url) + '&requestType=GET';
         if (options.client_id && options.client_secret) {
-            requestParams += "&client_id=" + options.client_id + '&client_secret=' + options.client_secret;
+            requestParams += '&client_id=' + options.client_id + '&client_secret=' + options.client_secret;
         }
         bclslog('requestParams', requestParams);
 
@@ -263,7 +224,7 @@ var BCLS = (function (window, document) {
         // open the request
         httpRequest.open('POST', proxyURL);
         // set headers
-        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         // open and send request
         httpRequest.send(requestParams);
     }
@@ -271,34 +232,23 @@ var BCLS = (function (window, document) {
     // convert data to CSV
     function jsonToCSV() {
         // templates are built dynamically to allow for additional fields added later
-        var headersRow = "", rowTemplate = "{{#items}}", property, template, results, str = "", obj = {};
-        obj.items = itemsArray;
-        $responseFrame.html("Loading CSV data...");
-        for (property in obj.items[0]) {
-            headersRow += "\"" + property + "\",";
-            rowTemplate += "\"{{" + property + "}}\",";
-        }
-        headersRow += " \r";
-        rowTemplate += "\r{{/items}}";
-        str = headersRow;
-        template = Handlebars.compile(rowTemplate);
-        results = template(obj);
-        str += results;
-        $responseFrame.html(str);
+        var headersRow = '';
+        $responseFrame.textContent = 'Loading CSV data...';
+        $responseFrame.textContent = str;
     }
 
     // set event listeners
-    useMyAccount.addEventListener("click", function () {
-        if (basicInfo.getAttribute('style') === "display:none;") {
+    useMyAccount.addEventListener('click', function () {
+        if (basicInfo.getAttribute('style') === 'display:none;') {
             basicInfo.setAttribute('style', 'display:block;');
-            useMyAccount.innerHTML = "Use Sample Account";
+            useMyAccount.innerHTML = 'Use Sample Account';
         } else {
             basicInfo.setAttribute('style', 'display:none;');
-            useMyAccount.innerHTML = "Use My Account Instead";
+            useMyAccount.innerHTML = 'Use My Account Instead';
         }
     });
     // send request
-    $submitButton.addEventListener("click", function () {
+    $submitButton.addEventListener('click', function () {
         // get input values
         if (isDefined($client_id.value)) {
             client_id = $client_id.value;
@@ -324,10 +274,10 @@ var BCLS = (function (window, document) {
         }
     });
     // convert to csv
-    $csvButton.addEventListener("click", jsonToCSV);
+    $csvButton.addEventListener('click', jsonToCSV);
     // select all the data
-    $selectData.addEventListener("click", function () {
-        document.getElementById("responseFrame").select();
+    $selectData.addEventListener('click', function () {
+        document.getElementById('responseFrame').select();
     });
     // set initial value of from
     from = now.valueOf() - mMonth;
