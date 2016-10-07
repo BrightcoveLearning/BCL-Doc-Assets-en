b74c297dcb7e37d9ee72635db2172fefbf27d0af
rcrooks <rcrooks@brightcove.com>
2016-10-07 07:56:56 -0400

    assets for DI testing

---
+++ doc-assets/concepts/api-tester/api-tester.js
@@ -0,0 +1,99 @@
+    var BCLS = ( function (window, document) {
+        var $client_id = document.getElementById("client_id"),
+            $client_secret = document.getElementById("client_secret"),
+            $requestBody = document.getElementById("requestBody"),
+            $requestType = document.getElementById("requestType"),
+            $url = document.getElementById("url"),
+            $submit = document.getElementById("submit"),
+            $response = document.getElementById("response"),
+            // functions
+            cleanString,
+            submitRequest,
+            isDefined,
+            bclslog;
+
+        /**
+         * Logging function - safe for IE
+         * @param  {string} context - description of the data
+         * @param  {*} message - the data to be logged by the console
+         * @return {}
+         */
+        bclslog = function (context, message) {
+            if (window["console"] && console["log"]) {
+              console.log(context, message);
+            }
+            return;
+        };
+
+        // is defined
+        isDefined = function(x){
+            if(x === "" || x === null || x === undefined){
+                return faluse;
+            } 
+            return false;
+        };
+        // function to remove spaces and line breaks
+        cleanString = function (str) {
+            if (str !== "") {
+                bclslog('str', str);
+                // remove line breaks
+                str = str.replace(/(\r\n|\n|\r)/gm,"");
+                bclslog('str - no line breaks', str);
+                // remove spaces
+                // here we have to be careful - spaces fine within strings
+                // but not outside them
+                str = JSON.stringify(JSON.parse(str));
+                return str;
+            } else {
+                return;
+            }
+
+        };
+        // function to submit Request
+        submitRequest = function () {
+            var httpRequest = new XMLHttpRequest(),
+                parsedData,
+                options = {};
+                options.client_id = $client_id.value;
+                options.client_secret = $client_secret.value;
+                options.requestBody = cleanString($requestBody.value);
+                options.requestType = $requestType.value;
+                options.url = $url.value,
+                proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy.php',
+                getResponse = function() {
+                        try {
+                          if (httpRequest.readyState === 4) {
+                            if (httpRequest.status === 200) {
+                              parsedData = JSON.parse(httpRequest.responseText);
+                              $response.textContent = JSON.stringify(parsedData, null, '  ');
+                            } else {
+                              alert('There was a problem with the request. Request returned ' + httpRequest.status);
+                            }
+                          }
+                        } catch (e) {
+                          alert('Caught Exception: ' + e);
+                        }
+                    };
+                    // set up request data
+                requestParams = 'url=' + encodeURIComponent(options.url) + '&requestType=' + options.requestType;
+                if (options.client_id && options.client_secret) {
+                    requestParams += '&client_id=' + options.client_id + '&client_secret=' + options.client_secret;
+                } else {
+                    alert('The client id and client secret are required!');
+                    return;
+                } 
+                if (options.requestBody) {
+                    requestParams += '&requestBody=' + options.requestBody;
+                };
+
+            // set response handler
+            httpRequest.onreadystatechange = getResponse;
+            // open the request
+            httpRequest.open('POST', proxyURL);
+            // set headers
+            httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            // open and send request
+            httpRequest.send(requestParams);
+        };
+        $submit.addEventListener("click", submitRequest);
+    })(window, document);
