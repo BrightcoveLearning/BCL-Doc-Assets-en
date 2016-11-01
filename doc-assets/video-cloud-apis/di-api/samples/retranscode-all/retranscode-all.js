823f8baa194d8a55f898674cdcbd9675351c9e44
rcrooks <rcrooks@brightcove.com>
2016-11-01 12:10:07 -0400

    WIP retranscode app

--- doc-assets/video-cloud-apis/di-api/samples/retranscode-all/retranscode-all.js
+++ doc-assets/video-cloud-apis/di-api/samples/retranscode-all/retranscode-all.js
@@ -1,36 +1,28 @@
 var BCLS = ( function (window, document) {
     var // CMS API stuff
-        account_id_display = document.getElementById("account_id"),
-        account_id,
-        client_id_display = document.getElementById("client_id"),
-        client_id,
-        client_secret_display = document.getElementById("client_secret"),
-        client_secret,
-        ingest_profile_display = document.getElementById("ingest_profile_display"),
+        account_id = document.getElementById('account_id'),
+        client_id = document.getElementById('client_id'),
+        client_secret = document.getElementById('client_secret'),
+        ingest_profile_select = document.getElementById('ingest_profile_select'),
         ingest_profile,
-        custom_profile_display = document.getElementById("custom_profile_display"),
-        videoDataDisplay = document.getElementById("videoData"),
+        custom_profiles = document.getElementById('custom_profiles'),
+        di_submit = document.getElementById('di_submit'),
         // Dynamic Ingest API stuff
-        profilesArray = ['high-resolution', 'balanced-nextgen-player', 'screencast-1280', 'smart-player-transition', 'single-bitrate-high', 'audio-only', 'videocloud-default-v1', 'single-bitrate-standard'],
-        di_url_display = document.getElementById("di_url"),
-        di_submit_display = document.getElementById("di_Submit"),
-        diURL = "https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy.php",
-        response = document.getElementById("response"),
+        profilesArray = ['videocloud-default-v1', 'high-resolution', 'screencast-1280', 'smart-player-transition', 'single-bitrate-high', 'single-bitrate-standard', 'audio-only'],
+        cmsURL = 'https://cms.api.brightcove.com/v1/accounts/',
+        ipURL = 'https://ingestion.api.brightcove.com/v1/accounts/',
+        diURL = 'https://ingest.api.brightcove.com/v1/accounts/',
+        diSuffix = '/ingest-requests',
+        proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/di-proxy.php',
+        response = document.getElementById('response'),
         videoData = [],
         totalVideos,
         videoNumber = 0,
         currentJobs = 0,
         t2,
         totalIngested = 0,
-        defaults = {account_id: 57838016001,client_id: "37cd3c5d-6f18-4702-bfb6-4fbc1cd095f1",client_secret: "gLSQANqe6A2PzJce_6xA4bTNu844up5-CSrC-jxNfur4EaOgWKRcqq_GTxKjhMpPSflMdNEhFdBmNe0qsTIZSQ"},
-        // functions
-        isDefined,
-        bclslog,
-        logResponse,
-        doIngest,
-        submitRequest,
-        setDIOptions,
-        init;
+        // profiles that shouldn't be used
+        deprecatedProfiles = ['balanced-nextgen-player', 'Express Standard', 'mp4-only', 'balanced-high-definition', 'low-bandwidth-devices', 'balanced-standard-definition', 'single-rendition', 'high-bandwidth-devices'];
 
     /**
      * Logging function - safe for IE
@@ -38,8 +30,8 @@ var BCLS = ( function (window, document) {
      * @param  {*} message - the data to be logged by the console
      * @return {}
      */
-    bclslog = function (context, message) {
-        if (window["console"] && console["log"]) {
+    function bclslog(context, message) {
+        if (window['console'] && console['log']) {
           console.log(context, message);
         }
         return;
@@ -47,36 +39,35 @@ var BCLS = ( function (window, document) {
 
 
     // is defined
-    isDefined = function(x){
-        if (x !== "" && x !== null && x !== undefined){
-            return true;
-        } else{
+    function isDefined(x){
+        if (x === '' || x === null || x === undefined){
             return false;
         }
+        return true;
     };
     // set options for the Dynamic Ingest API request
-    setDIOptions = function () {
+    function setOptions() {
         var options = {},
             custom_profile_display_value = custom_profile_display.value;
         // get the ingest profile
         if (isDefined(custom_profile_display_value)) {
             ingest_profile = custom_profile_display_value;
         } else {
-            ingest_profile = ingest_profile_display.options[ingest_profile_display.selectedIndex].value;
+            ingest_profile = ingest_profile_select.options[ingest_profile_select.selectedIndex].value;
         }
         options.client_id = client_id;
         options.client_secret = client_secret;
-        di_url_display.value = "https://ingest.api.brightcove.com/v1/accounts/" + account_id + "/videos/" + videoData[videoNumber].id + "/ingest-requests";
-        options.requestBody = '{"master":{"use_archived_master": true },"profile":"' + ingest_profile + '"}';
-        options.requestType = "POST";
+        di_url_display.value = 'https://ingest.api.brightcove.com/v1/accounts/' + account_id + '/videos/' + videoData[videoNumber].id + '/ingest-requests';
+        options.requestBody = '{'master':{'use_archived_master': true },'profile':'' + ingest_profile + ''}';
+        options.requestType = 'POST';
         options.url = di_url_display.value;
         bclslog('options', options);
         // now submit the request
-        submitRequest(options, diURL, "di");
+        submitRequest(options, diURL, 'di');
     };
     // function to set the request
     logResponse = function (type, data) {
-        response.textContent += type + ": " + data + ",\n";
+        response.textContent += type + ': ' + data + ',\n';
     };
 
     // function to submit Request
@@ -91,10 +82,10 @@ var BCLS = ( function (window, document) {
                       if (httpRequest.status === 200) {
                         logResponse(type, httpRequest.responseText);
                         responseData = httpRequest.responseText;
-                        if (responseData.indexOf("error_code") < 0) {
+                        if (responseData.indexOf('error_code') < 0) {
                             // handle the response
                         totalIngested++;
-                        logResponse("totalIngested", totalIngested);
+                        logResponse('totalIngested', totalIngested);
                         if (videoNumber < totalVideos - 1) {
                             videoNumber++;
                             currentJobs++;
@@ -105,21 +96,21 @@ var BCLS = ( function (window, document) {
                                 // reset currentJobs
                                 currentJobs = 0;
                                 // wait 30 min before resuming
-                                t2 = setTimeout(setDIOptions, 1800000);
+                                t2 = setTimeout(setOptions, 1800000);
                             } else {
                                 // pause to avoid CMS API timeouts
-                                t2 = setTimeout(setDIOptions, 1000);
+                                t2 = setTimeout(setOptions, 1000);
                             }
                         }
                     } else {
-                        logResponse("DI", "Request failed; retrying video number: " + videoData[videoNumber].id);
+                        logResponse('DI', 'Request failed; retrying video number: ' + videoData[videoNumber].id);
                         videoNumber++;
                         // give proxy a second to rest
-                        t2 = setTimeout(setDIOptions, 1000);
+                        t2 = setTimeout(setOptions, 1000);
                     }
 
                       } else {
-                        alert("There was a problem with the request. Request returned " + httpRequest.status);
+                        alert('There was a problem with the request. Request returned ' + httpRequest.status);
                       }
                     }
                   }
@@ -129,23 +120,23 @@ var BCLS = ( function (window, document) {
             };
         bclslog('options', options);
         // set up request data
-        requestData = "client_id=" + options.client_id + "&client_secret=" + options.client_secret + "&url=" + encodeURIComponent(options.url) + "&requestBody=" + encodeURIComponent(options.requestBody) + "&requestType=" + options.requestType;
+        requestData = 'client_id=' + options.client_id + '&client_secret=' + options.client_secret + '&url=' + encodeURIComponent(options.url) + '&requestBody=' + encodeURIComponent(options.requestBody) + '&requestType=' + options.requestType;
         // set response handler
         httpRequest.onreadystatechange = getResponse;
         // open the request
-        httpRequest.open("POST", proxyURL);
+        httpRequest.open('POST', proxyURL);
         // set headers
-        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         // open and send request
         httpRequest.send(requestData);
     };
-    di_submit_display.addEventListener("click", function () {
-        bclslog("in button handler", videoDataDisplay.value);
+    di_submit_display.addEventListener('click', function () {
+        bclslog('in button handler', videoDataDisplay.value);
         videoData = JSON.parse(videoDataDisplay.value);
         totalVideos = videoData.length;
-        bclslog("videoData length");
+        bclslog('videoData length');
         // to insure uniqueness,
-        bclslog("totalVideos", totalVideos);
+        bclslog('totalVideos', totalVideos);
         // in case of stop/start, reset videoNumber to 0
         videoNumber = 0;
         // get account inputs
@@ -153,7 +144,7 @@ var BCLS = ( function (window, document) {
         client_id = isDefined(client_id_display.value) ? client_id_display.value : defaults.client_id;
         client_secret = isDefined(client_secret_display.value) ? client_secret_display.value : defaults.client_secret;
         // set CMS API options for first video
-        setDIOptions();
+        setOptions();
     });
     // initialize
     init = function () {
@@ -163,7 +154,7 @@ var BCLS = ( function (window, document) {
         // add options for standard ingest profiles
         for (i = 0; i < iMax; i++) {
             newOpt = new Option(profilesArray[i]);
-            ingest_profile_display.add(newOpt);
+            ingest_profile_select.add(newOpt);
         }
     };
     // call init to set things up
