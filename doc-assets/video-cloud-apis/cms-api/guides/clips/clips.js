307af0ee1a3447aa023fa0c0179f201f43a0c8fc
rcrooks <rcrooks@brightcove.com>
2016-12-05 12:17:05 -0500

    clip assets

--- doc-assets/video-cloud-apis/cms-api/guides/clips/clips.js
+++ doc-assets/video-cloud-apis/cms-api/guides/clips/clips.js
@@ -24,4 +24,52 @@ var BCLS = (function (window, document) {
                 break;
         }
     }
+
+    /**
+     * send API request to the proxy
+     * @param  {Object} requestData options for the request
+     * @param  {Function} [callback] callback function
+     */
+    function getMediaData(options, callback) {
+        var httpRequest = new XMLHttpRequest(),
+            response,
+            parsedData,
+            requestParams,
+            dataString,
+            renditions,
+            i = 0,
+            iMax,
+            // response handler
+            getResponse = function() {
+                try {
+                    if (httpRequest.readyState === 4) {
+                        if (httpRequest.status === 200 || httpRequest.status === 204) {
+                            response = httpRequest.responseText;
+
+                            } else {
+                                alert('There was a problem with the request. Request returned ' + httpRequest.status);
+                            }
+                        }
+                    }
+                } catch (e) {
+                    alert('Caught Exception: ' + e);
+                }
+            };
+        // set up request data
+        requestParams = "url=" + encodeURIComponent(options.url) + "&requestType=" + options.requestType;
+        // only add client id and secret if both were submitted
+        if (isDefined(clientId) && isDefined(clientSecret)) {
+            requestParams += '&client_id=' + clientId + '&client_secret=' + clientSecret;
+        }
+
+        // set response handler
+        httpRequest.onreadystatechange = getResponse;
+        // open the request
+        httpRequest.open('POST', proxyURL);
+        // set headers
+        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        // open and send request
+        httpRequest.send(requestParams);
+    }
+
 })(window, document);
