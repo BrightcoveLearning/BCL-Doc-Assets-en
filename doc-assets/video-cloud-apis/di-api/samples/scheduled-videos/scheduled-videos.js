9e23a29b44c64e353da7205178f584c724b4e972
Robert Crooks <rcrooks@brightcove.com>
2017-03-09 12:29:29 -0500

    Update WIP

--- doc-assets/video-cloud-apis/di-api/samples/scheduled-videos/scheduled-videos.js
+++ doc-assets/video-cloud-apis/di-api/samples/scheduled-videos/scheduled-videos.js
@@ -9,24 +9,20 @@ var BCLS = ( function (window, document) {
         di_url                 = document.getElementById('di_url'),
         cms_url                = document.getElementById('cms_url'),
         di_submit              = document.getElementById('di_submit'),
+        profilesArray = ['videocloud-default-v1', 'high-resolution', 'screencast-1280', 'smart-player-transition', 'single-bitrate-high', 'audio-only', 'single-bitrate-standard'],
         fromPicker,
         toPicker,
+        video_id,
+        account,
+        cid,
+        csec,
+        starts_at,
+        ends_at,
+        profile,
         now                    = new Date(),
         // get the date part of the date-time string
         nowISO                 = now.toISOString().substr(0, 10);
 
-    // add date pickers to the date input fields
-    fromPicker = new Pikaday({
-      field: fromDatePicker,
-      format: 'YYYY-MM-DD'
-    });
-    toPicker = new Pikaday({
-      field: toDatePicker,
-      format: 'YYYY-MM-DD'
-    });
-    // set initial from value
-    fromDatePicker.value = nowISO;
-    console.log('nowISO', nowISO);
 
     /**
      * tests for all the ways a variable might be undefined or not have a value
@@ -45,46 +41,39 @@ var BCLS = ( function (window, document) {
      * @param  {string} type the request type
      */
     function createRequest(type) {
-        var options   = {},
-            cmsBaseURL = 'https://cms.api.brightcove.com/v1/accounts/' + account.value,
-            diBaseURL = 'https://ingest.api.brightcove.com/v1/accounts/' + account.value,
+        var options    = {},
+            cmsBaseURL = 'https://cms.api.brightcove.com/v1/accounts/' + account,
+            diBaseURL  = 'https://ingest.api.brightcove.com/v1/accounts/' + account,
             endpoint,
-            responseDecoded,
-            i,
-            iMax,
-            el,
-            txt;
+            responseDecoded;
 
         // set credentials
-        options.client_id     = client_id.value;
-        options.client_secret = client_secret.value;
+        options.client_id     = cid;
+        options.client_secret = csec;
+        options.proxyURL      = 'https://solutions.brightcove.com/bcls/bcls-proxy/brightcove-learning-proxy.php';
 
         switch (type) {
-            case 'getProfiles':
-                options.proxyURL = './profiles-proxy.php';
-                endpoint         = '/profiles';
-                options.url      = ipBaseURL + endpoint;
+            case 'createVideo':
+                endpoint           = '/videos';
+                options.url        = cmsBaseURL + endpoint;
+                option.requestType = 'POST';
                 makeRequest(options, function(response) {
                     responseDecoded = JSON.parse(response);
-                    if (Array.isArray(responseDecoded)) {
-                        // remove existing options
-                        iMax = profiles.length;
-                        for (i = 0; i < iMax; i++) {
-                            profiles.remove(i);
-                        }
-                        // add new options
-                        iMax = responseDecoded.length;
-                        for (i = 0; i < iMax; i++) {
-                            el = document.createElement('option');
-                            el.setAttribute('value', responseDecoded[i].name);
-                            txt = document.createTextNode(responseDecoded[i].name);
-                            el.appendChild(txt);
-                            profiles.appendChild(el);
-                        }
-                    }
+                    video_id = responseDecoded.id;
+                    createRequest('ingestVideo');
                 });
                 break;
             // additional cases
+            case 'ingestVideo':
+            endpoint           = '/videos/' + video_id + '/ingest-requests';
+            options.url        = diBaseURL + endpoint;
+            option.requestType = 'POST';
+            makeRequest(options, function(response) {
+                responseDecoded = JSON.parse(response);
+                video_id = responseDecoded.id;
+                createRequest('ingestVideo');
+            });
+                break;
             default:
                 console.log('Should not be getting to the default case - bad request type sent');
                 break;
@@ -101,6 +90,7 @@ var BCLS = ( function (window, document) {
      * @param  {String} options.proxyURL proxyURL to send the request to
      * @param  {String} options.client_id client id for the account (default is in the proxy)
      * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+     * @param  {String} options.requestType HTTP type for the request
      * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
      * @param  {Function} [callback] callback function that will process the response
      */
@@ -159,5 +149,41 @@ var BCLS = ( function (window, document) {
         httpRequest.send(requestParams);
     }
 
+    // initialize
+    function init() {
+        var i,
+            iMax = profilesArray.length,
+            newOpt;
+        // add date pickers to the date input fields
+        fromPicker = new Pikaday({
+          field: fromDatePicker,
+          format: 'YYYY-MM-DD'
+        });
+        toPicker = new Pikaday({
+          field: toDatePicker,
+          format: 'YYYY-MM-DD'
+        });
+        // set initial from value
+        fromDatePicker.value = nowISO;
+        // add options for standard ingest profiles
+        for (i = 0; i < iMax; i++) {
+            newOpt = new Option(profilesArray[i]);
+            ingest_profile_display.add(newOpt);
+        }
+        // add event listener for the submit button
+        di_submit.addEventListener('click', function() {
+            account = (isDefined(account_id.value)) ? account_id.value : '57838016001';
+            cid = client_id.value;
+            csec = client_secret.value;
+            starts_at = fromDatePicker.value;
+            ends_at = toDatePicker.value;
+            if (isDefined(custom_profile_display.value)) {
+
+            }
+
+        });
+    }
+    // call init to set things up
+    init();
 
 })(window, document);
