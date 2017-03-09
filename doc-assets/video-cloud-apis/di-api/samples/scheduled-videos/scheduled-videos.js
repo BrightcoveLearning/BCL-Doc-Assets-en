09a10d7b828d2dd3ed01a6decca5e6cc0fc4161a
Robert Crooks <rcrooks@brightcove.com>
2017-03-08 21:05:57 -0500

    WIP di sample

---
+++ doc-assets/video-cloud-apis/di-api/samples/scheduled-videos/scheduled-videos.js
@@ -0,0 +1,17 @@
+var BCLS = ( function (window, document) {
+    var fromDatePicker = document.getElementById('fromDatePicker'),
+        toDatePicker = document.getElementById('toDatePicker'),
+        account_id = document.getElementById('account_id'),
+        client_id = document.getElementById('client_id'),
+        client_secret = document.getElementById('client_secret'),
+        ingest_profile_display = document.getElementById('ingest_profile_display'),
+        custom_profile_display = document.getElementById('custom_profile_display'),
+        di_url = document.getElementById('di_url'),
+        cms_url = document.getElementById('cms_url'),
+        di_submit = document.getElementById('di_submit'),
+        fromPicker,
+        toPicker,
+        now = new Date(),
+
+
+})(window, document);
