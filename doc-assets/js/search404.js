b230e087e1b718625737369901ec42ad6e98d565
Robert Crooks <rcrooks@brightcove.com>
2017-05-21 13:02:07 -0400

    Script update

--- doc-assets/js/search404.js
+++ doc-assets/js/search404.js
@@ -1,6 +1,6 @@
 var BCLSfinder = ( function (window, document) {
-    var homeDomain = '//' + window.location.domain,
-        pathArray = window.location.path.split('/'),
+    var homeDomain = '//' + window.location.hostname,
+        pathArray = window.location.pathname.split('/'),
         path = pathArray[pathArray.length - 1],
         body = document.getElementsByTagName('body')[0],
         searchURL,
