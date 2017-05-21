c6aae4cedd17cc08414ca9c4b22faca72da27ed3
Robert Crooks <rcrooks@brightcove.com>
2017-05-21 14:02:51 -0400

    Last + 1

--- doc-assets/js/search404.js
+++ doc-assets/js/search404.js
@@ -2,7 +2,7 @@ var BCLSfinder = ( function (window, document) {
     var homeDomain = '//' + window.location.hostname,
         pathArray = window.location.pathname.split('/'),
         path = pathArray[pathArray.length - 1],
-        body = document.getElementsByTagName('body')[0],
+        body = document.getElementById('messageHeader'),
         searchURL,
         searchTag,
         searchLink,
@@ -10,7 +10,7 @@ var BCLSfinder = ( function (window, document) {
         message = '<p style="font-weight">To serve you better going forward, the Brightcove documentation site has been completely revamped. The downside is that some of the links you have been using are changed, and we apologize in advance for any inconvenience this causes.</p> <p style="font-weight">To get you as close as possible to the document you are seeking, an intelligent search has been implemented and will be executed when you click the Continue button located below. The search will very likely yield the desired document at or near the top of the returned list of documents.</p> <p style="font-weight">Please take the time to re-bookmark the page once you find it on the new site.</p> <p>For a video introduction to the new site, visit the <a id="homeLink" href="">home page</a> and watch the “Learn about the new Support site” video.</p><p style="text-align:center"><a id="searchLink" href=""><button class="bcls-button">Continue</button></a></p>';
 
     function injectMessage(el) {
-        body.insertAdjacentHTML('beforebegin', message);
+        body.insertAdjacentHTML('afterend', message);
         homeLink = document.getElementById('homeLink');
         searchLink = document.getElementById('searchLink');
         homeLink.setAttribute('href', homeDomain);
