f49d259cc61121f78a0bfa76ded1583568706710
rcrooks <rcrooks@brightcove.com>
2017-08-21 07:05:36 -0400

    another

--- doc-assets/js/searchDocs-once.js
+++ doc-assets/js/searchDocs-once.js
@@ -10,7 +10,7 @@ var BCLSfinder = ( function (window, document) {
         message = '<div style="padding:1em;background-color:#EEEEEE"><p style="font-weight:bold">To serve you better going forward, the Brightcove documentation site has been completely revamped. The downside is that some of the links you have been using are changed, and we apologize in advance for any inconvenience this causes.</p> <p style="font-weight:bold">To get you as close as possible to the document you are seeking, an intelligent search has been implemented and will be executed when you click the Continue button located below. The search will very likely yield the desired document at or near the top of the returned list of documents.</p> <p style="font-weight:bold">Please take the time to re-bookmark the page once you find it on the new site.</p> <p style="font-weight:bold">For a video introduction to the new site, visit the <a id="homeLink" href="">home page</a> and watch the “Learn about the new Support site” video.</p><p style="text-align:center"><a id="searchLink" href=""><span class="bcls-button">Continue</span></a></p></div>';
 
     function injectMessage(el) {
-        el.insertAdjacentHTML('afterbegin', message);
+        el.insertAdjacentHTML('afterend', message);
         homeLink = document.getElementById('homeLink');
         searchLink = document.getElementById('searchLink');
         homeLink.setAttribute('href', newSite);
