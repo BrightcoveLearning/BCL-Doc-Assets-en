e849702b3f22436a28eb4e08fd25f34ab4554711
Robert Crooks <rcrooks@brightcove.com>
2017-09-01 07:59:57 -0400

    asset file

---
+++ doc-assets/node/17931-dynamic-ingest-media-sharing/curl-share.sh
@@ -0,0 +1,4 @@
+curl --request POST \
+  --url https://cms.api.brightcove.com/v1/accounts/57838016001/video/5555086013001/shares \
+  --header 'content-type: application/x-www-form-urlencoded' \
+  --data '[{"id": "1752604059001"}]'
