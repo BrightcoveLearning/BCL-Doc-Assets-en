01797656a61763efed64b54d9c13c18ae8520a0b
Robert Crooks <rcrooks@brightcove.com>
2017-09-01 08:11:31 -0400

    another fix

--- doc-assets/node/17931-dynamic-ingest-media-sharing/curl-share.sh
+++ doc-assets/node/17931-dynamic-ingest-media-sharing/curl-share.sh
@@ -1,9 +1,4 @@
 curl --request POST \
-  --url https://cms.api.brightcove.com/v1/accounts/57838016001/videos/5555086013001/shares \
-  --header 'Authorization: Bearer ' \
-  --data '[{"id": "1752604059001"}]'
-
-  curl --request POST \
   --url 'https://cms.api.brightcove.com/v1/accounts/57838016001/videos/5555086013001/shares?=' \
   --header 'authorization: Bearer ACdSBb2p-m5VX8VvdQXocDjMQKOi3aLaXUsacoi81K7jPluq8yh0cTNolnXXZSV1OVr1o_Cbnq-m6SJ0g8MWRNXcbtl7Dta2drzgawcEPejhuKuSlB_IGD9DxIJi_ZQ03k8ooAYuFPNQzUs3QGTPJOa980Li8ksV8wUin9oeEQzDqsXBLBcJZZZLXxdp7DRP_EVqEwPPb46Mbpq8YUqtDh5aHEo1Bdi042pPwevuPFAvjZ9CGT1fxsLRNznETKZFnfzKmA0abWNKoz3uDrL7RoTFsuhdbzLnvECXYvbavDboQQIiX0A_gLkfuallXe6v5PmG1xrOrPIFFJ6Bb59vdW1_gFxo1ZjvfRmuxV1nSqUpt3YGo0gOwhArDOD1jofxP5OAjUt_TVa5atHbYK6QpLtJYqEtixdF7Gp6H6isrLYT4ZaDrGbtztFZexSs7ia_oj89GV0N2bnyUnUrjPRrvM41HvK9qzjzfoMcVPmDfeBGCxCUXbw_sKp2kjyK2sMlI3CruOEE3WlHQ7vRhrLnAV22NnU0RIfjUmt-JRlZ3OGgNLKCc2y7gWeyP9m_FLPdeUdGYWsvQk2VkLB3UcNZh9pPsq0z-_Au2jlkjzLO0MqWq-ptL8LQy1i33JA5EyqEdZNMIj9mswYPb8JeDerWUqLG8bj8lYU40C2Oun1p2XwxKofPhodbbThzn2v6o2_pmBwKN8qgVmTwwDFhChm3BfMEE6CSmRLxd3IsO_8YQANJ7BfTmU44qp4KEjE7bP3eV1qB1KdDzrwlCq_DTvOOod4keHbKj6KdvYEVl2WOr_skaElCA5egZ0E' \
   --header 'content-type: application/json' \
