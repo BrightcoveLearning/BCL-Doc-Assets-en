7892affc8d82b5dbaa316384e301526d51152b70
Robert Crooks <rcrooks@brightcove.com>
2017-11-01 15:12:41 -0400

    wip

--- doc-assets/node/18479-cms-api-sample-add-multiple-affiliate-accounts-media-sharing/add-affiliates.js
+++ doc-assets/node/18479-cms-api-sample-add-multiple-affiliate-accounts-media-sharing/add-affiliates.js
@@ -1,6 +1,8 @@
 var BCLS = (function(window, document) {
-    var
-    return {
-
-    }
+    var accountId = document.getElementById('accountId'),
+        clientId = document.getElementById('clientId'),
+        clientSecret = document.getElementById('clientSecret'),
+        affiliateId = document.getElementById('affiliateId'),
+        addAffiliateId = document.getElementById('addAffiliateId'),
+        affiliateIds = document.getElementById('affiliateIds'),
 })(window, document);
