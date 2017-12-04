da7d21c6dcc662ac17b262633f9907a5e543f8a0
Robert Crooks <rcrooks@brightcove.com>
2017-12-04 10:48:15 -0500

    helper script

---
+++ doc-assets/node/18502-cae-fast-publish/boltrenditions-list.js
@@ -0,0 +1,35 @@
+var renditionlist = [
+		{
+			"id": "default/video900",
+		},
+		{
+			"id": "default/video700",
+		},
+		{
+			"id": "default/video6000",
+		},
+		{
+			"id": "default/video450",
+		},
+		{
+			"id": "default/video4000",
+		},
+		{
+			"id": "default/video3800",
+		},
+		{
+			"id": "default/video3500",
+		},
+		{
+			"id": "default/video2500",
+		},
+		{
+			"id": "default/video2000",
+		},
+		{
+			"id": "default/video1700",
+		},
+		{
+			"id": "default/video1200",
+		}
+	];
