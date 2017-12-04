fa4f7e968481f7a2b525f4c2296eb4d3404a6bd0
Robert Crooks <rcrooks@brightcove.com>
2017-12-04 11:41:02 -0500

    wip

--- doc-assets/node/18502-cae-fast-publish/boltrenditions-list.js
+++ doc-assets/node/18502-cae-fast-publish/boltrenditions-list.js
@@ -1,35 +1,34 @@
-var renditionlist = [
-		{
-			"id": "default/video900",
-		},
-		{
-			"id": "default/video700",
-		},
-		{
-			"id": "default/video6000",
-		},
-		{
-			"id": "default/video450",
-		},
-		{
-			"id": "default/video4000",
-		},
-		{
-			"id": "default/video3800",
-		},
-		{
-			"id": "default/video3500",
-		},
-		{
-			"id": "default/video2500",
-		},
-		{
-			"id": "default/video2000",
-		},
-		{
-			"id": "default/video1700",
-		},
-		{
-			"id": "default/video1200",
-		}
-	];
+var renditionlist = [{
+    "id": "default/video900",
+  },
+  {
+    "id": "default/video700",
+  },
+  {
+    "id": "default/video6000",
+  },
+  {
+    "id": "default/video450",
+  },
+  {
+    "id": "default/video4000",
+  },
+  {
+    "id": "default/video3800",
+  },
+  {
+    "id": "default/video3500",
+  },
+  {
+    "id": "default/video2500",
+  },
+  {
+    "id": "default/video2000",
+  },
+  {
+    "id": "default/video1700",
+  },
+  {
+    "id": "default/video1200",
+  }
+];
