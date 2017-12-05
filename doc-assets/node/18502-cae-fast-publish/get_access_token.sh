bc12f53d7b994ff916ca93460bbfcabf51019e20
Robert Crooks <rcrooks@brightcove.com>
2017-12-05 13:04:15 -0500

    asset file

---
+++ doc-assets/node/18502-cae-fast-publish/get_access_token.sh
@@ -0,0 +1,4 @@
+curl --request POST \
+  --url 'https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials' \
+  --header 'Authorization: Basic <span class="bcls-input">ENCODED_AUTHENTICATION_STRING</span>' \
+  --header 'Content-Type: application/x-www-form-urlencoded'
