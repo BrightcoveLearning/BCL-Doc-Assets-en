4acdcc855fd8f7fbe7fab67eef5520e3b58fd115
Robert Crooks <rcrooks@brightcove.com>
2017-12-06 10:13:27 -0500

    Merge branch 'master' of https://github.com/BrightcoveLearning/BCL-Doc-Assets

--- doc-assets/node/18502-cae-fast-publish/ingest-api-request.sh
+++ doc-assets/node/18502-cae-fast-publish/ingest-api-request.sh
@@ -3,7 +3,7 @@ curl --request POST \
   --header 'authorization: Bearer ALGRD4YMibquRwWhfgKxSLR2AbQXWsJ8J1EzzowufJCJnaiia9zjO4P_zqPcPeBbeuvOu3CjfIgx36sn1CAKlW3lua4i4VUZP8cZotNlNdWpD3Kbzu5NqS85SJBRon-jFmvT5ZjttoNm9drrAnIoVt7KjMuJHGuLfSm5UAlteiUWWB91S0OxyPh8Rfbs2NZONTUnuIntadGXfMu8mDouaH-AOCU1we2q7asEne3qMLX1lAlGmF2gg7-heqiS6oxUxM5CY7DfrQkri715lDSWq1TZfSgFflbHN9oZ0twSLxTZZPsXEkzgFWr7a7RUZUN6EYmWunlFarZ0h47UTbAknZKw7G9vpTbDkLv8mRbKLRhQlSPEuetsTEEdvCvfeVNEWGq2FZAUW7k-xOhH_jSf4fl92rHq1ZMDQEopOuBWOM3PHcLqi-WC0_b7xHLkD33krpndVKywJAiCFSsW5Lhd9O2pzwqmSMZ1PmnhXYo63QllsPsYOzP_oJ_59R2vhgjZU0dMowOl45l4xWlH_OV1dx6u3EQGEw_zncesr8fKx7SYU-zolbFGDLl3YZ3Q1hi6AnUFsBLboxZdGNw3gWRHklHgTnY2FnqZO4DN5qaCYWwxGx5HTXoI4JpdCA477buwu1ZwxLYqGKrcfLRtMzHx4Cr3RqH6MijOqPhSo04caiZ-jj5ViM1PZ5-lge8OOD3mhvREZ3S3srdA7P1MQOi8VM1kgC9XjmfHEJuKPAjqvLT_iOCut4q7FIXgVRJU2qQKlsmXBE51k8tCfvhEHb3LBxpMOoGzsj7q5XFDbt8QeSCW4shROPrwRpDopktPZo1qK2BEGiLa0PnS5okcV6s4-GIH1m5pfoK-iba1wPEBQHdWo2rotf9BV0g' \
   --header 'content-type: application/json' \
   --data '{
-  "name": "standard-dynamic-fast-publish",
+  "name": "multi-platform-standard-dynamic-fast-publish",
   "display_name": "Fast Publish Standard (CAE)",
   "description": "Deliver a wide range of content types across a variety of platforms on mobile and desktop.",
   "account_id": "57838016001",
@@ -54,5 +54,4 @@ curl --request POST \
       "select_baseline_profile_configuration": true
     }
   }
-}
-'
+}'
