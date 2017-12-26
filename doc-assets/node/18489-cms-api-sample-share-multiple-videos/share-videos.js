df5de15922e8454b7b2f1d423b64e10197fc7d20
Robert Crooks <rcrooks@brightcove.com>
2017-12-26 13:26:50 -0500

    wip

--- doc-assets/node/18489-cms-api-sample-share-multiple-videos/share-videos.js
+++ doc-assets/node/18489-cms-api-sample-share-multiple-videos/share-videos.js
@@ -166,7 +166,18 @@ var BCLS = (function(window, document) {
     for (i = 0; i < iMax; i += 1) {
       checkboxCollection[i].setAttribute('checked', 'checked');
     }
-    return targetArray;
+  }
+
+  /**
+   * deselects all checkboxes in a collection
+   * @param {htmlElementCollection} checkboxCollection a collection of the checkbox elements, usually gotten by document.getElementsByName()
+   */
+  function deselectAllCheckboxes(checkboxCollection) {
+    var i,
+      iMax = checkboxCollection.length;
+    for (i = 0; i < iMax; i += 1) {
+      checkboxCollection[i].removeAttribute('checked');
+    }
   }
 
   /**
@@ -278,9 +289,13 @@ var BCLS = (function(window, document) {
             fragment.appendChild(label);
             fragment.appendChild(br);
           }
+          // get references to checkboxes
           affiliatesBlock.appendChild(fragment);
           affiliatesCollection = document.getElementsByName('affiliatesChk');
           affiliatesSelectAll = document.getElementById('affiliatesChkAll');
+          // add event listener for select allows
+          affiliatesSelectAll.addEventListener('change', function() {})
+
         });
         break;
       case 'getVideos':
