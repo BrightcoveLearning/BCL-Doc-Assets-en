0bf1ca9e8db1ac83874932bd8d9e430d28a551ab
Robert Crooks <rcrooks@brightcove.com>
2018-01-15 12:24:30 -0500

    wip

---
+++ doc-assets/node/18528-quick-start-ingest-profiles-api-dynamic-delivery/ip-api-quick-start.js
@@ -0,0 +1,7 @@
+var BCLS = ( function (window, document) {
+  var account_id_input = document.getElementById('account_id_input'),
+    client_id_input = document.getElementById('client_id_input'),
+    client_secret_input = document.getElementById('client_secret_input'),
+    get_profiles = document.getElementById('get_profiles'),
+    create_profile = document.getElementById('create_profile')
+})(window, document);
