495020fcbe84836aa6559b9d08c6b254b00d8e8e
Robert Crooks <rcrooks@brightcove.com>
2018-01-16 10:03:31 -0500

    wip

--- doc-assets/node/18528-quick-start-ingest-profiles-api-dynamic-delivery/ip-api-quick-start.js
+++ doc-assets/node/18528-quick-start-ingest-profiles-api-dynamic-delivery/ip-api-quick-start.js
@@ -4,4 +4,75 @@ var BCLS = ( function (window, document) {
     client_secret_input = document.getElementById('client_secret_input'),
     get_profiles = document.getElementById('get_profiles'),
     create_profile = document.getElementById('create_profile')
+
+
+    /**
+     * createRequest sets up requests, send them to makeRequest(), and handles responses
+     * @param  {string} type the request type
+     */
+    function createRequest(type) {
+        var options   = {},
+            requestBody = {},
+            proxyURL = 'https:/solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy.php'
+            ipBaseURL = 'https://ingestion.api.brightcove.com/v1/accounts/' + account.value,
+            diBaseURL = 'https://ingest.api.brightcove.com/v1/accounts/' + account.value,
+            endpoint,
+            responseDecoded,
+            i,
+            iMax,
+            el,
+            txt;
+
+        // set credentials
+        options.client_id     = cid.value;
+        options.client_secret = secret.value;
+        options.proxyURL      = proxyURL;
+
+        switch (type) {
+            case 'getProfiles':
+                endpoint            = '/profiles';
+                options.url         = ipBaseURL + endpoint;
+                options.requestType = 'GET';
+                makeRequest(options, function(response) {
+                    responseDecoded = JSON.parse(response);
+                    if (Array.isArray(responseDecoded)) {
+                        // remove existing options
+                        iMax = profiles.length;
+                        for (i = 0; i < iMax; i++) {
+                            profiles.remove(i);
+                        }
+                        // add new options
+                        iMax = responseDecoded.length;
+                        for (i = 0; i < iMax; i++) {
+                            el = document.createElement('option');
+                            el.setAttribute('value', responseDecoded[i].name);
+                            txt = document.createTextNode(responseDecoded[i].name);
+                            el.appendChild(txt);
+                            profiles.appendChild(el);
+                        }
+                    }
+                });
+                break;
+            case: 'ingestVideo'
+                endpoint            = '/profiles';
+                options.url         = ipBaseURL + endpoint;
+                options.requestType = 'POST';;
+                requestBocy.master  = {};
+                requestBody.master.url = 'http://myvideos.com/foo.mp4';
+                // add more properties
+                options.requestBody = JSON.stringify(requestBody);
+                makeRequest(options, function(response) {
+                    responseDecoded = JSON.parse(response);
+                    // do more stuff
+                });
+                break;
+
+            // additional cases
+            default:
+                console.log('Should not be getting to the default case - bad request type sent');
+                break;
+        }
+    }
+
+
 })(window, document);
