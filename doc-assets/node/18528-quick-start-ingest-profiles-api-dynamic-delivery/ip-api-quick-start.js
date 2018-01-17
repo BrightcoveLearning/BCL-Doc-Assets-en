4b7859e10ae0ee8eef3b5f3b66ec59e554134610
Robert Crooks <rcrooks@brightcove.com>
2018-01-17 09:57:28 -0500

    wip

--- doc-assets/node/18528-quick-start-ingest-profiles-api-dynamic-delivery/ip-api-quick-start.js
+++ doc-assets/node/18528-quick-start-ingest-profiles-api-dynamic-delivery/ip-api-quick-start.js
@@ -69,6 +69,20 @@ var BCLS = (function(window, document) {
       return true;
   }
 
+  /*
+   * tests to see if a string is json
+   * @param {String} str string to test
+   * @return {Boolean}
+   */
+  function isJson(str) {
+      try {
+          JSON.parse(str);
+      } catch (e) {
+          return false;
+      }
+      return true;
+  }
+
   /**
    * get selected value for single select element
    * @param {htmlElement} e the select element
