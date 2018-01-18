2bc695b1b94b9852d73dac9dd4e033cf3fbbadd4
Robert Crooks <rcrooks@brightcove.com>
2018-01-18 07:50:25 -0500

    add default account id

--- doc-assets/node/18528-quick-start-ingest-profiles-api-dynamic-delivery/ip-api-quick-start.js
+++ doc-assets/node/18528-quick-start-ingest-profiles-api-dynamic-delivery/ip-api-quick-start.js
@@ -64,7 +64,7 @@ var BCLS = (function(window, document) {
    * get account info from input fields
    */
   function getAccountInfo() {
-    account_id    = account_id_input.value;
+    account_id    = (isDefined(account_id_input.value)) ? account_id_input.value : '57838016001';
     client_id     = client_id_input.value;
     client_secret = client_secret_input.value;
   }
