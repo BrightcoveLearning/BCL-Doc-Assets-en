674a3bf27d231d3f98494976f2fec5223bacdb79
Robert Crooks <rcrooks@brightcove.com>
2018-01-30 13:37:59 -0800

    wip

--- doc-assets/apidoc/utils/fix-nav.js
+++ doc-assets/apidoc/utils/fix-nav.js
@@ -2,12 +2,23 @@ var BCLS_fix_nav = ( function (window, document) {
   var block_inpagenavigationblock = document.getElementById('block-inpagenavigationblock'),
     sidenav = document.getElementById('sidenav');
 
-    function removeElement(elementId) {
-    // Removes an element from the document
-    var element = document.getElementById(elementId);
-    element.parentNode.removeChild(element);
-}
+  /**
+   * remove an HTML element from a doc
+   * @param  {string} elementId the element's id
+   * @return {[type]}           [description]
+   */
+  function removeElement(elementId) {
+    if (elementId) {
+      var element = document.getElementById(elementId);
+      element.parentNode.removeChild(element);
+      return true;
+    } else {
+      console.log('removeElement: no element id passed');
+      return false;
+    }
+  }
 
     block_inpagenavigationblock.innerHTML = sidenav.innerHTML;
+    removeElement('sidenav');
 
 })(window, document);
