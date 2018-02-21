d493ae1589e94276c7c4b02f7c1444ad4b968cf1
Robert Crooks <rcrooks@brightcove.com>
2018-02-21 11:36:07 -0500

    new make request function

--- doc-assets/node/18029-update-geo-properties-sample/codepen-source.js
+++ doc-assets/node/18029-update-geo-properties-sample/codepen-source.js
@@ -230,80 +230,55 @@ var BCLS = (function(window, document) {
 
     /**
      * send API request to the proxy
-     * @param  {Object} options options for the request
-     * @param  {String} requestID the type of request = id of the button
+     * @param  {Object} options for the request
+     * @param  {String} options.url the full API request URL
+     * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+     * @param  {String} options.proxyURL proxyURL to send the request to
+     * @param  {String} options.client_id client id for the account (default is in the proxy)
+     * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+     * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+     * @param  {Function} [callback] callback function that will process the response
      */
-    function makeRequest(options, requestID) {
-        var httpRequest = new XMLHttpRequest(),
-            responseRaw,
-            parsedData,
-            requestParams,
-            dataString,
-            // response handler
-            getResponse = function() {
-                try {
-                  if (httpRequest.readyState === 4) {
-                    if (httpRequest.status === 200) {
-                      // check for completion
-                      if (requestID === 'getCount') {
-                        if (httpRequest.responseText === '[]') {
-                            // no video returned
-                            alert('no video returned');
-                        }
-                        responseRaw = httpRequest.responseText;
-                        responseData.textContent = responseRaw;
-                        parsedData = JSON.parse(responseRaw);
-                        // set total videos
-                        videoCount = parsedData.count;
-                        count.textContent = 'Total videos: ' + videoCount;
-                        createRequest('get1video');
-                      } else if (requestID === 'get1video') {
-                        responseRaw = httpRequest.responseText;
-                        responseData.textContent = responseRaw;
-                        parsedData = JSON.parse(responseRaw);
-                        // set the video id for the update
-                        video_id = parsedData[0].id;
-                        video_name = parsedData[0].name;
-                        logMessage(logger, ('Processing ' + video_name), true);
-                        createRequest('updateVideos');
-                      } else if (requestID === 'updateVideos') {
-                        responseRaw = httpRequest.responseText;
-                        responseData.textContent = responseRaw;
-                        // increment offset
-                        offset++;
-                        if (offset < videoCount) {
-                            // move on to next video
-                            createRequest('get1video');
-                        } else {
-                            // we are done
-                            logMessage(logger, ('Finished... ' + offset + ' videos processed'), true);
-                        }
-
-                      }
-                    } else {
-                      alert('There was a problem with the request. Request returned ' + httpRequest.status);
-                    }
-                  }
-                } catch (e) {
-                  alert('Caught Exception: ' + e);
+    function makeRequest(options, callback) {
+      var httpRequest = new XMLHttpRequest(),
+        response,
+        requestParams,
+        dataString,
+        proxyURL = options.proxyURL,
+        // response handler
+        getResponse = function() {
+          try {
+            if (httpRequest.readyState === 4) {
+              if (httpRequest.status >= 200 && httpRequest.status < 300) {
+                response = httpRequest.responseText;
+                // some API requests return '{null}' for empty responses - breaks JSON.parse
+                if (response === '{null}') {
+                  response = null;
                 }
-            };
-        // set up request data
-        requestParams = "url=" + encodeURIComponent(options.url) + "&requestType=" + options.requestType;
-        if (options.requestBody) {
-            dataString = JSON.stringify(options.requestBody);
-            requestParams += "&requestBody=" + encodeURIComponent(dataString);
-        }
-
-        // set response handler
-        httpRequest.onreadystatechange = getResponse;
-        // open the request
-        httpRequest.open('POST', proxyURL);
-        // set headers
-        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        // open and send request
-        httpRequest.send(requestParams);
+                // return the response
+                callback(response);
+              } else {
+                alert('There was a problem with the request. Request returned ' + httpRequest.status);
+              }
+            }
+          } catch (e) {
+            alert('Caught Exception: ' + e);
+          }
+        };
+      /**
+       * set up request data
+       * the proxy used here takes the following request body:
+       * JSON.stringify(options)
+       */
+      // set response handler
+      httpRequest.onreadystatechange = getResponse;
+      // open the request
+      httpRequest.open('POST', proxyURL);
+      // set headers if there is a set header line, remove it
+      // open and send request
+      httpRequest.send(JSON.stringify(options));
     }
+
     // event listeners
     updateVideos.addEventListener('click', function() {
         createRequest('getCount');
