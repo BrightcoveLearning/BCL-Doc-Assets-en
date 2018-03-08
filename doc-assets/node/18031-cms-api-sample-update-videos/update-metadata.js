718ea5d3ad1625e5f3c92dd7072bf377a5b33a7c
Robert Crooks <rcrooks@brightcove.com>
2018-03-08 11:15:06 -0500

    refactor script

--- doc-assets/node/18031-cms-api-sample-update-videos/update-metadata.js
+++ doc-assets/node/18031-cms-api-sample-update-videos/update-metadata.js
@@ -81,49 +81,58 @@ var BCLS = (function(window, document, creds) {
     }
 
   };
-  // function to set the request
-  logResponse = function(data) {
-    response.innerHTML += data + ",<br>";
-  };
-  // function to make API request
-  function makeRequest() {
+
+  /**
+   * send API request to the proxy
+   * @param  {Object} options for the request
+   * @param  {String} options.url the full API request URL
+   * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+   * @param  {String} options.proxyURL proxyURL to send the request to
+   * @param  {String} options.client_id client id for the account (default is in the proxy)
+   * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+   * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+   * @param  {Function} [callback] callback function that will process the response
+   */
+  function makeRequest(options, callback) {
     var httpRequest = new XMLHttpRequest(),
-      proxyURL = "https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy.php",
-      requestData,
+      response,
+      requestParams,
+      dataString,
+      proxyURL = options.proxyURL,
+      // response handler
       getResponse = function() {
         try {
           if (httpRequest.readyState === 4) {
             if (httpRequest.status >= 200 && httpRequest.status < 300) {
-              logResponse(httpRequest.responseText);
-              callNumber++;
-              if (callNumber < totalVideos) {
-                setRequest();
+              response = httpRequest.responseText;
+              // some API requests return '{null}' for empty responses - breaks JSON.parse
+              if (response === '{null}') {
+                response = null;
               }
+              // return the response
+              callback(response);
             } else {
-              alert("There was a problem with the request. Request returned " + httpRequest.status);
+              alert('There was a problem with the request. Request returned ' + httpRequest.status);
             }
           }
         } catch (e) {
           alert('Caught Exception: ' + e);
         }
       };
-    if (account_id === null || client_id === null || client_secret === null) {
-      alert("You must provide an account id, client id, and client secret");
-      return false;
-    }
-    // get latest request URL in case use edited it
-    apiRequest = cleanString(apiRequest_field.value);
-    // set up request data
-    requestData = "client_id=" + client_id + "&client_secret=" + client_secret + "&url=" + encodeURIComponent(apiRequest) + "&requestBody=" + encodeURIComponent(requestBody) + "&requestType=PATCH";
+    /**
+     * set up request data
+     * the proxy used here takes the following request body:
+     * JSON.stringify(options)
+     */
     // set response handler
     httpRequest.onreadystatechange = getResponse;
     // open the request
-    httpRequest.open("POST", proxyURL);
-    // set headers
-    httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    httpRequest.open('POST', proxyURL);
+    // set headers if there is a set header line, remove it
     // open and send request
-    httpRequest.send(requestData);
-  };
+    httpRequest.send(JSON.stringify(options));
+  }
+
   // init function to set up event listeners
   function init() {
     // event listeners
