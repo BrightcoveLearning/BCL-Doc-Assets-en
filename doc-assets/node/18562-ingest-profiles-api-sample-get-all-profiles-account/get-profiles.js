d7919040325cb5a598dec10e455c118b11cd986e
Robert Crooks <rcrooks@brightcove.com>
2018-03-13 16:25:50 -0400

    another fix

--- doc-assets/node/18562-ingest-profiles-api-sample-get-all-profiles-account/get-profiles.js
+++ doc-assets/node/18562-ingest-profiles-api-sample-get-all-profiles-account/get-profiles.js
@@ -28,7 +28,7 @@ var BCLS = (function(window, document) {
     createRequest('get_profiles');
   });
 
-  iMax = listFilters.length;
+  iMax = list_filters.length;
   for (i = 0; i < iMax; i++) {
     listFilters[i].addEventListener('change', function() {
       filterProfiles(getRadioValue(list_filters));
