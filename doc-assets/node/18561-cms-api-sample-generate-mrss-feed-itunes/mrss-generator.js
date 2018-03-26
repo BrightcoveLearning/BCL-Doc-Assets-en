1dbd5fccd79ea02a306000edd56648732a23af93
Robert Crooks <rcrooks@brightcove.com>
2018-03-26 09:03:42 -0400

    wip

--- doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/mrss-generator.js
+++ doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/mrss-generator.js
@@ -199,6 +199,54 @@ var BCLS = ( function (window, document) {
         }
     }
 
+    /**
+     * get array of values for checked boxes in a collection
+     * @param {htmlElementCollection} checkBoxCollection collection of checkbox elements
+     * @return {Array} array of the values of the checked boxes
+     */
+    function getCheckedBoxValues(checkBoxCollection) {
+      var checkedValues = [],
+        i,
+        iMax;
+      if (checkBoxCollection) {
+        iMax = checkBoxCollection.length;
+        for (i = 0; i < iMax; i++) {
+          if (checkBoxCollection[i].checked === true) {
+            checkedValues.push(checkBoxCollection[i].value);
+          }
+        }
+        return checkedValues;
+      } else {
+        console.log('Error: no input received');
+        return null;
+      }
+    }
+
+    /**
+     * selects all checkboxes in a collection
+     * @param {htmlElementCollection} checkboxCollection a collection of the checkbox elements, usually gotten by document.getElementsByName()
+     */
+    function selectAllCheckboxes(checkboxCollection) {
+      var i,
+        iMax = checkboxCollection.length;
+      for (i = 0; i < iMax; i += 1) {
+        checkboxCollection[i].setAttribute('checked', 'checked');
+      }
+    }
+
+    /**
+     * deselects all checkboxes in a collection
+     * @param {htmlElementCollection} checkboxCollection a collection of the checkbox elements, usually gotten by document.getElementsByName()
+     */
+    function deselectAllCheckboxes(checkboxCollection) {
+      var i,
+        iMax = checkboxCollection.length;
+      for (i = 0; i < iMax; i += 1) {
+        checkboxCollection[i].removeAttribute('checked');
+      }
+    }
+
+
     function addItems() {
         var i, iMax, video, pubdate, eItem, videoURL, thumbnailURL, doThumbnail = true;
         if (videosArray.length > 0) {
