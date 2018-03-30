742c054775655cda6e27e4618c460accc513cc8d
Robert Crooks <rcrooks@brightcove.com>
2018-03-30 10:07:19 -0400

    wip itunes

--- doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/mrss-generator.js
+++ doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/mrss-generator.js
@@ -220,7 +220,7 @@ var BCLS = ( function (window, document) {
     for (i = 0; i < iMax; i++) {
       index = findObjectInArray(all_videos, 'id', checkedBoxes[i]);
       videos.push(all_videos[index]);
-      sortArray(videos, 'published_at')
+      sortArray(videos, 'published_at');
     }
   }
 
