8dafdaab02d50278810789bef26beaa1a1880261
Robert Crooks <rcrooks@brightcove.com>
2018-04-04 09:38:38 -0400

    finish refactor

--- doc-assets/node/18499-quick-start-dynamic-ingest-api-dynamic-delivery/quick-start.js
+++ doc-assets/node/18499-quick-start-dynamic-ingest-api-dynamic-delivery/quick-start.js
@@ -180,33 +180,34 @@ var BCLS = (function(window, document) {
           break;
       }
     }
+
     /**
      * send API request to the proxy
-     * @param  {Object} options options for the request
-     * @param  {String} requestID the type of request = id of the button
+     * @param  {Object} options for the request
+     * @param  {String} options.url the full API request URL
+     * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+     * @param  {String} options.proxyURL proxyURL to send the request to
+     * @param  {String} options.client_id client id for the account (default is in the proxy)
+     * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+     * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+     * @param  {Function} [callback] callback function that will process the response
      */
-  function makeRequest(options, requestID) {
+    function makeRequest(options, callback) {
       var httpRequest = new XMLHttpRequest(),
-        responseRaw,
-        parsedData,
-        requestParams,
-        dataString,
+        response,
+        proxyURL = options.proxyURL,
         // response handler
         getResponse = function() {
           try {
             if (httpRequest.readyState === 4) {
-              if (httpRequest.status === 200) {
-                console.log(httpRequest.responseText);
-                responseRaw = httpRequest.responseText;
-                responseData.textContent = responseRaw;
-                parsedData = JSON.parse(responseRaw);
-                // save new ids on create requests
-                if (requestID === 'createVideo') {
-                  newVideo_id = parsedData.id;
+              if (httpRequest.status >= 200 && httpRequest.status < 300) {
+                response = httpRequest.responseText;
+                // some API requests return '{null}' for empty responses - breaks JSON.parse
+                if (response === '{null}') {
+                  response = null;
                 }
-                responseData.textContent = JSON.stringify(parsedData, null, '  ');
-                // re-enable the buttons
-                enableButtons();
+                // return the response
+                callback(response);
               } else {
                 alert('There was a problem with the request. Request returned ' + httpRequest.status);
               }
@@ -215,22 +216,20 @@ var BCLS = (function(window, document) {
             alert('Caught Exception: ' + e);
           }
         };
-      // set up request data
-      requestParams = 'url=' + encodeURIComponent(options.url) + '&requestType=' + options.requestType;
-      if (options.requestBody) {
-        dataString = JSON.stringify(options.requestBody);
-        requestParams += '&requestBody=' + encodeURIComponent(dataString);
-      }
-      console.log(requestParams);
+      /**
+       * set up request data
+       * the proxy used here takes the following request body:
+       * JSON.stringify(options)
+       */
       // set response handler
       httpRequest.onreadystatechange = getResponse;
       // open the request
       httpRequest.open('POST', proxyURL);
-      // set headers
-      httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      // set headers if there is a set header line, remove it
       // open and send request
-      httpRequest.send(requestParams);
+      httpRequest.send(JSON.stringify(options));
     }
+
     // event listeners
   createVideo.addEventListener('click', function() {
     createRequest('createVideo');
