e2bcb1fcea8dd11f5ceb51a90359ac522576052d
rcrooks <rcrooks@brightcove.com>
2018-04-11 15:01:02 -0400

    wip update sample

--- doc-assets/node/17992-analytics-api-sample-video-report-reference-id/video-report-by-reference-id.js
+++ doc-assets/node/17992-analytics-api-sample-video-report-reference-id/video-report-by-reference-id.js
@@ -105,35 +105,52 @@ function buildRequest() {
 
 /**
  * send API request to the proxy
- * @param  {Object} requestData options for the request
- * @param  {Function} callback the callback function to invoke
+ * @param  {Object} options for the request
+ * @param  {String} options.url the full API request URL
+ * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+ * @param  {String} options.proxyURL proxyURL to send the request to
+ * @param  {String} options.client_id client id for the account (default is in the proxy)
+ * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+ * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+ * @param  {Function} [callback] callback function that will process the response
  */
-function getData(options, callback) {
-    var httpRequest = new XMLHttpRequest(),
-        parsedData,
-        requestParams,
-        dataString,
-        // response handler
-        getResponse = function() {
-            try {
-              if (httpRequest.readyState === 4) {
-                if (httpRequest.status >= 200 && httpRequest.status < 300) {
-                  parsedData = JSON.parse(httpRequest.responseText);
-                  callback(parsedData);
-                } else {
-                  alert('There was a problem with the request. Request returned ' + httpRequest.status);
-                }
-              }
-            } catch (e) {
-              alert('Caught Exception: ' + e);
+function makeRequest(options, callback) {
+  var httpRequest = new XMLHttpRequest(),
+    response,
+    proxyURL = options.proxyURL,
+    // response handler
+    getResponse = function() {
+      try {
+        if (httpRequest.readyState === 4) {
+          if (httpRequest.status >= 200 && httpRequest.status < 300) {
+            response = httpRequest.responseText;
+            // some API requests return '{null}' for empty responses - breaks JSON.parse
+            if (response === '{null}') {
+              response = null;
             }
-        };
-        // set up request data
-    requestParams = 'url=' + encodeURIComponent(options.url) + '&requestType=GET';
-    if (options.client_id && options.client_secret) {
-        requestParams += '&client_id=' + options.client_id + '&client_secret=' + options.client_secret;
-    }
-
+            // return the response
+            callback(response);
+          } else {
+            alert('There was a problem with the request. Request returned ' + httpRequest.status);
+          }
+        }
+      } catch (e) {
+        alert('Caught Exception: ' + e);
+      }
+    };
+  /**
+   * set up request data
+   * the proxy used here takes the following request body:
+   * JSON.stringify(options)
+   */
+  // set response handler
+  httpRequest.onreadystatechange = getResponse;
+  // open the request
+  httpRequest.open('POST', proxyURL);
+  // set headers if there is a set header line, remove it
+  // open and send request
+  httpRequest.send(JSON.stringify(options));
+}
     // set response handler
     httpRequest.onreadystatechange = getResponse;
     // open the request
