aa52e2d2b69a4d3a88271afd27e0fdd1a93f4118
rcrooks <rcrooks@brightcove.com>
2018-04-11 15:42:12 -0400

    wip updating script for new proxy

--- doc-assets/node/17993-analytics-api-sample-video-report-tags/analytics-by-tag.js
+++ doc-assets/node/17993-analytics-api-sample-video-report-tags/analytics-by-tag.js
@@ -30,7 +30,7 @@ var BCLS = (function(window, document, Pikaday) {
     requestURL = '',
     endDate = '',
     startDate = '',
-    requestOptions = {},
+    options = {},
     i,
     iMax;
 
@@ -58,70 +58,79 @@ var BCLS = (function(window, document, Pikaday) {
     tags = (isDefined($tags.value)) ? $tags.value : tags;
     // add client credentials if any
     if (isDefined($client_id.value)) {
-        requestOptions.client_id = $client_id.value;
+      options.client_id = $client_id.value;
     }
     if (isDefined($client_secret.value)) {
-        requestOptions.client_secret = $client_secret.value;
+      options.client_secret = $client_secret.value;
     }
-
+    options.account_id = account_id;
+    options.proxyURL = proxyURL;
     // build the request
-    requestOptions.url = 'https://analytics.api.brightcove.com/v1';
-    requestOptions.url += '/data?accounts=' + account_id + '&dimensions=video';
+    options.url = 'https://analytics.api.brightcove.com/v1';
+    options.url += '/data?accounts=' + account_id + '&dimensions=video';
     // check for time filters
     startDate = from.value || fromISO;
-      // startDate = fromPicker.toString('YYYY-MM-DD');
-      requestOptions.url += '&from=' + startDate;
+    // startDate = fromPicker.toString('YYYY-MM-DD');
+    options.url += '&from=' + startDate;
     endDate = to.value || nowISO;
-      // endDate = toPicker.toString('YYYY-MM-DD');
-      requestOptions.url += '&to=' + endDate;
+    // endDate = toPicker.toString('YYYY-MM-DD');
+    options.url += '&to=' + endDate;
 
     // add limit and fields
-    requestOptions.url += '&limit=all&fields=engagement_score,play_rate,video,video_duration,video_engagement_1,video_engagement_100,video_engagement_25,video_engagement_50,video_engagement_75,video_impression,video_name,video_percent_viewed,video_seconds_viewed,video_view,video.tags';
+    options.url += '&limit=all&fields=engagement_score,play_rate,video,video_duration,video_engagement_1,video_engagement_100,video_engagement_25,video_engagement_50,video_engagement_75,video_impression,video_name,video_percent_viewed,video_seconds_viewed,video_view,video.tags';
     // add ref id filter
-    requestOptions.url += '&where=video.q==tags:' + removeSpaces(tags);
-    $request.textContent = requestOptions.url;
-    $request.setAttribute('value', requestOptions.url);
+    options.url += '&where=video.q==tags:' + removeSpaces(tags);
+    $request.textContent = options.url;
+    $request.setAttribute('value', options.url);
   }
 
   /**
    * send API request to the proxy
-   * @param  {Object} requestData options for the request
-   * @param  {Function} callback the callback function to invoke
+   * @param  {Object} options for the request
+   * @param  {String} options.url the full API request URL
+   * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+   * @param  {String} options.proxyURL proxyURL to send the request to
+   * @param  {String} options.client_id client id for the account (default is in the proxy)
+   * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+   * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+   * @param  {Function} [callback] callback function that will process the response
    */
-  function getData(options, callback) {
-      var httpRequest = new XMLHttpRequest(),
-          parsedData,
-          requestParams,
-          dataString,
-          // response handler
-          getResponse = function() {
-              try {
-                if (httpRequest.readyState === 4) {
-                  if (httpRequest.status >= 200 && httpRequest.status < 300) {
-                    parsedData = JSON.parse(httpRequest.responseText);
-                    callback(parsedData);
-                  } else {
-                    alert('There was a problem with the request. Request returned ' + httpRequest.status);
-                  }
-                }
-              } catch (e) {
-                alert('Caught Exception: ' + e);
+  function makeRequest(options, callback) {
+    var httpRequest = new XMLHttpRequest(),
+      response,
+      proxyURL = options.proxyURL,
+      // response handler
+      getResponse = function() {
+        try {
+          if (httpRequest.readyState === 4) {
+            if (httpRequest.status >= 200 && httpRequest.status < 300) {
+              response = httpRequest.responseText;
+              // some API requests return '{null}' for empty responses - breaks JSON.parse
+              if (response === '{null}') {
+                response = null;
               }
-          };
-          // set up request data
-      requestParams = 'url=' + encodeURIComponent(options.url) + '&requestType=GET';
-      if (options.client_id && options.client_secret) {
-          requestParams += '&client_id=' + options.client_id + '&client_secret=' + options.client_secret;
-      }
-
-      // set response handler
-      httpRequest.onreadystatechange = getResponse;
-      // open the request
-      httpRequest.open('POST', proxyURL);
-      // set headers
-      httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      // open and send request
-      httpRequest.send(requestParams);
+              // return the response
+              callback(response);
+            } else {
+              alert('There was a problem with the request. Request returned ' + httpRequest.status);
+            }
+          }
+        } catch (e) {
+          alert('Caught Exception: ' + e);
+        }
+      };
+    /**
+     * set up request data
+     * the proxy used here takes the following request body:
+     * JSON.stringify(options)
+     */
+    // set response handler
+    httpRequest.onreadystatechange = getResponse;
+    // open the request
+    httpRequest.open('POST', proxyURL);
+    // set headers if there is a set header line, remove it
+    // open and send request
+    httpRequest.send(JSON.stringify(options));
   }
 
   // add date pickers to the date input fields
@@ -142,27 +151,28 @@ var BCLS = (function(window, document, Pikaday) {
   from.value = fromISO;
 
   // set event listeners
-  useMyAccount.addEventListener('click', function () {
-      if (basicInfo.getAttribute('style') === 'display:none;') {
-          basicInfo.setAttribute('style', 'display:block;');
-          useMyAccount.innerHTML = 'Use Sample Account';
-      } else {
-          basicInfo.setAttribute('style', 'display:none;');
-          useMyAccount.textContent = 'Use My Account Instead';
-      }
+  useMyAccount.addEventListener('click', function() {
+    if (basicInfo.getAttribute('style') === 'display:none;') {
+      basicInfo.setAttribute('style', 'display:block;');
+      useMyAccount.innerHTML = 'Use Sample Account';
+    } else {
+      basicInfo.setAttribute('style', 'display:none;');
+      useMyAccount.textContent = 'Use My Account Instead';
+    }
   });
 
 
   // set listener for form fields
   iMax = $requestInputs.length;
   for (i = 0; i < iMax; i++) {
-      $requestInputs[i].addEventListener('change', buildRequest);
+    $requestInputs[i].addEventListener('change', buildRequest);
   }
   // send request
-  $submitButton.addEventListener('click', function(){
-      getData(requestOptions, function(response) {
-          responseFrame.textContent = JSON.stringify(response, null, '  ');
-      });
+  $submitButton.addEventListener('click', function() {
+    makeRequest(options, function(response) {
+      response = JSON.parse(response);
+      responseFrame.textContent = JSON.stringify(response, null, '  ');
+    });
   });
   // select all the data
   $selectData.addEventListener('click', function() {
