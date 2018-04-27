79e5c32a3d1eed78d524dedd42f3bb6afd242c87
rcrooks <rcrooks@brightcove.com>
2018-04-27 15:59:45 -0400

    update to v2 proxy

--- doc-assets/node/18531-ingest-profiles-api-sample-set-default-profile-multiple-accounts-dynamic-delivery/set-default-profile.js
+++ doc-assets/node/18531-ingest-profiles-api-sample-set-default-profile-multiple-accounts-dynamic-delivery/set-default-profile.js
@@ -164,7 +164,7 @@ var BCLS = (function(window, document) {
   function createRequest(type) {
     var options = {},
       requestBody = {},
-      proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/ip2-proxy.php',
+      proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/brightcove-learning-proxy-v2.php',
       baseURL = 'https://ingestion.api.brightcove.com/v1/accounts/' + account_id,
       endpoint,
       responseDecoded,
@@ -174,8 +174,11 @@ var BCLS = (function(window, document) {
       iMax;
 
     // set credentials
-    options.client_id = client_id;
-    options.client_secret = client_secret;
+    if (isDefined(client_id) && isDefined(client_secret)) {
+      options.client_id = client_id;
+      options.client_secret = client_secret;
+    }
+    options.account_id = account_id;
     options.proxyURL = proxyURL;
 
     switch (type) {
@@ -273,57 +276,41 @@ var BCLS = (function(window, document) {
    * @param  {Function} [callback] callback function that will process the response
    */
   function makeRequest(options, callback) {
-      var httpRequest = new XMLHttpRequest(),
-          response,
-          requestParams,
-          dataString,
-          proxyURL    = options.proxyURL,
-          // response handler
-          getResponse = function() {
-              try {
-                  if (httpRequest.readyState === 4) {
-                      if (httpRequest.status >= 200 && httpRequest.status < 300) {
-                          response = httpRequest.responseText;
-                          // some API requests return '{null}' for empty responses - breaks JSON.parse
-                          if (response === '{null}') {
-                              response = null;
-                          }
-                          // return the response
-                          callback(response);
-                      } else {
-                          alert('There was a problem with the request. Request returned ' + httpRequest.status);
-                      }
-                  }
-              } catch (e) {
-                  alert('Caught Exception: ' + e);
+    var httpRequest = new XMLHttpRequest(),
+      response,
+      proxyURL = options.proxyURL,
+      // response handler
+      getResponse = function() {
+        try {
+          if (httpRequest.readyState === 4) {
+            if (httpRequest.status >= 200 && httpRequest.status < 300) {
+              response = httpRequest.responseText;
+              // some API requests return '{null}' for empty responses - breaks JSON.parse
+              if (response === '{null}') {
+                response = null;
               }
-          };
-      /**
-       * set up request data
-       * the proxy used here takes the following params:
-       * url - the full API request (required)
-       * requestType - the HTTP request type (default: GET)
-       * clientId - the client id (defaults here to a Brightcove sample account value - this should always be stored on the server side if possible)
-       * clientSecret - the client secret (defaults here to a Brightcove sample account value - this should always be stored on the server side if possible)
-       * requestBody - request body for write requests (optional JSON string)
-       */
-      requestParams = "url=" + encodeURIComponent(options.url) + "&requestType=" + options.requestType;
-      // only add client id and secret if both were submitted
-      if (options.client_id && options.client_secret) {
-          requestParams += '&client_id=' + options.client_id + '&client_secret=' + options.client_secret;
-      }
-      // add request data if any
-      if (options.requestBody) {
-          requestParams += '&requestBody=' + options.requestBody;
-      }
-      // set response handler
-      httpRequest.onreadystatechange = getResponse;
-      // open the request
-      httpRequest.open('POST', proxyURL);
-      // set headers
-      httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      // open and send request
-      httpRequest.send(requestParams);
+              // return the response
+              callback(response);
+            } else {
+              alert('There was a problem with the request. Request returned ' + httpRequest.status);
+            }
+          }
+        } catch (e) {
+          alert('Caught Exception: ' + e);
+        }
+      };
+    /**
+     * set up request data
+     * the proxy used here takes the following request body:
+     * JSON.stringify(options)
+     */
+    // set response handler
+    httpRequest.onreadystatechange = getResponse;
+    // open the request
+    httpRequest.open('POST', proxyURL);
+    // set headers if there is a set header line, remove it
+    // open and send request
+    httpRequest.send(JSON.stringify(options));
   }
 
 })(window, document);
