814eb2f87a857fee50fcb08780115d6ad961c719
rcrooks <rcrooks@brightcove.com>
2018-05-09 12:59:24 -0400

    wip

--- doc-assets/node/18598-dynamic-ingest-api-sample-ingesting-low-priority-videos-dynamic-delivery/ingest.js
+++ doc-assets/node/18598-dynamic-ingest-api-sample-ingesting-low-priority-videos-dynamic-delivery/ingest.js
@@ -10,7 +10,6 @@ var BCLS = (function(window, document) {
     ingest_profile,
     custom_profile_display = document.getElementById('custom_profile_display'),
     cms_url_display = document.getElementById('cms_url'),
-    cmsURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy.php',
     videoDataDisplay = document.getElementById('videoData'),
     // Dynamic Ingest API stuff
     profilesArray = ['multi-platform-extended-static', 'multi-platform-standard-static'],
@@ -18,17 +17,15 @@ var BCLS = (function(window, document) {
     di_submit_display = document.getElementById('di_Submit'),
     diURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/brightcove-learning-proxy-v2.php',
     response = document.getElementById('response'),
+    current_profiles = [],
+    rawVideoData = [],
     videoData = [],
     totalVideos,
     videoNumber = 0,
     currentJobs = 0,
-    t1,
-    t2,
     totalIngested = 0,
     defaults = {
-      account_id: 57838016001,
-      client_id: '37cd3c5d-6f18-4702-bfb6-4fbc1cd095f1',
-      client_secret: 'gLSQANqe6A2PzJce_6xA4bTNu844up5-CSrC-jxNfur4EaOgWKRcqq_GTxKjhMpPSflMdNEhFdBmNe0qsTIZSQ'
+      account_id: 57838016001
     },
     callbacks = ["http://solutions.brightcove.com/bcls/dynamic-delivery/di-callbacks.php"];
 
@@ -40,14 +37,137 @@ var BCLS = (function(window, document) {
     }
     return true;
   }
-  // set options for the CMS API request
-  function setCMSOptions() {
+
+  /**
+   * determines whether specified item is in an array
+   *
+   * @param {array} array to check
+   * @param {string} item to check for
+   * @return {boolean} true if item is in the array, else false
+   */
+  function arrayContains(arr, item) {
+      var i,
+          iMax = arr.length;
+      for (i = 0; i < iMax; i++) {
+          if (arr[i] === item) {
+              return true;
+          }
+      }
+      return false;
+  }
+
+  /**
+   * find index of an object in array of objects
+   * based on some property value
+   *
+   * @param {array} targetArray array to search
+   * @param {string} objProperty object property to search
+   * @param {string} value of the property to search for
+   * @return {integer} index of first instance if found, otherwise returns -1
+  */
+  function findObjectInArray(targetArray, objProperty, value) {
+      var i, totalItems = targetArray.length, objFound = false;
+      for (i = 0; i < totalItems; i++) {
+          if (targetArray[i][objProperty] === value) {
+              objFound = true;
+              return i;
+          }
+      }
+      if (objFound === false) {
+          return -1;
+      }
+  }
+
+/**
+ * remove or add obsolete profiles from the current profiles list
+ */
+  function toggleObsoleteProfiles() {
+    // below are the obsolete profiles - you just have to know their names
+    var deprecated_profiles = ['balanced-nextgen-player', 'Express Standard', 'mp4-only', 'balanced-high-definition', 'low-bandwidth-devices', 'balanced-standard-definition', 'single-rendition', 'Live - Standard', 'high-bandwidth-devices', 'Live - Premium HD', 'Live - HD', 'videocloud-default-trial', 'screencast'];
+      i = current_profiles.length;
+      while (i > 0) {
+        i--;
+        if (arrayContains(deprecated_profiles, current_profiles[i].name)) {
+          current_profiles.splice(i, 1);
+        }
+    }
+    return;
+  }
+
+  /**
+   * displays the filtered list of profiles
+   */
+  function displayFilteredProfiles() {
+    var option,
+      fragment = document.createDocumentFragment(),
+      i,
+      iMax;
+    iMax = current_profiles.length;
+    for (i = 0; i < iMax; i++) {
+      option = document.createElement('option');
+      option.textContent = current_profiles[i].name;
+      option.value = current_profiles[i].name;
+      fragment.appendChild(option);
+    }
+    ingest_profile_display.innerHTML = '';
+    ingest_profile_display.appendChild(fragment);
+    return;
+  }
+
+  function processVideoData() {
+
+    rawVideoData =
+  }
+
+  function createRequest(type) {
     var options = {},
       reqBody = {};
+    if (isDefined(client_id) && isDefined(client_secret)) {
+      options.client_id = client_id;
+      options.client_secret = client_secret;
+    }
+    switch (type) {
+      case 'getProfiles':
+        var i;
+        endpoint = '/profiles';
+        options.url = 'https://ingestion.api.brightcove.com/v1/accounts/' + account_id + endpoint;
+        options.requestType = 'GET';
+        makeRequest(options, function(response) {
+          if (isJson(response)) {
+            current_profiles = JSON.parse(response);
+            toggleObsoleteProfiles();
+            // hide legacy profiles
+            i = current_profiles.length;
+            while (i > 0) {
+              i--;
+              if (!current_profiles[i].hasOwnProperty('dynamic_origin')) {
+                current_profiles.splice(i, 1);
+              }
+            }
+
+            displayFilteredProfiles();
+          } else {
+            api_response.textContent = response;
+            logMessage(logger, 'The get all profiles operation failed; see the API Response for the error', true);
+            return;
+          }
+        });
+        break;
+      case 'createVideo':
+
+        break;
+      case 'ingestVideo':
+
+        break;
+      default:
+        console.log('bad type - shouldn\'t be here: ', type);
+    }
+  }
+  // set options for the CMS API request
+  function setCMSOptions() {
     // truncate description if too long
     videoData[videoNumber].description = videoData[videoNumber].description.substr(0, 120) + '...';
-    options.client_id = client_id;
-    options.client_secret = client_secret;
+    options.proxyURL = proxyURL;
     reqBody.name = videoData[videoNumber].name;
     reqBody.description = videoData[videoNumber].description;
     reqBody.reference_id = videoData[videoNumber].reference_id;
@@ -161,11 +281,11 @@ var BCLS = (function(window, document) {
     videoNumber = 0;
     // get account inputs
     account_id = isDefined(account_id_display.value) ? account_id_display.value : defaults.account_id;
-    client_id = isDefined(client_id_display.value) ? client_id_display.value : defaults.client_id;
-    client_secret = isDefined(client_secret_display.value) ? client_secret_display.value : defaults.client_secret;
+    client_id = client_id_display;
+    client_secret = client_secret_display;
     cms_url_display.value = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/videos';
     // set CMS API options for first video
-    setCMSOptions();
+    createRequest('createVideo');
   });
   // initialize
   function init() {
