e88396bd691b920bc1bbfb6897c85d8bdb049d44
Robert Crooks <rcrooks@brightcove.com>
2018-07-17 10:28:40 -0400

    wip

--- doc-assets/js/get-bc-cookies.js
+++ doc-assets/js/get-bc-cookies.js
@@ -1,16 +1,19 @@
-var cookiesArray = document.cookie.split(";"), params = {}, i, tmpArray = [];
+var cookiesArray = document.cookie.split(";"),
+  feedbackParams = {},
+  i,
+  tmpArray = [];
 for (i = 0; i < cookiesArray.length; i++) {
     tmpArray = cookiesArray[i].split("=");
     if (tmpArray[0].indexOf('BC_EMAIL') > -1) {
-      params.BC_EMAIL = tmpArray[1];
+      feedbackParams.BC_EMAIL = tmpArray[1];
     } else if (tmpArray[0].indexOf('BC_ACCOUNT') > -1) {
-      params.BC_ACCOUNT = tmpArray[1];
+      feedbackParams.BC_ACCOUNT = tmpArray[1];
     }
 }
-if (!params.hasOwnProperty('BC_EMAIL')) {
-  params.BC_EMAIL = 'unknown';
+if (!feedbackParams.hasOwnProperty('BC_EMAIL')) {
+  feedbackParams.BC_EMAIL = 'unknown';
 }
-if (!params.hasOwnProperty('BC_ACCOUNT')) {
-  params.BC_ACCOUNT = 'unknown';
+if (!feedbackParams.hasOwnProperty('BC_ACCOUNT')) {
+  feedbackParams.BC_ACCOUNT = 'unknown';
 }
-console.log('params', params);
+console.log('feedbackParams', feedbackParams);
