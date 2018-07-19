d648dea866ff2741b9006c4c932f4b4bee831b22
rcrooks <rcrooks@brightcove.com>
2018-07-19 15:52:10 -0400

    wip

--- doc-assets/js/bcls-feedback-drupal.js
+++ doc-assets/js/bcls-feedback-drupal.js
@@ -20,16 +20,16 @@ console.log('contentBlock', contentBlock);
   for (i = 0; i < cookiesArray.length; i++) {
     tmpArray = cookiesArray[i].split("=");
     if (tmpArray[0].indexOf('BC_EMAIL') > -1) {
-      feedbackParams.BC_EMAIL = tmpArray[1];
+      feedbackParams.bc_email = tmpArray[1];
     } else if (tmpArray[0].indexOf('BC_ACCOUNT') > -1) {
-      feedbackParams.BC_ACCOUNT = tmpArray[1];
+      feedbackParams.bc_account = tmpArray[1];
     }
   }
   if (!feedbackParams.hasOwnProperty('BC_EMAIL')) {
-    feedbackParams.BC_EMAIL = 'unknown';
+    feedbackParams.bc_email = 'unknown';
   }
   if (!feedbackParams.hasOwnProperty('BC_ACCOUNT')) {
-    feedbackParams.BC_ACCOUNT = 'unknown';
+    feedbackParams.bc_account = 'unknown';
   }
 
   iframeEl.addEventListener('load', function() {
