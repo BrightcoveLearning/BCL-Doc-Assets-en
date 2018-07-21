5848b4edc970469e34aeead17add6dda81c9dfd5
rcrooks <rcrooks@brightcove.com>
2018-07-21 16:14:54 -0400

    wip

--- doc-assets/js/bcls-feedback.js
+++ doc-assets/js/bcls-feedback.js
@@ -5,6 +5,7 @@ var BCLS_feedback = ( function (window, document) {
     feedbackParams = {},
     i,
     tmpArray = [],
+    tags = [],
     p;
 
 
@@ -24,16 +25,16 @@ var BCLS_feedback = ( function (window, document) {
   for (i = 0; i < cookiesArray.length; i++) {
     tmpArray = cookiesArray[i].split("=");
     if (tmpArray[0].indexOf('BC_EMAIL') > -1) {
-      feedbackParams.BC_EMAIL = tmpArray[1];
+      feedbackParams.bc_email = tmpArray[1];
     } else if (tmpArray[0].indexOf('BC_ACCOUNT') > -1) {
-      feedbackParams.BC_ACCOUNT = tmpArray[1];
+      feedbackParams.bc_account = tmpArray[1];
     }
   }
   if (!feedbackParams.hasOwnProperty('BC_EMAIL')) {
-    feedbackParams.BC_EMAIL = 'unknown';
+    feedbackParams.bc_email = 'unknown';
   }
   if (!feedbackParams.hasOwnProperty('BC_ACCOUNT')) {
-    feedbackParams.BC_ACCOUNT = 'unknown';
+    feedbackParams.bc_account = 'unknown';
   }
 
   iframeEl.addEventListener('load', function() {
