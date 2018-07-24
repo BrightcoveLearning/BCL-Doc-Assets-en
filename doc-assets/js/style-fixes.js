90520b7947245e637809b9a481f5dfd18136e8cf
rcrooks <rcrooks@brightcove.com>
2018-07-24 14:04:02 -0400

    wip

--- doc-assets/js/style-fixes.js
+++ doc-assets/js/style-fixes.js
@@ -1,17 +1,21 @@
 var BCLS_style_fixes = ( function (window, document) {
-  var headerBlock = document.querySelector('.full-width-headline'),
-    headerText = document.getElementById('block-brightcoveheadlineblock'),
+  var header_block = document.querySelector('.full-width-headline'),
+    header_text = document.getElementById('block-brightcoveheadlineblock'),
     side_nav_block = document.getElementById('block-inpagenavigationblock'),
-    related_content_block = document.getElementById('block-views-block-related-content-block-1');
+    related_content_block = document.getElementById('block-views-block-related-content-block-1'),
+    search_block = document.querySelector('.paragraph--type--search-block');
 
-  if (headerBlock) {
-    headerBlock.setAttribute('style', 'color:white;background-color:RGB(45, 59, 108);padding-top:10px;padding-botton:5px;margin-bottom:30px;');
-    headerText.setAttribute('style', 'color:white;');
+  if (header_block) {
+    header_block.setAttribute('style', 'color:white;background-color:RGB(45, 59, 108);padding-top:10px;padding-botton:5px;margin-bottom:30px;');
+    header_text.setAttribute('style', 'color:white;');
   }
   if (side_nav_block) {
     side_nav_block.setAttribute('style', 'background-color:transparent;')
   }
   if (related_content_block) {
     related_content_block.setAttribute('style', 'background-color:transparent;');
+  }
+  if (search_block) {
+    search_block.setAttribute('style', 'padding-top:10px; padding-bottom:15px;')
   }
 })(window, document);
