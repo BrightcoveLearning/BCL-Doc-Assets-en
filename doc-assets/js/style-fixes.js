cda6b2b1debc977e43671bd1f39293f5a0c25d07
rcrooks <rcrooks@brightcove.com>
2018-07-24 13:52:50 -0400

    wip

--- doc-assets/js/style-fixes.js
+++ doc-assets/js/style-fixes.js
@@ -4,8 +4,14 @@ var BCLS_style_fixes = ( function (window, document) {
     side_nav_block = document.getElementById('block-inpagenavigationblock'),
     related_content_block = document.getElementById('block-views-block-related-content-block-1');
 
+  if (headerBlock) {
     headerBlock.setAttribute('style', 'color:white;background-color:RGB(45, 59, 108);padding-top:10px;padding-botton:5px;margin-bottom:30px;');
     headerText.setAttribute('style', 'color:white;');
+  }
+  if (side_nav_block) {
     side_nav_block.setAttribute('style', 'background-color:transparent;')
+  }
+  if (related_content_block) {
     related_content_block.setAttribute('style', 'background-color:transparent;');
+  }
 })(window, document);
