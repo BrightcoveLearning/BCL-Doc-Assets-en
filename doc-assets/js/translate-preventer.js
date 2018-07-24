daf82a73c8e6cc3129104c3891362cc8e6b28465
Robert Crooks <rcrooks@brightcove.com>
2018-07-24 10:47:14 -0400

    wip

--- doc-assets/js/translate-preventer.js
+++ doc-assets/js/translate-preventer.js
@@ -1,3 +1,11 @@
 var BCLSnotranlate = ( function (window, document) {
-  var classesToFix = ['bcls-highlight', 'bcls-input']
+  var classesToFix = ['.bcls-highlight', '.bcls-input'],
+    preBlocks = document.getElementsByTagName('pre'),
+    i,
+    iMax;
+
+  iMax = preBlocks.length;
+  for (i = 0; i < iMax; i++) {
+    preBlocks[i].className += 
+  }
 })(window, document);
