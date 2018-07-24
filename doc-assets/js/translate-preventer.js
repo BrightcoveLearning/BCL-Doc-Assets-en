76ff6d979e48a7a4e466f6f8812718c134cb89ba
rcrooks <rcrooks@brightcove.com>
2018-07-24 11:44:49 -0400

    tranlation blocker

--- doc-assets/js/translate-preventer.js
+++ doc-assets/js/translate-preventer.js
@@ -1,11 +1,22 @@
 var BCLSnotranlate = ( function (window, document) {
-  var classesToFix = ['.bcls-highlight', '.bcls-input'],
+  var classesToFix = ['bcls-highlight', 'bcls-input'],
     preBlocks = document.getElementsByTagName('pre'),
     i,
-    iMax;
+    iMax,
+    j,
+    jMax;
 
   iMax = preBlocks.length;
   for (i = 0; i < iMax; i++) {
     preBlocks[i].className += ' notranslate';
+  }
+
+  iMax = classesToFix.length;
+  for (i = 0; i < iMax; i++) {
+    var elements = document.getElementsByClassName(classesToFix[i]);
+    jMax = elements.length;
+    for (j = 0; j < jMax; j++) {
+      elements[i].className += ' notranslate';
+    }
   }
 })(window, document);
