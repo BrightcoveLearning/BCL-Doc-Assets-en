0ccdaae558a997995d2c4a307da1eb87a945bab3
rcrooks <rcrooks@brightcove.com>
2018-08-06 14:59:00 -0400

    last format fix

--- doc-assets/node/18386-standard-ingest-profiles-dynamic-delivery/standard-profiles.js
+++ doc-assets/node/18386-standard-ingest-profiles-dynamic-delivery/standard-profiles.js
@@ -709,6 +709,7 @@ var BCLSprofiles = (function(window, document, bclsProfiles_cached) {
       jMax = profiles.length;
       for (j = 0; j < jMax; j++) {
         td = document.createElement('td');
+        td.setAttribute('style', 'text-align:center');
         img = document.createElement('img');
         if (isItemInArray(profiles[j].dynamic_origin.renditions, progressiveRenditions[i].id)) {
           img.setAttribute('src', yesImg);
