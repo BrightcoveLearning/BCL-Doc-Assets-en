a232a26a1946b426e115eae1d79f4a35ae393cf3
Robert Crooks <rcrooks@brightcove.com>
2018-08-22 08:51:38 -0400

    wip

---
+++ doc-assets/node/18353-context-aware-encoding/progress-renditions.js
@@ -0,0 +1,13 @@
+var BCLS = ( function (window, document) {
+  var progressiveRenditions = [
+    'default/progressive450',
+    'default/progressive700',
+    'default/progressive900',
+    'default/progressive1200',
+    'default/progressive1700',
+    'default/progressive2000',
+    'default/progressive2500',
+    'default/progressive3500',
+    'default/progressive400'
+  ]
+})(window, document);
