b51312f0cf441ad900dedffdd1678e67abd5e9fb
Robert Crooks <rcrooks@brightcove.com>
2018-08-27 17:32:53 -0400

    several fixes

--- doc-assets/node/18474-playback-api-sample-mrss-generator/mrss-generator.js
+++ doc-assets/node/18474-playback-api-sample-mrss-generator/mrss-generator.js
@@ -184,7 +184,7 @@ var BCLS = ( function (window, document) {
                     }
                 }
                 mrssStr += eMediaContent;
-                if (video.hasOwnProperty('schedule') && video.schedule.ends_at) {
+                if (video.hasOwnProperty('schedule') && video.schedule.hasOwnProperty('ends_at')) {
                     eItem = eItemStart + video.schedule.ends_at + eItemEnd;
                 } else {
                     eItem = eItemStart + defaultEndDate + eItemEnd;
