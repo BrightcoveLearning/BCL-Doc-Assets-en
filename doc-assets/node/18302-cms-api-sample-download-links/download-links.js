beedeb913ace9b30ccd311b5162b869b62e3bdd7
Robert Crooks <rcrooks@brightcove.com>
2018-08-30 07:17:03 -0400

    debugging

--- doc-assets/node/18302-cms-api-sample-download-links/download-links.js
+++ doc-assets/node/18302-cms-api-sample-download-links/download-links.js
@@ -101,10 +101,12 @@ var BCLS = (function(window, document) {
     // remove non-MP4 sources
     while (i > 0) {
       i--;
-      if (sources[i].container !== 'MP4' && sources[i].container.toLowerCase() !== 'flv') {
-        sources.splice(i, 1);
-      } else if (sources[i].hasOwnProperty('stream_name')) {
-        sources.splice(i, 1);
+      if (sources[i].hasOwnProperty('container') {
+        if (sources[i].container !== 'MP4' && sources[i].container !== 'flv') {
+          sources.splice(i, 1);
+        } else if (sources[i].hasOwnProperty('stream_name')) {
+          sources.splice(i, 1);
+        }
       }
     }
     // sort sources by encoding rate
