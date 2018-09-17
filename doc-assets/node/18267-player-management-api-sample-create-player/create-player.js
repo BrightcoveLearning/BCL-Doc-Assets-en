fbb6ed1848a09cd3076a208b8765c8d6fd6b0365
Robert Crooks <rcrooks@brightcove.com>
2018-09-17 16:14:20 -0700

    wip

--- doc-assets/node/18267-player-management-api-sample-create-player/create-player.js
+++ doc-assets/node/18267-player-management-api-sample-create-player/create-player.js
@@ -13,56 +13,108 @@ var BCLS = ( function (window, document) {
     create_response = document.getElementById('create_response'),
     publish_response = document.getElementById('publish_response'), player_embedded = document.getElementById('player_embedded');
 
-    /**
+  /**
    * createRequest sets up requests, send them to makeRequest(), and handles responses
    * @param  {string} type the request type
    */
   function createRequest(type) {
-      var options   = {},
-          requestBody = {},
-          // next line for BrightcoveLearning proxy (use if you are making write requests)
-          // for read requests only, use https://solutions.brightcove.com/bcls/bcls-proxy/doc-samples-proxy-v2.php
-          proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/brightcove-learning-proxy-v2.php'
-          baseURL = 'https://cms.api.brightcove.com/v1/accounts',
-          endpoint,
-          responseDecoded;
+    var options   = {},
+      requestBody = {},
+      // next line for BrightcoveLearning proxy (use if you are making write requests)
+      // for read requests only, use https://solutions.brightcove.com/bcls/bcls-proxy/doc-samples-proxy-v2.php
+      proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/brightcove-learning-proxy-v2.php'
+      baseURL = 'https://players.api.brightcove.com/v1/accounts/',
+      endpoint,
+      responseDecoded;
 
-      // set credentials
-      // assumes input fields with ids: account_id, client_id, and client_secret
-      if (document.getElementById('client_id').value.length > 0 && document.getElementById('client_secret').value.length > 0)
-      options.client_id     = document.getElementById('client_id').value;
-      options.client_secret = document.getElementById('client_secret').value;
-      options.proxyURL      = proxyURL;
+    // set credentials
+    // assumes input fields with ids: account_id, client_id, and client_secret
+    if (document.getElementById('client_id').value.length > 0 && document.getElementById('client_secret').value.length > 0)
+    options.client_id     = document.getElementById('client_id').value;
+    options.client_secret = document.getElementById('client_secret').value;
+    options.proxyURL      = proxyURL;
 
-      switch (type) {
-          case 'getVideoCount':
-              endpoint            = '/' + options.account_id + '/counts/videos';
-              options.url         = baseURL + endpoint;
-              options.requestType = 'GET';
-              makeRequest(options, function(response) {
-                  responseDecoded = JSON.parse(response);
-                  // do what you want here
-              });
-              break;
-          case 'createVideo':
-              endpoint            = '/' + options.account_id + '/counts/videos';
-              options.url         = baseURL + endpoint;
-              options.requestType = 'POST';;
-              requestBody.name    = 'My New Video';
-              // add more properties
-              options.requestBody = JSON.stringify(requestBody);
-              makeRequest(options, function(response) {
-                  responseDecoded = JSON.parse(response);
-                  // do more stuff
-              });
-              break;
+    switch (type) {
+        case 'createPlayer':
+          endpoint            = '/' + options.account_id + '/counts/videos';
+          options.url         = baseURL + endpoint;
+          options.requestType = 'GET';
+          makeRequest(options, function(response) {
+              responseDecoded = JSON.parse(response);
+              // do what you want here
+          });
+          break;
+        case 'publishPlayer':
+          endpoint            = '/' + options.account_id + '/counts/videos';
+          options.url         = baseURL + endpoint;
+          options.requestType = 'POST';;
+          requestBody.name    = 'My New Video';
+          // add more properties
+          options.requestBody = JSON.stringify(requestBody);
+          makeRequest(options, function(response) {
+              responseDecoded = JSON.parse(response);
+              // do more stuff
+          });
+          break;
 
-          // additional cases
-          default:
-              console.log('Should not be getting to the default case - bad request type sent');
-              break;
-      }
+        // additional cases
+        default:
+          console.log('Should not be getting to the default case - bad request type sent');
+          break;
+    }
   }
 
+  /**
+   * send API request to the proxy
+   * @param {Object} options for the request
+   * @param {String} options.url the full API request URL
+   * @param {String = "GET", "POST", "PATCH", "PUT", "DELETE"} requestData[options.requestType = "GET"] HTTP type for the request
+   * @param {String} options.proxyURL proxyURL to send the request to
+   * @param {String} options.client_id client id for the account( default is in the proxy)
+   * @param {String} options.client_secret client secret for the account(default is in the proxy)
+   * @param {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+   *
+     @param {Function} [callback] callback function that will process the response
+   */
+
+   function makeRequest(options, callback) {
+     var httpRequest = new XMLHttpRequest(),
+       response,
+       requestParams,
+       dataString,
+       proxyURL = options.proxyURL,
+       // response handler
+       getResponse = function() {
+         try {
+           if (httpRequest.readyState === 4) {
+             if (httpRequest.status >= 200 && httpRequest.status < 300) {
+               response = httpRequest.responseText;
+               // some API requests return '{null}' for empty responses - breaks JSON.parse
+               if (response === '{null}') {
+                 response = null;
+               }
+               // return the response
+               callback(response);
+             } else {
+               alert('There was a problem with the request. Request returned ' + httpRequest.status);
+             }
+           }
+         } catch (e) {
+           alert('Caught Exception: ' + e);
+         }
+       };
+     /**
+      * set up request data
+      * the proxy used here takes the following request body:
+      * JSON.strinify(options)
+      */
+     // set response handler
+     httpRequest.onreadystatechange = getResponse;
+     // open the request
+     httpRequest.open('POST', proxyURL);
+     // set headers if there is a set header line, remove it
+     // open and send request
+     httpRequest.send(JSON.stringify(options));
+   }
 
 })(window, document);
