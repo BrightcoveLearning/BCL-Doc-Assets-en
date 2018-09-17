dd98bfba471930bd328cfa55c9f343d7b8fbeae7
Robert Crooks <rcrooks@brightcove.com>
2018-09-17 16:03:43 -0700

    wip

--- doc-assets/node/18267-player-management-api-sample-create-player/create-player.js
+++ doc-assets/node/18267-player-management-api-sample-create-player/create-player.js
@@ -6,19 +6,12 @@ var BCLS = ( function (window, document) {
     media_url = "",
     media_type = "",
     callPurpose = "",
-    ;
-})(window, document);
+    account_id_input = document.getElementById('account_id_input'),
+    client_id_input = document.getElementById('client_id_input'),
+    client_secrect_input - document.getElementById('client_secrect_input'),
+    player_name_input = document.getElementById('player_name_input'),
+    create_response = document.getElementById('create_response'),
+    publish_response = document.getElementById('publish_response'), player_embedded = document.getElementById('player_embedded');
 
-var account_id = "",
-  account_password = "",
-  account_username = "",
-  player_name = "",
-  media_url = "",
-  media_type = "",
-  callPurpose = "",
-  account_id_input = document.getElementById('account_id_input'),
-  client_id_input = document.getElementById('client_id_input'),
-  client_secrect_input - document.getElementById('client_secrect_input'),
-  player_name_input = document.getElementById('player_name_input'),
-create_response = document.getElementById('create_response'),
-publish_response = document.getElementById('publish_response'), player_embedded = document.getElementById('player_embedded');
+  
+})(window, document);
