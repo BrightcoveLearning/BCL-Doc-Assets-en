9793757bebe54d39b7ed8379dc60091c4a3bf71d
Robert Crooks <rcrooks@brightcove.com>
2018-10-01 17:04:36 -0400

    wip

---
+++ doc-assets/node/18680-page-experience-client-api-sample/player-script.js
@@ -0,0 +1,29 @@
+  var BCLS = ( function (window, document) {
+    var experience = window.top.bcov.gal.getEmbed('experience_5bb2134180b4990011750f06'),
+    current_state = document.getElementById('current_state'),
+    current_video = document.getElementById('current_video'),
+    current_position = document.getElementById('current_position');
+    console.log('experience', experience);
+    // event listeners
+    experience.on('stateChanged', function() {
+      current_state.textContent = experience.getCurrentState();
+      console.log('current state', experience.getCurrentState())
+    });
+
+    experience.on('videoChanged', function() {
+      current_video.textContent = experience.getCurrentVideo();
+      console.log('current video', experience.getCurrentVideo());
+      current_position.textContent = '0';
+    });
+
+    experience.on('playerLoad', function() {
+      current_video.textContent = experience.getCurrentVideo();
+      console.log('current video', experience.getCurrentVideo());
+    });
+
+    experience.on('videoStarted', function() {
+      var interval;
+      current_position.textContent = '0';
+    });
+
+  })(window, document);
