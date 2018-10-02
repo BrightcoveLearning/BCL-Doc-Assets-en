dd5c1e1437cd6fd12d8ccb079ddb2ae637e706b1
Robert Crooks <rcrooks@brightcove.com>
2018-10-02 08:54:02 -0400

    wip

--- doc-assets/node/18680-page-experience-client-api-sample/player-script.js
+++ doc-assets/node/18680-page-experience-client-api-sample/player-script.js
@@ -1,31 +1,31 @@
-  var BCLS = ( function (window, document) {
-    var experience = window.top.bcov.gal.getEmbed('experience_5bb2134180b4990011750f06'),
-    experienceApi = experience.clientApi,
-    current_video = document.getElementById('current_video'),
-    video_paused = document.getElementById('video_paused');
-    console.log('experience', experience);
-    console.log('experienceApi', experienceApi);
+var BCLS = ( function (window, document) {
+  var experience = window.top.bcov.gal.getEmbed('experience_5bb2134180b4990011750f06'),
+  experienceApi = experience.clientApi,
+  current_video = document.getElementById('current_video'),
+  video_paused = document.getElementById('video_paused');
+  console.log('experience', experience);
+  console.log('experienceApi', experienceApi);
 
-    // var videos = experience.getAllVideos();
-    // console.log('videos', videos);
-    // event listeners
+  // var videos = experience.getAllVideos();
+  // console.log('videos', videos);
+  // event listeners
 
-    experience.on('videoChanged', function() {
-      current_video.textContent = experienceApi.getCurrentVideo();
-      console.log('current video', experienceApi.getCurrentVideo());
-    });
+  experience.on('videoChanged', function() {
+    current_video.textContent = experienceApi.getCurrentVideo();
+    console.log('current video', experienceApi.getCurrentVideo());
+  });
 
-    experience.on('playerLoad', function(player) {
-      current_video.textContent = experienceApi.getCurrentVideo();
-      console.log('current video', experienceApi.getCurrentVideo());
-    });
+  experience.on('playerLoad', function(player) {
+    current_video.textContent = experienceApi.getCurrentVideo();
+    console.log('current video', experienceApi.getCurrentVideo());
+  });
 
-    experience.on('videoStarted', function(video) {
-      video_paused.textContent = 'false';
-    });
+  experience.on('videoStarted', function(video) {
+    video_paused.textContent = 'false';
+  });
 
-    experience.on('videoPaused', function(video) {
-      video_paused.textContent = 'true';
-    });
+  experience.on('videoPaused', function(video) {
+    video_paused.textContent = 'true';
+  });
 
-  })(window, document);
+})(window, document);
