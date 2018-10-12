cd902cea8e9f2ef9d907ea501a3102374e2bef8b
Robert Crooks <rcrooks@brightcove.com>
2018-10-12 15:08:28 -0400

    wip

--- doc-assets/node/18683-quick-start-page-experience-platform-rest-api/codepen.js
+++ doc-assets/node/18683-quick-start-page-experience-platform-rest-api/codepen.js
@@ -203,9 +203,9 @@ var BCLS = (function(window, document) {
         break;
       case 'create_ipx':
         var now = new Date().toISOString(),
+          new_experience_template = getSelectedValue(template_selector);
           is_playlist_template = arrayContains(playlist_templates, new_experience_template);
           console.log('is playlist', is_playlist_template);
-          new_experience_template = getSelectedValue(template_selector);
           new_experience_name = 'Experience from Quick Start ' + now;
         endPoint = options.account_id + '/experiences';
         options.url = ipxURL + endPoint;
