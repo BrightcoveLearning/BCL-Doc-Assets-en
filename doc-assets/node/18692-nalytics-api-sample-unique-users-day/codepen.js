b90e2462e0da3803a582a91de990830cf3694904
Robert Crooks <rcrooks@brightcove.com>
2018-11-09 16:24:02 -0500

    wip

--- doc-assets/node/18692-nalytics-api-sample-unique-users-day/codepen.js
+++ doc-assets/node/18692-nalytics-api-sample-unique-users-day/codepen.js
@@ -23,4 +23,67 @@ var BCLS = ( function (window, document) {
   // date pickers
   rome(fromDate);
   rome(toDate);
+
+  function getIsoDateString(d) {
+    
+  }
+
+
+  function init() {
+    // event listeners
+    csvData.addEventListener('click', function() {
+      this.select();
+    });
+    // set up the log elements
+  }
+
+  // button event handlers
+  makeReport.addEventListener('click', function() {
+    // get the inputs
+    client_id = client_id_input.value;
+    client_secret = client_secret_input.value;
+    totalVideos = getSelectedValue(videoCount);
+    // check for search terms
+    if (isDefined(searchTags.value)) {
+      tagsSearchString = '%2Btags:' + removeSpaces(searchTags.value);
+    }
+    if (isDefined(searchFieldValue.value)) {
+      if (isDefined(searchField.value)) {
+        fieldsSearchString = '%2B' + searchField.value + ':' + convertSpaces(searchFieldValue.value);
+      } else {
+        fieldsSearchString = '%2Bcustom_fields:"' + convertSpaces(searchFieldValue.value) + '"';
+      }
+    }
+    dateTypeValue = getSelectedValue(dateRangeType).value;
+    fromDateValue = rome(fromDate).getDate();
+    if (isDefined(fromDateValue)) {
+      fromDateValue = fromDateValue.toISOString();
+    }
+    toDateValue = rome(toDate).getDate();
+    if (isDefined(toDateValue)) {
+      toDateValue = toDateValue.toISOString();
+    }
+    if (isDefined(fromDateValue) || isDefined(toDateValue)) {
+      dateSearchString = '%2B' + dateTypeValue + ':' + fromDateValue + '..' + toDateValue;
+    }
+
+    // only use entered account id if client id and secret are entered also
+    if (isDefined(client_id) && isDefined(client_secret)) {
+      if (isDefined(account_id_input.value)) {
+        account_id = account_id_input.value;
+      } else {
+        window.alert('To use your own account, you must specify an account id, and client id, and a client secret - since at least one of these is missing, a sample account will be used');
+        client_id = '';
+        client_secret = '';
+        account_id = '1485884786001';
+      }
+    } else {
+      account_id = '1485884786001';
+    }
+    // get video count
+    createRequest('getAffiliates');
+
+  });
+
+  init();
 })(window, document);
