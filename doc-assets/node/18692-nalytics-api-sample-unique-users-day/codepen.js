e015a21d730924c0aa9a8c67bc9d5ffe3c0a6652
Robert Crooks <rcrooks@brightcove.com>
2018-11-10 12:26:51 -0500

    wip

--- doc-assets/node/18692-nalytics-api-sample-unique-users-day/codepen.js
+++ doc-assets/node/18692-nalytics-api-sample-unique-users-day/codepen.js
@@ -1,4 +1,4 @@
-var BCLS = ( function (window, document) {
+var BCLS = ( function (window, document, rome) {
   var account_id,
     client_id,
     client_secret,
@@ -20,6 +20,7 @@ var BCLS = ( function (window, document) {
     fromDateValue,
     toDateValue;
 
+  // date pickers
   rome(fromDate);
   rome(toDate);
 
@@ -74,6 +75,57 @@ var BCLS = ( function (window, document) {
               break;
       }
   }
-  // date pickers
 
-})(window, document);
+  /**
+   * send API request to the proxy
+   * @param  {Object} options for the request
+   * @param  {String} options.url the full API request URL
+   * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+   * @param  {String} options.proxyURL proxyURL to send the request to
+   * @param  {String} options.client_id client id for the account (default is in the proxy)
+   * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+   * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+   * @param  {Function} [callback] callback function that will process the response
+   */
+  function makeRequest(options, callback) {
+      var httpRequest = new XMLHttpRequest(),
+          response,
+          requestParams,
+          dataString,
+          proxyURL    = options.proxyURL,
+          // response handler
+          getResponse = function() {
+              try {
+                  if (httpRequest.readyState === 4) {
+                      if (httpRequest.status >= 200 && httpRequest.status < 300) {
+                          response = httpRequest.responseText;
+                          // some API requests return '{null}' for empty responses - breaks JSON.parse
+                          if (response === '{null}') {
+                              response = null;
+                          }
+                          // return the response
+                          callback(response);
+                      } else {
+                          alert('There was a problem with the request. Request returned ' + httpRequest.status);
+                      }
+                  }
+              } catch (e) {
+                  alert('Caught Exception: ' + e);
+              }
+          };
+      /**
+       * set up request data
+       * the proxy used here takes the following request body:
+       * JSON.strinify(options)
+       */
+      // set response handler
+      httpRequest.onreadystatechange = getResponse;
+      // open the request
+      httpRequest.open('POST', proxyURL);
+      // set headers if there is a set header line, remove it
+      // open and send request
+      httpRequest.send(JSON.stringify(options));
+  }
+
+
+})(window, document, rome);
