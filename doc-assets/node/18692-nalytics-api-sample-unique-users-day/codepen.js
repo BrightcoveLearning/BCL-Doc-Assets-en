7ee5de5ece5eca2726a39d8ffaa316280b494265
Robert Crooks <rcrooks@brightcove.com>
2018-11-11 11:40:16 -0500

    wip

--- doc-assets/node/18692-nalytics-api-sample-unique-users-day/codepen.js
+++ doc-assets/node/18692-nalytics-api-sample-unique-users-day/codepen.js
@@ -1,4 +1,4 @@
-var BCLS = ( function (window, document, rome) {
+var BCLS = (function(window, document, rome) {
   var account_id,
     client_id,
     client_secret,
@@ -61,40 +61,41 @@ var BCLS = ( function (window, document, rome) {
    * @param  {string} type the request type
    */
   function createRequest(type) {
-      var options   = {},
-          proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy-v2.php';
+    var options = {},
+      proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy-v2.php';
 
-      // set credentials
-      // assumes input fields with ids: account_id, client_id, and client_secret
-      if (document.getElementById('client_id').value.length > 0 && document.getElementById('client_secret').value.length > 0) {
-        options.client_id     = client_id;
-        options.client_secret = client_secret;
-      }
-      options.requestType = 'GET';
-      options.proxyURL      = proxyURL;
-      options.url = baseURL + account_id + '&dimensions=date&limit=all&fields=video_view,daily_unique_users&from=' + fromDateValue + '&to=' + toDateValue;
+    // set credentials
+    // assumes input fields with ids: account_id, client_id, and client_secret
+    if (document.getElementById('client_id').value.length > 0 && document.getElementById('client_secret').value.length > 0) {
+      options.client_id = client_id;
+      options.client_secret = client_secret;
+    }
+    options.requestType = 'GET';
+    options.proxyURL = proxyURL;
+    options.url = baseURL + account_id + '&dimensions=date&limit=all&fields=video_view,daily_unique_users&from=' + fromDateValue + '&to=' + toDateValue;
 
-      switch (type) {
-          case 'getJSON':
-            makeRequest(options, function(response) {
-                response = JSON.parse(response);
-                createReportTable(response);
-                // now get the csv version
-                createRequest('getCSV');
-            });
-            break;
-          case 'getCSV':
-              options.url         += '&format=csv';
-              makeRequest(options, function(response) {
-                  response = JSON.parse(response);
-              });
-              break;
+    switch (type) {
+      case 'getJSON':
+        makeRequest(options, function(response) {
+          response = JSON.parse(response);
+          createReportTable(response);
+          // now get the csv version
+          createRequest('getCSV');
+        });
+        break;
+      case 'getCSV':
+        options.url += '&format=csv';
+        makeRequest(options, function(response) {
+          response = JSON.parse(response);
+          csvData_display.textContent = response;
+        });
+        break;
 
-          // additional cases
-          default:
-              console.log('Should not be getting to the default case - bad request type sent');
-              break;
-      }
+        // additional cases
+      default:
+        console.log('Should not be getting to the default case - bad request type sent');
+        break;
+    }
   }
 
   /**
@@ -109,48 +110,48 @@ var BCLS = ( function (window, document, rome) {
    * @param  {Function} [callback] callback function that will process the response
    */
   function makeRequest(options, callback) {
-      var httpRequest = new XMLHttpRequest(),
-          response,
-          requestParams,
-          dataString,
-          proxyURL    = options.proxyURL,
-          // response handler
-          getResponse = function() {
-              try {
-                  if (httpRequest.readyState === 4) {
-                      if (httpRequest.status >= 200 && httpRequest.status < 300) {
-                          response = httpRequest.responseText;
-                          // some API requests return '{null}' for empty responses - breaks JSON.parse
-                          if (response === '{null}') {
-                              response = null;
-                          }
-                          // return the response
-                          callback(response);
-                      } else {
-                          alert('There was a problem with the request. Request returned ' + httpRequest.status);
-                      }
-                  }
-              } catch (e) {
-                  alert('Caught Exception: ' + e);
+    var httpRequest = new XMLHttpRequest(),
+      response,
+      requestParams,
+      dataString,
+      proxyURL = options.proxyURL,
+      // response handler
+      getResponse = function() {
+        try {
+          if (httpRequest.readyState === 4) {
+            if (httpRequest.status >= 200 && httpRequest.status < 300) {
+              response = httpRequest.responseText;
+              // some API requests return '{null}' for empty responses - breaks JSON.parse
+              if (response === '{null}') {
+                response = null;
               }
-          };
-      /**
-       * set up request data
-       * the proxy used here takes the following request body:
-       * JSON.strinify(options)
-       */
-      // set response handler
-      httpRequest.onreadystatechange = getResponse;
-      // open the request
-      httpRequest.open('POST', proxyURL);
-      // set headers if there is a set header line, remove it
-      // open and send request
-      httpRequest.send(JSON.stringify(options));
+              // return the response
+              callback(response);
+            } else {
+              alert('There was a problem with the request. Request returned ' + httpRequest.status);
+            }
+          }
+        } catch (e) {
+          alert('Caught Exception: ' + e);
+        }
+      };
+    /**
+     * set up request data
+     * the proxy used here takes the following request body:
+     * JSON.strinify(options)
+     */
+    // set response handler
+    httpRequest.onreadystatechange = getResponse;
+    // open the request
+    httpRequest.open('POST', proxyURL);
+    // set headers if there is a set header line, remove it
+    // open and send request
+    httpRequest.send(JSON.stringify(options));
   }
 
   function init() {
     // event listeners
-    csvData.addEventListener('click', function() {
+    csvData_display.addEventListener('click', function() {
       this.select();
     });
     // get initial account info
@@ -161,7 +162,7 @@ var BCLS = ( function (window, document, rome) {
   makeReport.addEventListener('click', function() {
     // get the inputs
     getAccountInfo();
-
+    // get date range or default values
     fromDateValue = rome(fromDate).getDate();
     if (isDefined(fromDateValue)) {
       fromDateValue = getIsoDate(fromDateValue);
