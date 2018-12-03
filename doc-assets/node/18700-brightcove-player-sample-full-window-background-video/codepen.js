2c88cc0ab07ab97f7242d47092161a9f0425ae27
Robert Crooks <rcrooks@brightcove.com>
2018-12-03 09:44:21 -0500

    reworking

--- doc-assets/node/18700-brightcove-player-sample-full-window-background-video/codepen.js
+++ doc-assets/node/18700-brightcove-player-sample-full-window-background-video/codepen.js
@@ -5,37 +5,43 @@ videojs.getPlayer('myPlayerID').ready(function() {
   // +++ Set the playlist to repeat +++
   myPlayer.playlist.repeat(true);
 
+  // +++ Initial player sizing +++
+  sizePlayer();
+
+  // +++ Event handler for window resize +++
+  window.addEventListener('resize', function(){
+    sizePlayer();
+  });
+
+  function sizePlayer() {
+    var windowWidth = window.innerWidth,
+      windowHeight = window.innerHeight,
+      playerWidth,
+      playerHeight,
+      windowAspectRatio = windowWidth / windowHeight;
+      /* If the aspect ratio of the window is
+       greater than or equal to 16:9, we will set
+       the player width to 100% and then set the
+       width to match the 16:9 aspect ratio. If
+       the window aspect ratio is less than 16:9,
+       we will set the height to 100% and the width
+       accordingly */
+    if (windowAspectRatio >= (16 / 9)) {
+      playerWidth = windowWidth;
+      playerHeight = (9 / 16) * playerWidth;
+    } else {
+      playerHeight = windowHeight;
+      playerWidth = (16 / 9) * playerHeight;
+    }
+    // now use the Player API to set the actual width and height
+    myPlayer.width(playerWidth);
+    myPlayer.height(playerHeight);
+  }
+
+
   // +++ Display the title and description +++
   myPlayer.on("loadstart", function() {
-    sizePlayer();
     videoTitle.textContent = myPlayer.mediainfo.name;
     videoDescription.textContent = myPlayer.mediainfo.description;
-    function sizePlayer() {
-      var windowWidth = window.innerWidth,
-        windowHeight = window.innerHeight,
-        playerWidth,
-        playerHeight,
-        windowAspectRatio = windowWidth / windowHeight;
-        /* If the aspect ratio of the window is
-         greater than or equal to 16:9, we will set
-         the player width to 100% and then set the
-         width to match the 16:9 aspect ratio. If
-         the window aspect ratio is less than 16:9,
-         we will set the height to 100% and the width
-         accordingly */
-      if (windowAspectRatio >= (16 / 9)) {
-        playerWidth = windowWidth;
-        playerHeight = (9 / 16) * playerWidth;
-      } else {
-        playerHeight = windowHeight;
-        playerWidth = (16 / 9) * playerHeight;
-      }
-      // now use the Player API to set the actual width and height
-      myPlayer.width(playerWidth);
-      myPlayer.height(playerHeight);
-    }
-    window.addEventListener('resize', function(){
-      sizePlayer();
-    })
   });
 });
