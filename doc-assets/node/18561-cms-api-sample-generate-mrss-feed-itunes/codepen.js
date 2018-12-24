6a9867da9bc19a5df638873ce8a1a1972f951c99
Robert Crooks <rcrooks@brightcove.com>
2018-12-24 13:02:12 -0500

    wip

--- doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/codepen.js
+++ doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/codepen.js
@@ -1,6 +1,8 @@
 var BCLS = (function(window, document) {
   var mrssStr =
       '<?xml version="1.0" encoding="utf-8"?><rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">',
+    sCdata = '<![CDATA[',
+    eCdata = ']>',
     sChannel = "<channel>",
     eChannel = "</channel>",
     sTitle = "<title>",
@@ -514,52 +516,6 @@ var BCLS = (function(window, document) {
     }
   }
 
-  /**
-   * get array of values for checked boxes in a collection
-   * @param {htmlElementCollection} checkBoxCollection collection of checkbox elements
-   * @return {Array} array of the values of the checked boxes
-   */
-  function getCheckedBoxValues(checkBoxCollection) {
-    var checkedValues = [],
-      i,
-      iMax;
-    if (checkBoxCollection) {
-      iMax = checkBoxCollection.length;
-      for (i = 0; i < iMax; i++) {
-        if (checkBoxCollection[i].checked === true) {
-          checkedValues.push(checkBoxCollection[i].value);
-        }
-      }
-      return checkedValues;
-    } else {
-      console.log("Error: no input received");
-      return null;
-    }
-  }
-
-  /**
-   * selects all checkboxes in a collection
-   * @param {htmlElementCollection} checkboxCollection a collection of the checkbox elements, usually gotten by document.getElementsByName()
-   */
-  function selectAllCheckboxes(checkboxCollection) {
-    var i,
-      iMax = checkboxCollection.length;
-    for (i = 0; i < iMax; i += 1) {
-      checkboxCollection[i].setAttribute("checked", "checked");
-    }
-  }
-
-  /**
-   * deselects all checkboxes in a collection
-   * @param {htmlElementCollection} checkboxCollection a collection of the checkbox elements, usually gotten by document.getElementsByName()
-   */
-  function deselectAllCheckboxes(checkboxCollection) {
-    var i,
-      iMax = checkboxCollection.length;
-    for (i = 0; i < iMax; i += 1) {
-      checkboxCollection[i].removeAttribute("checked");
-    }
-  }
 
   function addItems() {
     var i,
