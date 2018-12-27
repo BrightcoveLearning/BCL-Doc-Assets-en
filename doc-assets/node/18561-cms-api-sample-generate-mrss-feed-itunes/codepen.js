aa6d5bc653879cb423f419b92f468ea60b035b1a
Robert Crooks <rcrooks@brightcove.com>
2018-12-27 12:54:40 -0500

    wip

--- doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/codepen.js
+++ doc-assets/node/18561-cms-api-sample-generate-mrss-feed-itunes/codepen.js
@@ -123,73 +123,21 @@ var BCLS = (function(window, document) {
     feedDisplay = document.getElementById("feedDisplay"),
     allButtons = document.getElementsByName("button"),
     categories = {
-      Arts: [
-        "Design",
-        "Fashion & Beauty",
-        "Food",
-        "Literature",
-        "Performing Arts",
-        "Visual Arts"
-      ],
-      Business: [
-        "Business News",
-        "Careers",
-        "Investing",
-        "Management & Marketing",
-        "Shopping"
+      Arts: [ "Design", "Fashion & Beauty", "Food", "Literature", "Performing Arts", "Visual Arts" ],
+      Business: [ "Business News", "Careers", "Investing", "Management & Marketing", "Shopping"
       ],
       Comedy: [],
-      Education: [
-        "Educational Technology",
-        "Higher Education",
-        "K-12",
-        "Language Courses",
-        "Training"
-      ],
-      "Games & Hobbies": [
-        "Automotive",
-        "Aviation",
-        "Hobbies",
-        "Other Games",
-        "Video Games"
-      ],
-      "Government & Organizations": [
-        "Local",
-        "National",
-        "Non-Profit",
-        "Regional"
-      ],
-      Health: [
-        "Alternative Health",
-        "Fitness & Nutrition",
-        "Self-Help",
-        "Sexuality"
+      Education: [ "Educational Technology", "Higher Education", "K-12", "Language Courses", "Training" ],
+      "Games & Hobbies": [ "Automotive", "Aviation", "Hobbies", "Other Games", "Video Games" ],
+      "Government & Organizations": [ "Local", "National", "Non-Profit", "Regional" ],
+      Health: [ "Alternative Health", "Fitness & Nutrition", "Self-Help", "Sexuality"
       ],
       "Kids & Family": [],
       Music: [],
       "News & Politics": [],
-      "Religion & Spirituality": [
-        "Buddhism",
-        "Christianity",
-        "Hinduism",
-        "Islam",
-        "Judaism",
-        "Other",
-        "Spirituality"
-      ],
+      "Religion & Spirituality": [ "Buddhism", "Christianity", "Hinduism", "Islam", "Judaism", "Other", "Spirituality" ],
       "Science & Medicine": ["Medicine", "Natural Sciences", "Social Sciences"],
-      "Society & Culture": [
-        "History",
-        "Personal Journals",
-        "Philosophy",
-        "Places & Travel"
-      ],
-      "Sports & Recreation": [
-        "Amateur",
-        "College & High School",
-        "Outdoor",
-        "Professional"
-      ],
+      "Sports & Recreation": [ "Amateur", "College & High School", "Outdoor", "Professional" ],
       Technology: ["Gadgets", "Tech News", "Podcasting", "Software How-To"],
       "TV & Film": []
     };
