680f5d95dd2f1074793a5166fd82973cf6c17adc
Robert Crooks <rcrooks@brightcove.com>
2019-02-16 16:47:56 -0500

    wip

--- doc-assets/node/18731-quick-start-social-api/codepen.js
+++ doc-assets/node/18731-quick-start-social-api/codepen.js
@@ -2,6 +2,10 @@ var BCLS = (function(window, document) {
   var baseURL = 'https://social.api.brightcove.com/v1/',
     proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/bcls-proxy-v2.php',
     video_id,
+    account_id,
+    client_id,
+    client_secret,
+    account_id_default = '1486906377',
     account_id_input = document.getElementById('account_id_input'),
     client_id_input = document.getElementById('client_id_input'),
     client_secret_input = document.getElementById('client_secret_input'),
@@ -20,7 +24,7 @@ var BCLS = (function(window, document) {
    * disables a button so user can't submit new request until current one finishes
    * @param {htmlElement} button
    */
-  function disableButton(button) {
+  function disableElement(button) {
       button.setAttribute('disabled', 'disabled');
       button.classList.add('disabled');
   }
@@ -28,7 +32,7 @@ var BCLS = (function(window, document) {
   /**
    * re-enables a button
    */
-  function enableButton(button) {
+  function enableElement(button) {
       button.removeAttribute('disabled');
       button.classList.remove('disabled');
   }
@@ -68,6 +72,10 @@ var BCLS = (function(window, document) {
     return;
   }
 
+  function getInfo() {
+
+  }
+
   /**
    * sets up the data for the API request
    * @param {String} id the id of the button that was clicked
@@ -80,7 +88,7 @@ var BCLS = (function(window, document) {
       i,
       iMax;
     // disable buttons to prevent a new request before current one finishes
-    disableButtons();
+    disableElements();
     options.account_id = '1486906377';
     options.proxyURL = proxyURL;
     switch (id) {
@@ -97,10 +105,16 @@ var BCLS = (function(window, document) {
           // add ids to array to add to playlist later
           iMax = parsedData.length;
           for (i = 0; i < iMax; i++) {
-            playlist_videos.push(parsedData[i].id);
+            setoptions('getVideoName', parsedData[i].id, function(name) {
+              selectorData.push({'id': parsedData[i].id, 'name': name});
+            });
           }
-          enableButton('getStatusOne);
-          enableButton('getHistory');
+          populateSelector(videoForStatus, selectorData, 'id', 'name');
+          populateSelector(videoForHistory, selectorData, 'id', 'name');
+          enableElement(getStatusOne);
+          enableElement(getHistory);
+          enableElement(videoForStatus);
+          enableElement(videoForHistory);
         });
         break;
       case 'getVideoName':
@@ -118,10 +132,10 @@ var BCLS = (function(window, document) {
           for (i = 0; i < iMax; i++) {
             playlist_videos.push(parsedData[i].id);
           }
-          enableButtons();
+          enableElements();
         });
         break;
-      case 'sort':
+      case 'getStatusOne':
         endPoint = '/videos?limit=5&sort=name';
         options.url = baseURL + endPoint;
         options.requestType = 'GET';
@@ -130,10 +144,10 @@ var BCLS = (function(window, document) {
         apiData.textContent = '';
         makeRequest(options, function(response) {
           displayResponse(response);
-          enableButtons();
+          enableElements();
         });
         break;
-      case 'get1video':
+      case 'getHistory':
         endPoint = '/videos/' + video_id;
         options.url = baseURL + endPoint;
         options.requestType = 'GET';
@@ -142,254 +156,11 @@ var BCLS = (function(window, document) {
         apiData.textContent = '';
         makeRequest(options, function(response) {
           displayResponse(response);
-          enableButtons();
+          enableElements();
         });
         break;
-      case 'sources':
-        endPoint = '/videos/' + video_id + '/sources';
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'search':
-        endPoint = '/videos?q=' + encodeURI('name:sea');
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'searchCount':
-        endPoint = '/counts/videos?q=' + encodeURI('name:sea');
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'createVideo':
-        endPoint = '/videos';
-        options.url = baseURL + endPoint;
-        options.requestType = 'POST';
-        requestBody.name = 'Great Blue Heron - from CMS API Quick Start - ' + nowISO;
-        options.requestBody = JSON.stringify(requestBody);
-        apiRequest.textContent = options.url;
-        apiData.textContent = JSON.stringify(requestBody, null, '  ');
-        apiMethod.textContent = options.requestType;
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          parsedData = JSON.parse(response);
-          newVideo_id = parsedData.id;
-          enableButtons();
-        });
-        break;
-      case 'updateVideo':
-        endPoint = '/videos/' + newVideo_id;
-        options.url = baseURL + endPoint;
-        options.requestType = 'PATCH';
-        requestBody.description = 'This video was updated ' + nowISO;
-        requestBody.tags = ['test', 'quick_start'];
-        options.requestBody = JSON.stringify(requestBody);
-        apiRequest.textContent = options.url;
-        apiData.textContent = JSON.stringify(requestBody, null, '  ');
-        apiMethod.textContent = options.requestType;
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'addRendition':
-        endPoint = '/videos/' + newVideo_id + '/assets/renditions';
-        options.url = baseURL + endPoint;
-        options.requestType = 'POST';
-        requestBody.progressive_download = true;
-        requestBody.remote_url = 'https://learning-services-media.brightcove.com/videos/mp4/greatblueheron.mp4';
-        requestBody.video_duration = 31000;
-        requestBody.size = 90990884;
-        requestBody.frame_height = 1080;
-        requestBody.frame_width = 1920;
-        requestBody.video_codec = 'h264';
-        requestBody.video_container = 'MP4';
-        options.requestBody = JSON.stringify(requestBody);
-        apiRequest.textContent = options.url;
-        apiData.textContent = JSON.stringify(requestBody, null, '  ');
-        apiMethod.textContent = options.requestType;
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'addPoster':
-        endPoint = '/videos/' + newVideo_id + '/assets/poster';
-        options.url = baseURL + endPoint;
-        options.requestType = 'POST';
-        requestBody.remote_url = 'https://learning-services-media.brightcove.com/images/great-blue-heron-poster.png';
-        options.requestBody = JSON.stringify(requestBody);
-        apiRequest.textContent = options.url;
-        apiData.textContent = JSON.stringify(requestBody, null, '  ');
-        apiMethod.textContent = options.requestType;
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'addThumbnail':
-        endPoint = '/videos/' + newVideo_id + '/assets/thumbnail';
-        options.url = baseURL + endPoint;
-        options.requestType = 'POST';
-        requestBody.remote_url = 'https://learning-services-media.brightcove.com/images/great-blue-heron-thumbnail.png';
-        options.requestBody = JSON.stringify(requestBody);
-        apiRequest.textContent = options.url;
-        apiData.textContent = JSON.stringify(requestBody, null, '  ');
-        apiMethod.textContent = options.requestType;
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'get3playlists':
-        endPoint = '/playlists?limit=3';
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'get1playlist':
-        endPoint = '/playlists/' + playlist_id;
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'getPlaylistVideoCount':
-        endPoint = '/counts/playlists/' + playlist_id + '/videos';
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'getPlaylistVideos':
-        endPoint = '/playlists/' + playlist_id + '/videos';
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'createPlaylist':
-        endPoint = '/playlists';
-        options.url = baseURL + endPoint;
-        options.requestType = 'POST';
-        requestBody.name = 'New Manual Playlist from CMS API Quick Start ' + nowISO;
-        requestBody.type = 'EXPLICIT';
-        requestBody.name = 'New Playlist from CMS API Quick Start' + nowISO;
-        options.requestBody = JSON.stringify(requestBody);
-        apiRequest.textContent = options.url;
-        apiData.textContent = JSON.stringify(requestBody, null, '  ');
-        apiMethod.textContent = options.requestType;
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          parsedData = JSON.parse(response);
-          newPlaylist_id = parsedData.id;
-          enableButtons();
-        });
-        break;
-      case 'updatePlaylist':
-        endPoint = '/playlists/' + newPlaylist_id;
-        options.url = baseURL + endPoint;
-        options.requestType = 'PATCH';
-        requestBody.name = 'Updated Playlist from CMS API Quick Start' + nowISO;
-        requestBody.video_ids = playlist_videos;
-        options.requestBody = JSON.stringify(requestBody);
-        apiRequest.textContent = options.url;
-        apiData.textContent = JSON.stringify(requestBody, null, '  ');
-        apiMethod.textContent = options.requestType;
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'getFolders':
-        endPoint = '/folders';
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'getFolderVideos':
-        endPoint = '/folders/' + folder_id + '/videos';
-        options.url = baseURL + endPoint;
-        options.requestType = 'GET';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          displayResponse(response);
-          enableButtons();
-        });
-        break;
-      case 'addVideoToFolder':
-        endPoint = '/folders/' + folder_id + '/videos/' + video_id;
-        options.url = baseURL + endPoint;
-        options.requestType = 'PUT';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          responseData.textContent = 'This request returns 204 No Content';
-          enableButtons();
-        });
-        break;
-      case 'removeVideoFromFolder':
-        endPoint = '/folders/' + folder_id + '/videos/' + video_id;
-        options.url = baseURL + endPoint;
-        options.requestType = 'DELETE';
-        apiRequest.textContent = options.url;
-        apiMethod.textContent = options.requestType;
-        apiData.textContent = '';
-        makeRequest(options, function(response) {
-          responseData.textContent = 'This request returns 204 No Content';
-          enableButtons();
-        });
-        break;
+      default:
+        console.log('should not be here - ', id);
     }
   }
 
@@ -443,70 +214,16 @@ var BCLS = (function(window, document) {
   }
 
   // event listeners
-  get5videos.addEventListener('click', function() {
-    setoptions('get5videos');
+  getStatusAll.addEventListener('click', function() {
+    getInfo();
+    setoptions('getStatusAll');
   });
   get5more.addEventListener('click', function() {
-    setoptions('get5more');
+    getInfo();
+    setoptions('getStatusOne');
   });
   sort.addEventListener('click', function() {
-    setoptions('sort');
-  });
-  get1video.addEventListener('click', function() {
-    setoptions('get1video');
-  });
-  sources.addEventListener('click', function() {
-    setoptions('sources');
-  });
-  search.addEventListener('click', function() {
-    setoptions('search');
-  });
-  searchCount.addEventListener('click', function() {
-    setoptions('searchCount');
-  });
-  createVideo.addEventListener('click', function() {
-    setoptions('createVideo');
-  });
-  updateVideo.addEventListener('click', function() {
-    setoptions('updateVideo');
-  });
-  addRendition.addEventListener('click', function() {
-    setoptions('addRendition');
-  });
-  addPoster.addEventListener('click', function() {
-    setoptions('addPoster');
-  });
-  addThumbnail.addEventListener('click', function() {
-    setoptions('addThumbnail');
-  });
-  get3playlists.addEventListener('click', function() {
-    setoptions('get3playlists');
-  });
-  get1playlist.addEventListener('click', function() {
-    setoptions('get1playlist');
-  });
-  getPlaylistVideoCount.addEventListener('click', function() {
-    setoptions('getPlaylistVideoCount');
-  });
-  getPlaylistVideos.addEventListener('click', function() {
-    setoptions('getPlaylistVideos');
-  });
-  createPlaylist.addEventListener('click', function() {
-    setoptions('createPlaylist');
-  });
-  updatePlaylist.addEventListener('click', function() {
-    setoptions('updatePlaylist');
-  });
-  getFolders.addEventListener('click', function() {
-    setoptions('getFolders');
-  });
-  getFolderVideos.addEventListener('click', function() {
-    setoptions('getFolderVideos');
-  });
-  addVideoToFolder.addEventListener('click', function() {
-    setoptions('addVideoToFolder');
-  });
-  removeVideoFromFolder.addEventListener('click', function() {
-    setoptions('removeVideoFromFolder');
+    getInfo();
+    setoptions('getHistory');
   });
 })(window, document);
