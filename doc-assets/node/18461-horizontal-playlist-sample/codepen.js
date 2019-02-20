90b94885308d6396c2b6dc5a4766e87e8cde6178
Robert Crooks <rcrooks@brightcove.com>
2019-02-20 13:23:01 -0500

    third time lucky

--- doc-assets/node/18461-horizontal-playlist-sample/codepen.js
+++ doc-assets/node/18461-horizontal-playlist-sample/codepen.js
@@ -81,8 +81,8 @@ videojs.getPlayer('myPlayerID').ready(function() {
     left.setAttribute('id', 'left-arrow');
     right.setAttribute('id', 'right-arrow');
     var parentElement = document.getElementsByClassName("bcls-playlist-wrapper");
-    parentElement[0].appendChild(left);
-    parentElement[0].appendChild(right);
+    parentElement[0].insertAdjacentElement('afterbegin', left);
+    parentElement[0].insertAdjacentElement('afterbegin', right);
     document.getElementById('left-arrow').innerHTML = '<';
     document.getElementById('right-arrow').innerHTML = '>';
     $(".bcls-playlist-item").wrapAll("<div class='list-container'></div>");
