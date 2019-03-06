021e792fd2ac84cd70973e008da7fabf0db81884
Robert Crooks <rcrooks@brightcove.com>
2019-03-06 15:36:07 -0500

    wip

--- doc-assets/video-cloud-apis/analytics-api/aapi-model-v2.js
+++ doc-assets/video-cloud-apis/analytics-api/aapi-model-v2.js
@@ -796,15 +796,30 @@ var aapi_model = {
       from: '2011-01-01',
       fields: ['browser_type', 'ad_mode_begin', 'ad_mode_complete', 'active_media', 'bytes_delivered', 'daily_unique_viewers', 'date', 'drm_bytes_packaged', 'engagement_score', 'licenses_served', 'live_seconds_streamed', 'play_request', 'play_rate', 'player_load', 'video_engagement_1', 'video_engagement_100', 'video_engagement_25', 'video_engagement_50', 'video_engagement_75', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
     },
+    browser_type__date_hour: {
+      dimensions: ['browser_type', 'date_hour'],
+      from: '2011-01-01',
+      fields: ['browser_type', 'ad_mode_begin', 'ad_mode_complete', 'date_hour', 'engagement_score', 'play_request', 'play_rate', 'player_load', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
+    },
     city__date: {
       dimensions: ['city', 'date'],
       from: '2011-01-01',
       fields: ['city', 'dma', 'ad_mode_begin', 'ad_mode_complete', 'active_media', 'bytes_delivered', 'daily_unique_viewers', 'date', 'drm_bytes_packaged', 'engagement_score', 'licenses_served', 'live_seconds_streamed', 'play_request', 'play_rate', 'player_load', 'video_engagement_1', 'video_engagement_100', 'video_engagement_25', 'video_engagement_50', 'video_engagement_75', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
     },
+    city__date_hour: {
+      dimensions: ['city', 'date_hour'],
+      from: '2011-01-01',
+      fields: ['city', 'dma', 'ad_mode_begin', 'ad_mode_complete', 'date_hour', 'engagement_score', 'play_request', 'play_rate', 'player_load', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
+    },
     country__date: {
       dimensions: ['country', 'date'],
       from: '2011-01-01',
       fields: ['country', 'country_name', 'ad_mode_begin', 'ad_mode_complete', 'active_media', 'bytes_delivered', 'daily_unique_viewers', 'date', 'drm_bytes_packaged', 'engagement_score', 'licenses_served', 'live_seconds_streamed', 'play_request', 'play_rate', 'player_load', 'video_engagement_1', 'video_engagement_100', 'video_engagement_25', 'video_engagement_50', 'video_engagement_75', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
+    },
+    country__date_hour: {
+      dimensions: ['country', 'date_hour'],
+      from: '2011-01-01',
+      fields: ['country', 'country_name', 'ad_mode_begin', 'ad_mode_complete', 'date_hour', 'engagement_score', 'play_request', 'play_rate', 'player_load', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
     },
     region__date: {
       dimensions: ['region', 'date'],
