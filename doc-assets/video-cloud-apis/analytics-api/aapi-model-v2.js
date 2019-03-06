761ba1c467b60bc5f20b548ba14018862d326b21
Robert Crooks <rcrooks@brightcove.com>
2019-03-06 13:51:50 -0500

    wip

--- doc-assets/video-cloud-apis/analytics-api/aapi-model-v2.js
+++ doc-assets/video-cloud-apis/analytics-api/aapi-model-v2.js
@@ -796,6 +796,21 @@ var aapi_model = {
       from: '2011-01-01',
       fields: ['city', 'dma', 'ad_mode_begin', 'ad_mode_complete', 'active_media', 'bytes_delivered', 'daily_unique_viewers', 'date', 'drm_bytes_packaged', 'engagement_score', 'licenses_served', 'live_seconds_streamed', 'play_request', 'play_rate', 'player_load', 'video_engagement_1', 'video_engagement_100', 'video_engagement_25', 'video_engagement_50', 'video_engagement_75', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
     },
+    country__date: {
+      dimensions: ['account', 'date'],
+      from: '2011-01-01',
+      fields: ['country', 'country_name', 'ad_mode_begin', 'ad_mode_complete', 'active_media', 'bytes_delivered', 'daily_unique_viewers', 'date', 'drm_bytes_packaged', 'engagement_score', 'licenses_served', 'live_seconds_streamed', 'play_request', 'play_rate', 'player_load', 'video_engagement_1', 'video_engagement_100', 'video_engagement_25', 'video_engagement_50', 'video_engagement_75', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
+    },
+    destination_domain__date: {
+      dimensions: ['account', 'date'],
+      from: '2011-01-01',
+      fields: ['destination_domain', 'ad_mode_begin', 'ad_mode_complete', 'active_media', 'bytes_delivered', 'daily_unique_viewers', 'date', 'drm_bytes_packaged', 'engagement_score', 'licenses_served', 'live_seconds_streamed', 'play_request', 'play_rate', 'player_load', 'video_engagement_1', 'video_engagement_100', 'video_engagement_25', 'video_engagement_50', 'video_engagement_75', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
+    },
+    destination_domain__date: {
+      dimensions: ['account', 'date'],
+      from: '2011-01-01',
+      fields: ['destination_domain', 'ad_mode_begin', 'ad_mode_complete', 'active_media', 'bytes_delivered', 'daily_unique_viewers', 'date', 'drm_bytes_packaged', 'engagement_score', 'licenses_served', 'live_seconds_streamed', 'play_request', 'play_rate', 'player_load', 'video_engagement_1', 'video_engagement_100', 'video_engagement_25', 'video_engagement_50', 'video_engagement_75', 'video_impression', 'video_percent_viewed', 'video_seconds_viewed', 'video_view']
+    },
     date__live_stream: {
       dimensions: ['date', 'live_stream'],
       from: '2015-10-19',
