bdb0f0e887c7a5a20203118f7c1252aede8fe5ac
Robert Crooks <rcrooks@brightcove.com>
2019-03-24 12:01:19 -0400

    fix

--- doc-assets/node/18474-playback-api-sample-mrss-generator/mrss-generator.js
+++ doc-assets/node/18474-playback-api-sample-mrss-generator/mrss-generator.js
@@ -188,12 +188,7 @@ var BCLS = (function(window, document) {
       mrssStr += sMediaTitle + sCdata + video.name + eCdata + eMediaTitle;
       mrssStr += sMediaDescription + sCdata + video.description + eCdata + eMediaDescription;
       if (doThumbnail) {
-        mrssStr += sMediaThumbnail + ' url="' + thumbnailURL + '"';
-        if (video.hasOwnProperty('images')) {
-          mrssStr += ' height="' + video.images.thumbnail.sources[0].height + '" width="' + video.images.thumbnail.sources[0].width + '"' + eMediaThumbnail;
-        } else {
-          mrssStr += eMediaThumbnail;
-        }
+        mrssStr += sMediaThumbnail + ' url="' + thumbnailURL + '"' + eMediaThumbnail;
       }
       mrssStr += eMediaContent;
       mrssStr += eItem;
