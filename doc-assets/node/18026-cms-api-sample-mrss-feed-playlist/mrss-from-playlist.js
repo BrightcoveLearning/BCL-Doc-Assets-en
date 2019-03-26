bb0d3a1e905a36d08228c5567a08030d945ceaf5
Robert Crooks <rcrooks@brightcove.com>
2019-03-26 15:58:09 -0400

    cleanup

--- doc-assets/node/18026-cms-api-sample-mrss-feed-playlist/mrss-from-playlist.js
+++ doc-assets/node/18026-cms-api-sample-mrss-feed-playlist/mrss-from-playlist.js
@@ -161,16 +161,16 @@ var BCLS = (function(window, document) {
         mrssStr += sMediaTitle + video.name + eMediaTitle;
         mrssStr += sMediaDescription + sCdata + video.description + eCdata + eMediaDescription;
         if (doThumbnail) {
-            mrssStr += sMediaThumbnail + ' url="' + thumbnailURL + '"';
-            if (isDefined(video.images)) {
-                mrssStr += ' height="' + video.images.thumbnail.sources[0].height + '" width="' + video.images.thumbnail.sources[0].width + '"' + eMediaThumbnail;
-            } else {
-                mrssStr += eMediaThumbnail;
-            }
+          mrssStr += sMediaThumbnail + ' url="' + thumbnailURL + '"';
+          if (isDefined(video.images)) {
+            mrssStr += ' height="' + video.images.thumbnail.sources[0].height + '" width="' + video.images.thumbnail.sources[0].width + '"' + eMediaThumbnail;
+          } else {
+            mrssStr += eMediaThumbnail;
+          }
         }
         mrssStr += eItem;
+      }
     }
-}
     mrssStr += eChannel + '</rss>';
     logger.textContent = 'Finished!';
     feedDisplay.textContent = vkbeautify.xml(mrssStr);
