cb3881e77f5217c9eb596bd5d03a27f49bbcc707
Robert Crooks <rcrooks@brightcove.com>
2019-04-04 12:33:16 -0400

    more

---
+++ doc-assets/ooyala/docs/DITA-OT/internal_staging/docs/out/android_mobilesdk/navtreedata.js
@@ -0,0 +1,42 @@
+var NAVTREE =
+[
+  [ "Ooyala Mobile SDK for Android", "index.html", [
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Packages", null, [
+      [ "Packages", "namespaces.html", "namespaces" ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classcom_1_1ooyala_1_1android_1_1_ooyala_player.html#a4f52a11709d4ed3c026acb8a80de3ed2",
+"classcom_1_1ooyala_1_1android_1_1adobeanalyticssdk_1_1_ooyala_adobe_heartbeat_configuration_1_1_builder.html",
+"classcom_1_1ooyala_1_1android_1_1ads_1_1vast_1_1_icon.html#a656b664c67701727b2ca79d3abebd219",
+"classcom_1_1ooyala_1_1android_1_1captions_1_1_caption_utils.html",
+"classcom_1_1ooyala_1_1android_1_1imasdk_1_1_i_m_a_ad_player.html#ac6dacac373921edfa42b8d592367d26e",
+"classcom_1_1ooyala_1_1android_1_1item_1_1_channel_set.html#a269c2de0f6a5990f55b78c24f5dc4daa",
+"classcom_1_1ooyala_1_1android_1_1item_1_1_dynamic_channel.html#a8be49bb370a82aaabd1c9cbdc0123163",
+"classcom_1_1ooyala_1_1android_1_1item_1_1_stream.html#a8576717712e5c2c370d7bb5ec295fe58",
+"classcom_1_1ooyala_1_1android_1_1performance_1_1_performance_statistics_snapshot_builder.html#a11516d65d4dbfa0b95ea1fef277f8d28",
+"classcom_1_1ooyala_1_1android_1_1player_1_1_movie_player.html#a48c90816be0b4b5c46ab34a0a5195bc9",
+"classcom_1_1ooyala_1_1android_1_1player_1_1exoplayer_1_1_event_logger.html#a8e379f57ca0c65607e020134c6e03a29",
+"classcom_1_1ooyala_1_1android_1_1pulseintegration_1_1_ooyala_pulse_manager.html#a26d464c3c1618926aaeaf8fa6f5c7d51",
+"classcom_1_1ooyala_1_1android_1_1ui_1_1_optimized_ooyala_player_layout_controller.html",
+"functions_func_m.html",
+"interfacecom_1_1ooyala_1_1android_1_1player_1_1exoplayer_1_1drm_1_1_drm_constants.html#a20513c412ffd1c260d62ea3e83aebf36"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
