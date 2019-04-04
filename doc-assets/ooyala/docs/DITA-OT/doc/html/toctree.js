386146a7dbcc7d2f700e3fb22324ed0d13b7cadf
Robert Crooks <rcrooks@brightcove.com>
2019-04-04 11:38:19 -0400

    more

---
+++ doc-assets/ooyala/docs/DITA-OT/doc/html/toctree.js
@@ -0,0 +1,129 @@
+
+      var tree;
+      
+      function treeInit() {
+      tree = new YAHOO.widget.TreeView("treeDiv1");
+      var root = tree.getRoot();
+    
+  
+  var objd4e11 = { label: "DITA Open Toolkit 1.6", href:"DITA-OTProjectHome.html", target:"contentwin" };
+    var d4e11 = new YAHOO.widget.TextNode(objd4e11, root, false);
+  
+  var objd4e24 = { label: "Quick start: running the demo", href:"quickstartguide/demostart.html", target:"contentwin" };
+    var d4e24 = new YAHOO.widget.TextNode(objd4e24, root, false);var objd4e43 = { label: "DITA-OT Easy Install", href:"quickstartguide/installing.html", target:"contentwin" };
+    var d4e43 = new YAHOO.widget.TextNode(objd4e43, d4e24, false);var objd4e53 = { label: "Running the demo build", href:"quickstartguide/rundemo.html", target:"contentwin" };
+    var d4e53 = new YAHOO.widget.TextNode(objd4e53, d4e24, false);var objd4e63 = { label: "Building other maps and other formats", href:"quickstartguide/runmore.html", target:"contentwin" };
+    var d4e63 = new YAHOO.widget.TextNode(objd4e63, d4e24, false);
+  
+  
+  var objd4e74 = { label: "User Guide", href:"readme/DITA-readme.html", target:"contentwin" };
+    var d4e74 = new YAHOO.widget.TextNode(objd4e74, root, false);var objd4e84 = { label: "Release notes", href:"readme/changes/rel1.6.html", target:"contentwin" };
+    var d4e84 = new YAHOO.widget.TextNode(objd4e84, d4e74, false);var objd4e91 = { label: "DITA 1.2 Specification Support", href:"specification/DITA1-2.html", target:"contentwin" };
+    var d4e91 = new YAHOO.widget.TextNode(objd4e91, d4e74, false);var objd4e114 = { label: "Distribution packages", href:"readme/distribution_packages.html", target:"contentwin" };
+    var d4e114 = new YAHOO.widget.TextNode(objd4e114, d4e74, false);var objd4e124 = { label: "Installing DITA-OT", href:"readme/DITA-installation.html", target:"contentwin" };
+    var d4e124 = new YAHOO.widget.TextNode(objd4e124, d4e74, false);var objd4e134 = { label: "Running DITA-OT from Ant", href:"readme/DITA-ant.html", target:"contentwin" };
+    var d4e134 = new YAHOO.widget.TextNode(objd4e134, d4e74, false);var objd4e144 = { label: "Running Ant", href:"readme/DITA-antuse.html", target:"contentwin" };
+    var d4e144 = new YAHOO.widget.TextNode(objd4e144, d4e134, false);var objd4e157 = { label: "Ant tasks and scripts", href:"readme/DITA-antscript.html", target:"contentwin" };
+    var d4e157 = new YAHOO.widget.TextNode(objd4e157, d4e134, false);var objd4e167 = { label: "More advanced Ant builds", href:"quickstartguide/concepts/about_antbuildfiles.html", target:"contentwin" };
+    var d4e167 = new YAHOO.widget.TextNode(objd4e167, d4e134, false);var objd4e177 = { label: "Running DITA-OT from command-line tool", href:"readme/DITA-javacmd.html", target:"contentwin" };
+    var d4e177 = new YAHOO.widget.TextNode(objd4e177, d4e74, false);var objd4e187 = { label: "Command line tool arguments", href:"readme/dita-ot_java_properties.html", target:"contentwin" };
+    var d4e187 = new YAHOO.widget.TextNode(objd4e187, d4e177, false);var objd4e200 = { label: "Arguments", href:"readme/dita-ot_ant_properties.html", target:"contentwin" };
+    var d4e200 = new YAHOO.widget.TextNode(objd4e200, d4e74, false);var objd4e210 = { label: "Configuration", href:"readme/configuration.html", target:"contentwin" };
+    var d4e210 = new YAHOO.widget.TextNode(objd4e210, d4e74, false);var objd4e220 = { label: "Available DITA-OT Transforms", href:"readme/AvailableTransforms.html", target:"contentwin" };
+    var d4e220 = new YAHOO.widget.TextNode(objd4e220, d4e74, false);var objd4e230 = { label: "DITA to XHTML", href:"readme/dita2xhtml.html", target:"contentwin" };
+    var d4e230 = new YAHOO.widget.TextNode(objd4e230, d4e220, false);var objd4e243 = { label: "DITA to Eclipse help", href:"readme/dita2eclipsehelp.html", target:"contentwin" };
+    var d4e243 = new YAHOO.widget.TextNode(objd4e243, d4e220, false);var objd4e253 = { label: "DITA to TocJS", href:"readme/dita2tocjs.html", target:"contentwin" };
+    var d4e253 = new YAHOO.widget.TextNode(objd4e253, d4e220, false);var objd4e263 = { label: "DITA to PDF (PDF2)", href:"readme/dita2pdf.html", target:"contentwin" };
+    var d4e263 = new YAHOO.widget.TextNode(objd4e263, d4e220, false);var objd4e273 = { label: "Customizing PDF output", href:"readme/dita2pdf-customization.html", target:"contentwin" };
+    var d4e273 = new YAHOO.widget.TextNode(objd4e273, d4e263, false);var objd4e283 = { label: "DITA to HTML Help (CHM)", href:"readme/dita2htmlhelp.html", target:"contentwin" };
+    var d4e283 = new YAHOO.widget.TextNode(objd4e283, d4e220, false);var objd4e296 = { label: "DITA to ODT", href:"readme/dita2odt.html", target:"contentwin" };
+    var d4e296 = new YAHOO.widget.TextNode(objd4e296, d4e220, false);var objd4e306 = { label: "DITA to Docbook", href:"readme/dita2docbook.html", target:"contentwin" };
+    var d4e306 = new YAHOO.widget.TextNode(objd4e306, d4e220, false);var objd4e316 = { label: "DITA to Troff", href:"readme/dita2troff.html", target:"contentwin" };
+    var d4e316 = new YAHOO.widget.TextNode(objd4e316, d4e220, false);var objd4e326 = { label: "DITA to RTF", href:"readme/dita2word.html", target:"contentwin" };
+    var d4e326 = new YAHOO.widget.TextNode(objd4e326, d4e220, false);var objd4e356 = { label: "DITA to Eclipse Content", href:"readme/dita2eclipsecontent.html", target:"contentwin" };
+    var d4e356 = new YAHOO.widget.TextNode(objd4e356, d4e220, false);var objd4e367 = { label: "DITA to legacy PDF", href:"readme/dita2legacypdf.html", target:"contentwin" };
+    var d4e367 = new YAHOO.widget.TextNode(objd4e367, d4e220, false);var objd4e377 = { label: "Migrating HTML to DITA", href:"readme/DITA-h2d.html", target:"contentwin" };
+    var d4e377 = new YAHOO.widget.TextNode(objd4e377, d4e220, false);var objd4e405 = { label: "Globalization in the DITA-OT", href:"readme/DITA-globalization.html", target:"contentwin" };
+    var d4e405 = new YAHOO.widget.TextNode(objd4e405, d4e74, false);var objd4e415 = { label: "Localization in XHTML based transforms", href:"readme/DITA-globalization-xhtml.html", target:"contentwin" };
+    var d4e415 = new YAHOO.widget.TextNode(objd4e415, d4e405, false);var objd4e425 = { label: "Localization in PDF based transforms", href:"readme/DITA-globalization-pdf.html", target:"contentwin" };
+    var d4e425 = new YAHOO.widget.TextNode(objd4e425, d4e405, false);var objd4e435 = { label: "Error messages and troubleshooting", href:"readme/troubleshooting-overview.html", target:"contentwin" };
+    var d4e435 = new YAHOO.widget.TextNode(objd4e435, d4e74, false);var objd4e445 = { label: "DITA-OT error messages", href:"readme/DITA-messages.html", target:"contentwin" };
+    var d4e445 = new YAHOO.widget.TextNode(objd4e445, d4e435, false);var objd4e465 = { label: "Other error messages", href:"readme/DITA-troubleshooting.html", target:"contentwin" };
+    var d4e465 = new YAHOO.widget.TextNode(objd4e465, d4e435, false);var objd4e475 = { label: "Log files", href:"readme/DITA-log.html", target:"contentwin" };
+    var d4e475 = new YAHOO.widget.TextNode(objd4e475, d4e435, false);var objd4e485 = { label: "Enabling debug mode", href:"readme/enabling-debug-mode.html", target:"contentwin" };
+    var d4e485 = new YAHOO.widget.TextNode(objd4e485, d4e435, false);var objd4e495 = { label: "Increasing Java memory allocation", href:"readme/increasing-the-jvm.html", target:"contentwin" };
+    var d4e495 = new YAHOO.widget.TextNode(objd4e495, d4e435, false);
+  var d4e516 = new YAHOO.widget.TextNode("Developer Reference", root, false);var objd4e517 = { label: "DITA Open Toolkit Architecture", href:"dev_ref/DITA-OTArchitecture.html", target:"contentwin" };
+    var d4e517 = new YAHOO.widget.TextNode(objd4e517, d4e516, false);var objd4e527 = { label: "Processing modules in the DITA-OT", href:"dev_ref/processing-pipeline-modules.html", target:"contentwin" };
+    var d4e527 = new YAHOO.widget.TextNode(objd4e527, d4e517, false);var objd4e537 = { label: "Processing order within the DITA-OT", href:"dev_ref/processing-order.html", target:"contentwin" };
+    var d4e537 = new YAHOO.widget.TextNode(objd4e537, d4e517, false);var objd4e547 = { label: "DITA-OT pre-processing architecture", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e547 = new YAHOO.widget.TextNode(objd4e547, d4e517, false);var objd4e557 = { label: "Generate lists (gen-list)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e557 = new YAHOO.widget.TextNode(objd4e557, d4e547, false);var objd4e570 = { label: "Debug and filter (debug-filter)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e570 = new YAHOO.widget.TextNode(objd4e570, d4e547, false);var objd4e583 = { label: "Copy related files (copy-files)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e583 = new YAHOO.widget.TextNode(objd4e583, d4e547, false);var objd4e596 = { label: "Conref push (conrefpush)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e596 = new YAHOO.widget.TextNode(objd4e596, d4e547, false);var objd4e609 = { label: "Conref (conref)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e609 = new YAHOO.widget.TextNode(objd4e609, d4e547, false);var objd4e622 = { label: "Move metadata (move-meta-entries)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e622 = new YAHOO.widget.TextNode(objd4e622, d4e547, false);var objd4e635 = { label: "Resolve keyref (keyref)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e635 = new YAHOO.widget.TextNode(objd4e635, d4e547, false);var objd4e648 = { label: "Resolve code references (codref)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e648 = new YAHOO.widget.TextNode(objd4e648, d4e547, false);var objd4e664 = { label: "Resolve map references (mapref)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e664 = new YAHOO.widget.TextNode(objd4e664, d4e547, false);var objd4e677 = { label: "Pull content into maps (mappull)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e677 = new YAHOO.widget.TextNode(objd4e677, d4e547, false);var objd4e691 = { label: "Chunk topics (chunk)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e691 = new YAHOO.widget.TextNode(objd4e691, d4e547, false);var objd4e704 = { label: "Map based linking (maplink and move-links)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e704 = new YAHOO.widget.TextNode(objd4e704, d4e547, false);var objd4e714 = { label: "Pull content into topics (topicpull)", href:"dev_ref/DITA-OTPreprocess.html", target:"contentwin" };
+    var d4e714 = new YAHOO.widget.TextNode(objd4e714, d4e547, false);var objd4e733 = { label: "Generating XHTML with navigation", href:"dev_ref/XhtmlWithNavigation.html", target:"contentwin" };
+    var d4e733 = new YAHOO.widget.TextNode(objd4e733, d4e517, false);var objd4e743 = { label: "Default XHTML output", href:"dev_ref/XhtmlDefault.html", target:"contentwin" };
+    var d4e743 = new YAHOO.widget.TextNode(objd4e743, d4e733, false);var objd4e756 = { label: "Eclipse help output (transform type \"eclipsehelp\")", href:"dev_ref/XhtmlEclipse.html", target:"contentwin" };
+    var d4e756 = new YAHOO.widget.TextNode(objd4e756, d4e733, false);var objd4e769 = { label: "TocJS output path", href:"dev_ref/XhtmlTocjs.html", target:"contentwin" };
+    var d4e769 = new YAHOO.widget.TextNode(objd4e769, d4e733, false);var objd4e779 = { label: "Compiled Help (CHM) output", href:"dev_ref/XhtmlCHM.html", target:"contentwin" };
+    var d4e779 = new YAHOO.widget.TextNode(objd4e779, d4e733, false);var objd4e789 = { label: "Javahelp output", href:"dev_ref/XhtmlJavahelp.html", target:"contentwin" };
+    var d4e789 = new YAHOO.widget.TextNode(objd4e789, d4e733, false);var objd4e799 = { label: "PDF output pipeline", href:"dev_ref/PdfDefault.html", target:"contentwin" };
+    var d4e799 = new YAHOO.widget.TextNode(objd4e799, d4e517, false);var objd4e809 = { label: "ODT Transform type (Open Document Format)", href:"dev_ref/OdtDefault.html", target:"contentwin" };
+    var d4e809 = new YAHOO.widget.TextNode(objd4e809, d4e517, false);var objd4e819 = { label: "Extending the DITA Open Toolkit", href:"dev_ref/extending-the-ot.html", target:"contentwin" };
+    var d4e819 = new YAHOO.widget.TextNode(objd4e819, d4e516, false);var objd4e829 = { label: "Installing plug-ins", href:"dev_ref/plugins-installing.html", target:"contentwin" };
+    var d4e829 = new YAHOO.widget.TextNode(objd4e829, d4e819, false);var objd4e839 = { label: "Removing plug-ins", href:"dev_ref/plugins-removing.html", target:"contentwin" };
+    var d4e839 = new YAHOO.widget.TextNode(objd4e839, d4e819, false);var objd4e849 = { label: "Rebuilding the DITA-OT documentation", href:"dev_ref/rebuilding-the-dita-ot-documentation.html", target:"contentwin" };
+    var d4e849 = new YAHOO.widget.TextNode(objd4e849, d4e819, false);var objd4e859 = { label: "Creating DITA-OT plug-ins", href:"dev_ref/plugins-overview.html", target:"contentwin" };
+    var d4e859 = new YAHOO.widget.TextNode(objd4e859, d4e516, false);var objd4e869 = { label: "Plug-in configuration file", href:"dev_ref/plugin-configfile.html", target:"contentwin" };
+    var d4e869 = new YAHOO.widget.TextNode(objd4e869, d4e859, false);var objd4e882 = { label: "Extending the XML Catalog", href:"dev_ref/plugin-xmlcatalog.html", target:"contentwin" };
+    var d4e882 = new YAHOO.widget.TextNode(objd4e882, d4e859, false);var objd4e892 = { label: "Adding new targets to the Ant build process", href:"dev_ref/plugin-anttarget.html", target:"contentwin" };
+    var d4e892 = new YAHOO.widget.TextNode(objd4e892, d4e859, false);var objd4e902 = { label: "Adding Ant targets to the pre-process pipeline", href:"dev_ref/plugin-antpreprocess.html", target:"contentwin" };
+    var d4e902 = new YAHOO.widget.TextNode(objd4e902, d4e859, false);var objd4e912 = { label: "Integrating a new transform type", href:"dev_ref/plugin-newtranstype.html", target:"contentwin" };
+    var d4e912 = new YAHOO.widget.TextNode(objd4e912, d4e859, false);var objd4e922 = { label: "Override styles with XSLT", href:"dev_ref/plugin-overridestyle.html", target:"contentwin" };
+    var d4e922 = new YAHOO.widget.TextNode(objd4e922, d4e859, false);var objd4e932 = { label: "Modifying or adding generated text", href:"dev_ref/plugin-addgeneratedtext.html", target:"contentwin" };
+    var d4e932 = new YAHOO.widget.TextNode(objd4e932, d4e859, false);var objd4e942 = { label: "Passing parameters to existing XSLT steps", href:"dev_ref/plugin-xsltparams.html", target:"contentwin" };
+    var d4e942 = new YAHOO.widget.TextNode(objd4e942, d4e859, false);var objd4e955 = { label: "Adding Java libraries to the classpath", href:"dev_ref/plugin-javalib.html", target:"contentwin" };
+    var d4e955 = new YAHOO.widget.TextNode(objd4e955, d4e859, false);var objd4e965 = { label: "Adding diagnostic messages", href:"dev_ref/plugin-messages.html", target:"contentwin" };
+    var d4e965 = new YAHOO.widget.TextNode(objd4e965, d4e859, false);var objd4e976 = { label: "Managing plug-in dependencies", href:"dev_ref/plugin-dependencies.html", target:"contentwin" };
+    var d4e976 = new YAHOO.widget.TextNode(objd4e976, d4e859, false);var objd4e998 = { label: "Version and support information", href:"dev_ref/plugin-support.html", target:"contentwin" };
+    var d4e998 = new YAHOO.widget.TextNode(objd4e998, d4e859, false);var objd4e1008 = { label: "Creating a new plug-in extension point", href:"dev_ref/plugin-newextensions.html", target:"contentwin" };
+    var d4e1008 = new YAHOO.widget.TextNode(objd4e1008, d4e859, false);var objd4e1021 = { label: "Example plugin.xml file", href:"dev_ref/plugin-sample.html", target:"contentwin" };
+    var d4e1021 = new YAHOO.widget.TextNode(objd4e1021, d4e859, false);var objd4e1034 = { label: "Implementation dependent features", href:"dev_ref/DITA1.2-implementation-dependent-features.html", target:"contentwin" };
+    var d4e1034 = new YAHOO.widget.TextNode(objd4e1034, d4e516, false);var objd4e1041 = { label: "Extended functionality", href:"dev_ref/extended-functionality.html", target:"contentwin" };
+    var d4e1041 = new YAHOO.widget.TextNode(objd4e1041, d4e516, false);var objd4e1048 = { label: "Topic merge", href:"dev_ref/topicmerge.html", target:"contentwin" };
+    var d4e1048 = new YAHOO.widget.TextNode(objd4e1048, d4e516, false);var objd4e1055 = { label: "Creating Eclipse help from within Eclipse", href:"dev_ref/workingwithdocplugin.html", target:"contentwin" };
+    var d4e1055 = new YAHOO.widget.TextNode(objd4e1055, d4e516, false);
+var objd4e1067 = { label: "Project management", href:"admin/DITA-OTProjectManagement.html", target:"contentwin" };
+    var d4e1067 = new YAHOO.widget.TextNode(objd4e1067, root, false);var objd4e1077 = { label: "Goals and objectives", href:"admin/DITA-OTProjectGoals.html", target:"contentwin" };
+    var d4e1077 = new YAHOO.widget.TextNode(objd4e1077, d4e1067, false);var objd4e1087 = { label: "Development Process", href:"admin/DevelopmentProcess.html", target:"contentwin" };
+    var d4e1087 = new YAHOO.widget.TextNode(objd4e1087, d4e1067, false);var objd4e1097 = { label: "Project roles", href:"admin/ProjectRoles.html", target:"contentwin" };
+    var d4e1097 = new YAHOO.widget.TextNode(objd4e1097, d4e1087, false);var objd4e1107 = { label: "Release management", href:"admin/ReleaseLifeCycle.html", target:"contentwin" };
+    var d4e1107 = new YAHOO.widget.TextNode(objd4e1107, d4e1087, false);var objd4e1117 = { label: "Requesting features and reporting defects", href:"admin/FixesAndFeatures.html", target:"contentwin" };
+    var d4e1117 = new YAHOO.widget.TextNode(objd4e1117, d4e1087, false);var objd4e1127 = { label: "How to participate in the DITA Open Toolkit", href:"admin/HowToParticipate.html", target:"contentwin" };
+    var d4e1127 = new YAHOO.widget.TextNode(objd4e1127, d4e1087, false);var objd4e1137 = { label: "Bug fix guidelines", href:"admin/BugFixGuidelines-NoCLA.html", target:"contentwin" };
+    var d4e1137 = new YAHOO.widget.TextNode(objd4e1137, d4e1127, false);var objd4e1147 = { label: "Contribution policy", href:"admin/ContributionPolicy.html", target:"contentwin" };
+    var d4e1147 = new YAHOO.widget.TextNode(objd4e1147, d4e1127, false);var objd4e1157 = { label: "Contribution Questionnaire Form", href:"admin/ContributionForm.html", target:"contentwin" };
+    var d4e1157 = new YAHOO.widget.TextNode(objd4e1157, d4e1127, false);
+  var d4e1165 = new YAHOO.widget.TextNode("Resources", root, false);var objd4e1166 = { label: "Project page at dita.xml.org", href:"http://dita.xml.org/wiki/the-dita-open-toolkit", target:"contentwin" };
+    var d4e1166 = new YAHOO.widget.TextNode(objd4e1166, d4e1165, false);var objd4e1173 = { label: "Yahoo! Group: dita-users", href:"http://groups.yahoo.com/group/dita-users/", target:"contentwin" };
+    var d4e1173 = new YAHOO.widget.TextNode(objd4e1173, d4e1165, false);var objd4e1180 = { label: "OASIS DITA Technical Committee ", href:"http://www.oasis-open.org/committees/dita/", target:"contentwin" };
+    var d4e1180 = new YAHOO.widget.TextNode(objd4e1180, d4e1165, false);var objd4e1187 = { label: "DITA developerWorks Articles", href:"articles/DITA-dWarticles.html", target:"contentwin" };
+    var d4e1187 = new YAHOO.widget.TextNode(objd4e1187, d4e1165, false);
+  
+  
+  
+
+      tree.draw(); 
+      } 
+      
+      YAHOO.util.Event.addListener(window, "load", treeInit); 
+    
