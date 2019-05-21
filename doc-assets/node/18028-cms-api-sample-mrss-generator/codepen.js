a546dbe46e2d4175bcedaf16abb31150e34f1410
Robert Crooks <rcrooks@brightcove.com>
2019-05-21 08:39:54 -0400

    fix to remove videos that have no sources

--- doc-assets/node/18028-cms-api-sample-mrss-generator/codepen.js
+++ doc-assets/node/18028-cms-api-sample-mrss-generator/codepen.js
@@ -259,8 +259,10 @@ var BCLS = ( function (window, document) {
                     var source = processSources(sources);
                     videosArray[callNumber].source = source;
                 } else {
-                    // video has no sources
-                    videosArray[callNumber].source = null;
+                    // video has no sources - remove it
+                    videosArray.splice(callNumber, 1);
+                    // to get next video, call getVideoSources without incrementing callNumber
+                    createRequest('getVideoSources');
                 }
                 callNumber++;
                 if (callNumber < iMax) {
