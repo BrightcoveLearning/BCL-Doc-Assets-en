92379ea73c089f52ef80e3865da0957280780865
Robert Crooks <rcrooks@brightcove.com>
2019-05-30 16:13:36 -0400

    wip

--- doc-assets/js/learning-paths/learning-paths.js
+++ doc-assets/js/learning-paths/learning-paths.js
@@ -2,18 +2,24 @@ var BCLS_learning_paths = (function (window, document) {
   var thisWindow = window,
     learning_path_wrapper document.createElement('nav'),
     learning_path_item,
-    learning_path_hr;
+    learning_path_hr,
+    thisPath = window.location.pathname;
 
   learning_path_wrapper.classList.add('learning-path-wrapper');
     
   if (learning_path) {
     var i,
-      Imax = learning_path.length;
-    learning_path_item = document.createElement('div')
+      Imax = learning_path.length,
+      item,
+      a;
+    for (i = 0; i < Imax; i++) {
+      item = learning_path[i];
+      learning_path_item = document.createElement('div');
+      learning_path_item.classList.add('learning-path-item');
+    }
   }
 
   thisWindow.addEventListener('resize', function () {
-    console.log('window size', window.innerWidth);
     if (window.innerWidth < 1200) {
       learning_path_wrapper.setAttribute('style', 'visibility:hidden');
     } else {
