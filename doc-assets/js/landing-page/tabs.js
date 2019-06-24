cb5a52403f3c610c037b4e23336e3e9c2f6552e3
Robert Crooks <rcrooks@brightcove.com>
2019-06-24 07:55:08 -0400

    move script to S3

---
+++ doc-assets/js/landing-page/tabs.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function () {
+  jQuery('.tabs .tab-links a').on('click', function (e) {
+    var currentAttrValue = jQuery(this).attr('href');
+    // prevent default click action for links
+    e.preventDefault();
+    // Show/Hide Tabs
+    jQuery('.tabs ' + currentAttrValue).slideDown(400).siblings().slideUp(400);
+    // Change/remove current tab to active
+    jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+  });
+
+  var h = window.location.hash,
+  tabLinks = document.querySelectorAll('.tabs .tab-links a');
+  
+  if (h.length > 0) {
+    var iMax = tabLinks.length,
+      i;
+    for (i = 0; i < iMax; i++) {
+      if (tabLinks[i].getAttribute('href') === h) {
+        tabLinks[i].click();
+      }
+    }
+  }
+});
