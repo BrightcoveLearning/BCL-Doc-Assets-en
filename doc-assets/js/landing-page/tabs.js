728632d4645f21fc2440ab73b2d187f4309b48c9
Robert Crooks <rcrooks@brightcove.com>
2019-06-29 07:14:03 -0400

    fix

--- doc-assets/js/landing-page/tabs.js
+++ doc-assets/js/landing-page/tabs.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function () {
+  var h = window.location.hash,
+  tabLinks = document.querySelectorAll('.tabs .tab-links a');
   jQuery('.tabs .tab-links a').on('click', function (e) {
-    var currentAttrValue = {h.length > 0} ? h : jQuery(this).attr('href');
-    console.log('h', currentAttrValue);
-    
     // prevent default click action for links
     e.preventDefault();
+    var currentAttrValue = jQuery(this).attr('href');
+    console.log('h', currentAttrValue);
+    
     // Show/Hide Tabs
     jQuery('.tabs ' + currentAttrValue).slideDown(400).siblings().slideUp(400);
     // Change/remove current tab to active
@@ -21,8 +23,6 @@ jQuery(document).ready(function () {
   });
 
   // check to see if the URL has a hash and if so, open that tab
-  var h = window.location.hash,
-  tabLinks = document.querySelectorAll('.tabs .tab-links a');
   
   if (h.length > 0) {
     var iMax = tabLinks.length,
