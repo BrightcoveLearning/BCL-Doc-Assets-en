c9af2173edc08de32285ac1b041e10ae6d61e2ac
Robert Crooks <rcrooks@brightcove.com>
2019-09-17 12:34:11 -0400

    work on talla

--- doc-assets/js/talla/talla.js
+++ doc-assets/js/talla/talla.js
@@ -1,3 +1,18 @@
 var BCLS_talla = ( function (window, document) {
-  
+  var token = "​[ Customer Generated Token ]​"
+   Talla.widget = Talla.init(
+'00c26be3-4421-41e6-bb00-1dee68740f67, 
+Talla.OptionQuickLoadBeacon, 
+Talla.OptionBotName(" Talla.OptionThemeColor(" Talla.OptionBotIcon(
+"​{ image url for Bot profile }​" ),
+Talla.OptionOpenIcon(
+"​{ image url for beacon }​"
+), Talla.OptionAnonymous(
+"​{true or false}​" )
+)
+"),
+"),
+ ​{Bot Name}​
+ ​{ Color in Hex }​
+Talla.widget.display(Talla.OptionJWTCredentials(token))
 })(window, document);
