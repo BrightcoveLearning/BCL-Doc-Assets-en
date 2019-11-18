ae4b33122d1229c5289491ad4770e83e268f3854
Robert Crooks <rcrooks@brightcove.com>
2019-11-18 14:04:53 -0500

    highlighting in right nav menu

--- doc-assets/site-assets/js/site/toc.js
+++ doc-assets/site-assets/js/site/toc.js
@@ -24,10 +24,31 @@ var BCLS_toc = ( function (window, document) {
       }
       
       if (frag.firstChild) {
-        in_page_nav.appendChild(frag);''
+        in_page_nav.appendChild(frag);
       } else {
         parent = in_page_nav.parentNode;
         grandparent = parent.parentNode;
         grandparent.removeChild(parent);
+      }
+
+      // implement highlighting
+      function implementHighlighting() {
+        var navLinks = document.querySelectorAll('#in_page_nav>li>a'),
+          navItems = document.querySelectorAll('#in_page_nav>li'),
+          linkEl,
+          j,
+          jMax;
+
+        iMax = navLinks.length;
+        for (i = 0; i < iMax; i++) {
+          linkEl = navLinks[i];
+          linkEl.addEventListener('click', function() {
+            jMax = navItems.length;
+            for (j = 0; j < jMax; j++) {
+              navItems[j].removeAttribute('style');
+            }
+            this.parent.setAttribute('style', 'color:rgb(220, 243, 251);background-color:rgb(21, 160, 183);')
+          });
+        }
       }
 })(window, document);
