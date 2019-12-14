ba0740e78d4939003aa51c59ce9a67a888a55a79
Robert Crooks <rcrooks@brightcove.com>
2019-12-14 09:50:15 -0500

    try timeout

--- doc-assets/js/support-feedback.js
+++ doc-assets/js/support-feedback.js
@@ -1,8 +1,11 @@
 var BCLS_oldfeedback = ( function (window, document) {
-  var oldFeedback = document.getElementById('surveyDiv');
-  
-  if (oldFeedback) {
-    console.log('old feedback present');
-    oldFeedback.setAttribute('style', 'display:none');
+  var oldFeedback = document.getElementById('surveyDiv'),
+  t = window.setTimeout(removeOldFeedback, 2000);
+  function removeOldFeedback() {
+    console.log('try remove old feedback');
+    if (oldFeedback) {
+      console.log('old feedback present');
+      oldFeedback.setAttribute('style', 'display:none');
+    }
   }
 })(window, document);
