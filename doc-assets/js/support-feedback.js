852e7651326ac0806fe8c380bd5e347d05760186
Robert Crooks <rcrooks@brightcove.com>
2019-12-14 09:23:14 -0500

    debugging

--- doc-assets/js/support-feedback.js
+++ doc-assets/js/support-feedback.js
@@ -2,6 +2,7 @@ var BCLS_oldfeedback = ( function (window, document) {
   var oldFeedback = document.getElementById('surveyDiv');
   
   if (oldFeedback) {
+    console.log('old feedback present');
     oldFeedback.setAttribute('style', 'display:none');
   }
 })(window, document);
