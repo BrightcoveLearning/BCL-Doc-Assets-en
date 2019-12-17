a6ff23482c19d871746ff77fc36729236ae5a3e8
Robert Crooks <rcrooks@brightcove.com>
2019-12-17 10:10:12 -0500

    script to hide google + icon

---
+++ doc-assets/js/hide-google-plus.js
@@ -0,0 +1,4 @@
+var BCLS_hide_google_plus = ( function (window, document) {
+  var googleSVG = document.getElementsByClassName('icon__social--google-plus')[0].parentElement.parentElement;
+  googleSVG.setAttribute('style', 'display:none;color:white');
+})(window, document);
