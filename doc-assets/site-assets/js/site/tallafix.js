59658fd5574487f99bf08b8dbcb15cdc29eb1858
Robert Crooks <rcrooks@brightcove.com>
2020-03-17 13:14:41 -0400

    fix

--- doc-assets/site-assets/js/site/tallafix.js
+++ doc-assets/site-assets/js/site/tallafix.js
@@ -1,5 +1,18 @@
 var BCLS_tallafix = ( function (window, document) {
-  var  talla_frame = document.querySelector('#talla_parent>iframe'),
-  talla_style = talla_frame.getAttribute('style');
-  talla_frame.setAttribute('style', talla_style + 'bottom:unset;top:9rem;');
+  var  talla_parent = document.getElementById('talla_parent'),
+  t,
+  n = 0;
+
+  function fix_talla() {
+    if (talla_parent) {
+      var talla_frame = talla_parent.firstElementChild;
+      talla_style = talla_frame.getAttribute('style');
+      talla_frame.setAttribute('style', talla_style + 'bottom:unset;top:9rem;');
+    } else {
+      n++;
+      if (n < 5) {
+        t = window.setTimeout(fix_talla, 1000);
+      }
+    }
+  }
 })(window, document);
