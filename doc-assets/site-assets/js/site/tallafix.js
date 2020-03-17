6edfba826e71efc89fb1dac1e286a6db861cbde0
Robert Crooks <rcrooks@brightcove.com>
2020-03-17 12:06:22 -0400

    fix

--- doc-assets/site-assets/js/site/tallafix.js
+++ doc-assets/site-assets/js/site/tallafix.js
@@ -1,5 +1,5 @@
 var BCLS_tallafix = ( function (window, document) {
-  var  talla_frame = document.querySelector('talla_parent>iframe'),
+  var  talla_frame = document.querySelector('#talla_parent>iframe'),
   talla_style = talla_frame.getAttribute('style');
   talla_frame.setAttribute('style', talla_style + 'bottom:unset;top:9rem;');
 })(window, document);
