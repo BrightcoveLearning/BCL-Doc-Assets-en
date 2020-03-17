ed7ea5fe5fddad80af7c91235a59955fc890b546
Robert Crooks <rcrooks@brightcove.com>
2020-03-17 13:39:41 -0400

    another fix

--- doc-assets/site-assets/js/site/tallafix.js
+++ doc-assets/site-assets/js/site/tallafix.js
@@ -6,14 +6,16 @@ var BCLS_tallafix = ( function (window, document) {
   function fix_talla() {
     if (talla_parent) {
       var talla_frame = talla_parent.firstElementChild;
-      talla_style = talla_frame.getAttribute('style');
-      talla_frame.setAttribute('style', talla_style + 'bottom:unset;top:9rem;');
-    } else {
-      n++;
-      if (n < 5) {
-        console.log('n', n);
-        
-        t = window.setTimeout(fix_talla, 2000);
+      if (talla_frame) {
+        talla_style = talla_frame.getAttribute('style');
+        talla_frame.setAttribute('style', talla_style + 'bottom:unset;top:9rem;');
+      } else {
+        n++;
+        if (n < 10) {
+          console.log('n', n);
+          
+          t = window.setTimeout(fix_talla, 2000);
+        }
       }
     }
   }
