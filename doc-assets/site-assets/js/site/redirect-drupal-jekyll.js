17325159d0da6db2ac983767274596229ed6db0b
Robert Crooks <rcrooks@brightcove.com>
2020-04-07 15:24:10 -0400

    another fix

--- doc-assets/site-assets/js/site/redirect-drupal-jekyll.js
+++ doc-assets/site-assets/js/site/redirect-drupal-jekyll.js
@@ -16,7 +16,7 @@
         if (path === item.oldURL) {
           new_location = item.newURL;
           message = '<aside class="bcls-aside bcls-aside--warning" style="font-weight:bold;font-size:x-large">This page is obsolete and no longer updated. In 5 seconds, you will be redirected to the home page, where you can try to search for or navigate to the new location: ' + new_location + 'page you are looking for.</aside>';
-          cdiv.insertAdjacentElement('afterbegin', message);
+          cdiv.insertAdjacentHTML('afterbegin', message);
           redirect();
           break;
         }
