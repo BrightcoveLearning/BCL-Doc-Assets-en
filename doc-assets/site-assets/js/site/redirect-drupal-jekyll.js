94a0be04af5680e1a93320f2006c2fc03bbe45fd
Robert Crooks <rcrooks@brightcove.com>
2020-04-07 15:21:31 -0400

    fix

--- doc-assets/site-assets/js/site/redirect-drupal-jekyll.js
+++ doc-assets/site-assets/js/site/redirect-drupal-jekyll.js
@@ -11,20 +11,21 @@
       
       for (i = 0; i < iMax; i++) {
         item = navArray[i];
+        // console.log('item', item);
+        
         if (path === item.oldURL) {
           new_location = item.newURL;
           message = '<aside class="bcls-aside bcls-aside--warning" style="font-weight:bold;font-size:x-large">This page is obsolete and no longer updated. In 5 seconds, you will be redirected to the home page, where you can try to search for or navigate to the new location: ' + new_location + 'page you are looking for.</aside>';
           cdiv.insertAdjacentElement('afterbegin', message);
-          // redirect();
-          break;
-        } else {
-          new_location = 'https://support.brightcove.com';
-          message = '<aside class="bcls-aside bcls-aside--warning" style="font-weight:bold;font-size:x-large">This page is obsolete and no longer updated. In 5 seconds, you will be redirected to the home page, where you can try to search for or navigate to the page you are looking for.</aside>'
-          cdiv.insertAdjacentHTML('afterbegin', message);
-          // redirect();
+          redirect();
           break;
         }
       }
+      // no match, go home
+      new_location = 'https://support.brightcove.com';
+      message = '<aside class="bcls-aside bcls-aside--warning" style="font-weight:bold;font-size:x-large">This page is obsolete and no longer updated. In 5 seconds, you will be redirected to the home page, where you can try to search for or navigate to the page you are looking for.</aside>'
+      cdiv.insertAdjacentHTML('afterbegin', message);
+      redirect();
 
       function redirect() {
         var t = window.setTimeout(go_to_new_location, 5000);
