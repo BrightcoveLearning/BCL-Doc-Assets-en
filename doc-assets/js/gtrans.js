daaa00a8d4998458867635a970e0846133215d28
Robert Crooks <rcrooks@brightcove.com>
2020-04-10 15:51:19 -0400

    comments

--- doc-assets/js/gtrans.js
+++ doc-assets/js/gtrans.js
@@ -1,7 +1,7 @@
 function doGTranslate(lang_pair) {
   if(lang_pair.value)lang_pair=lang_pair.value;var lang=lang_pair.split('|')[1];var plang=location.pathname.split('/')[1];if(plang.length !=2 && plang != 'zh-CN' && plang != 'zh-TW')plang='en';if(lang == 'en')location.pathname=location.pathname.replace('/'+plang, '');else location.pathname='/'+lang+location.pathname.replace('/'+plang, '');
 }
-
+// keep on same language site
 function keepLanguage() {
   var plang = location.pathname.split('/')[1],
     domain = location.hostname,
@@ -22,7 +22,7 @@ function keepLanguage() {
         }
       }
     }
-}
+  } 
 
 function absolute(relative) {
   var base = location.href;
