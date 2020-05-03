da7ba8133e2df66780f2305c910abe79cfefdf1a
Robert Crooks <rcrooks@brightcove.com>
2020-05-03 11:12:48 -0400

    fix

--- doc-assets/site-assets/js/site/gtrans.js
+++ doc-assets/site-assets/js/site/gtrans.js
@@ -1,6 +1,6 @@
-function doGTranslate(lang_pair) {
-  if(lang_pair.value)lang_pair=lang_pair.value;var lang=lang_pair.split('|')[1];var plang=location.pathname.split('/')[1];if(plang.length !=2 && plang != 'zh-CN' && plang != 'zh-TW')plang='en';if(lang == 'en')location.pathname=location.pathname.replace('/'+plang, '');else location.pathname='/'+lang+location.pathname.replace('/'+plang, '');
-}
+// function doGTranslate(lang_pair) {
+//   if(lang_pair.value)lang_pair=lang_pair.value;var lang=lang_pair.split('|')[1];var plang=location.pathname.split('/')[1];if(plang.length !=2 && plang != 'zh-CN' && plang != 'zh-TW')plang='en';if(lang == 'en')location.pathname=location.pathname.replace('/'+plang, '');else location.pathname='/'+lang+location.pathname.replace('/'+plang, '');
+// }
 // keep on same language site
 function keepLanguage() {
   var domain = location.hostname,
