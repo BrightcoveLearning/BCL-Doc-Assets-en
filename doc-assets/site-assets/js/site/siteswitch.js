e197165e3660a2fad91e5e7f64451c85cb3eab98
Robert Crooks <rcrooks@brightcove.com>
2020-06-10 12:39:48 -0400

    debugging

--- doc-assets/site-assets/js/site/siteswitch.js
+++ doc-assets/site-assets/js/site/siteswitch.js
@@ -22,8 +22,11 @@ var BCLS_site_switch = ( function (window, document) {
       iMax = site_options.length;
       // note: starting iteration at 1 because first option isn't a link
       for (i = 1; i < iMax; i++) {
-        var site = site_options[i];
-          site.value = site.value.replace('https://', 'https://' + qLang + '.');
+        var site = site_options[i],
+          val = site.getAttribute('value');
+          site.setAttribute('value', val.replace('https://', 'https://' + qLang + '.'));
+          console.log('site', site.getAttribute('value'));
+          
       }
     } 
 
