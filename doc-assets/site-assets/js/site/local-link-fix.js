83599f4b9a82b0f7bce875fe91005222313e2e82
Robert Crooks <rcrooks@brightcove.com>
2020-06-10 12:56:47 -0400

    move site navigator link fix into general link fix

--- doc-assets/site-assets/js/site/local-link-fix.js
+++ doc-assets/site-assets/js/site/local-link-fix.js
@@ -8,7 +8,10 @@ function keepLanguage() {
     all_links = document.querySelectorAll('a[href]'),
     i,
     iMax,
-    talla_wrapper = document.getElementById('talla_wrapper');
+    talla_wrapper = document.getElementById('talla_wrapper'),
+    site_select = document.getElementById('site_select'),
+    site_options = site_select.querySelectorAll('option');
+
     console.log('lang', lang);
     
   if (lang === 'ja' || lang === 'ko' || lang === 'es' || lang === 'fr' || lang === 'de' || lang === 'zh-tw') {
@@ -25,34 +28,23 @@ function keepLanguage() {
       if (hrefValue[0].charAt(0) !== '#') {
         if (hrefValue[1].lastIndexOf('support.brightcove') > -1 && hrefValue[1].lastIndexOf(lang + '.') < 0) {
           newHref = hrefValue[0] + '//' + lang + '.' + hrefValue[1];
-          console.log('newHref remote', newHref);
+          // console.log('newHref remote', newHref);
           all_links[i].setAttribute('href', newHref);
         }
       }
     }
+    if  (lang !== '') {
+      iMax = site_options.length;
+      // note: starting iteration at 1 because first option isn't a link
+      for (i = 1; i < iMax; i++) {
+        var site = site_options[i],
+          val = site.getAttribute('value');
+          site.setAttribute('value', val.replace('https://', 'https://' + lang + '.'));
+          console.log('site', site.getAttribute('value'));
+          
+      }
+    } 
   }
 
-// function absolute(relative) {
-//   var base = location.href;
-//   if (relative.indexOf('//') === -1) { 
-//     var stack = base.split("/"),
-//       parts = relative.split("/");
-//     stack.pop(); // remove current file name (or empty string)
-//                   // (omit if "base" is the current folder without trailing slash)
-//     for (var i=0; i<parts.length; i++) {
-//       if (parts[i] == ".") {
-//         continue;
-//       }
-//       if (parts[i] == "..") {
-//         stack.pop();
-//       } else {
-//         stack.push(parts[i]);
-//       }
-//     }
-//     return stack.join("/");
-//   } else {
-//     return relative;
-//   }
-}
 
 keepLanguage();
