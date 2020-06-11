6dc3d3239eb73430934c1ea7bc8d9dce98052b22
Robert Crooks <rcrooks@brightcove.com>
2020-06-11 13:08:12 -0400

    fix for old links

--- doc-assets/site-assets/js/site/local-link-fix.js
+++ doc-assets/site-assets/js/site/local-link-fix.js
@@ -1,6 +1,7 @@
 // keep on same language site
 function keepLanguage() {
-  var domain = location.hostname,
+  var href = location.href,
+    domain = location.hostname,
     lang = domain.split('.')[0],
     all_links = document.querySelectorAll('a[href]'),
     i,
@@ -9,7 +10,33 @@ function keepLanguage() {
     site_select = document.getElementById('site_select'),
     site_options = site_select.querySelectorAll('option');
 
-    console.log('lang', lang);
+    // if old /en/ path, redirect
+    if (href.indexOf('/en/') > 0) {
+      location.href = href.replace('/en/', '/');
+    }
+
+    // if old localized site path, redirect to new and hope for the best
+    if (href.indexOf('/ja/') > 0) {
+      href = href.replace('/ja/', '/');
+      href = href.replace('//', '//ja.');
+      location.href = href;
+    } else if (href.indexOf('/ko/') > 0) {
+      href = href.replace('/ko/', '/');
+      href = href.replace('//', '//ko.');
+      location.href = href;
+    } else if (href.indexOf('/es/') > 0) {
+      href = href.replace('/es/', '/');
+      href = href.replace('//', '//es.');
+      location.href = href;
+    } else if (href.indexOf('/fr/') > 0) {
+      href = href.replace('/fr/', '/');
+      href = href.replace('//', '//fr.');
+      location.href = href;
+    } else if (href.indexOf('/de/') > 0) {
+      href = href.replace('/de/', '/');
+      href = href.replace('//', '//de.');
+      location.href = href;
+    }
     
   if (lang === 'ja' || lang === 'ko' || lang === 'es' || lang === 'fr' || lang === 'de' || lang === 'zh-tw') {
     var hrefValue, 
